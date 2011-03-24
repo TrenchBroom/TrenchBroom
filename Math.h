33db4746fedafac93826c6183627d47ee1a17358
Kristian Duske <kristian.duske@gmail.com>
2011-03-24 17:42:59 +0100

    Playing around with implementing the core math functions in C.
    Signed-off-by: Kristian Duske <kristian.duske@gmail.com>

--- Math.h
+++ Math.h
@@ -10,6 +10,38 @@
 
 extern float const AlmostZero;
 
+typedef struct {
+    float x,y,z;
+} TVector3f;
+
+typedef struct {
+    int x,y,z;
+} TVector3i;
+
+typedef struct {
+    float x,y,z;
+} TVector2f;
+
+typedef struct {
+    TVector3f point;
+    TVector3f direction;
+} TLine;
+
+typedef struct {
+    TVector3f origin;
+    TVector3f direction;
+} TRay;
+
+typedef struct {
+    TVector3f point;
+    TVector3f norm;
+} TPlane;
+
+typedef struct {
+    float scalar;
+    TVector3f vector;
+} TQuaternion;
+
 BOOL fzero(float v);
 BOOL fpos(float v);
 BOOL fneg(float v);
@@ -22,6 +54,32 @@ BOOL finxx(float v, float b1, float b2);
 BOOL finxi(float v, float b1, float b2);
 BOOL finix(float v, float b1, float b2);
 BOOL finii(float v, float b1, float b2);
+
+void setV3f(TVector3f* l, TVector3f* r);
+void addV3f(TVector3f* l, TVector3f* r);
+void subV3f(TVector3f* l, TVector3f* r);
+float dotV3f(TVector3f* l, TVector3f* r);
+void crossV3f(TVector3f* l, TVector3f* r);
+void scaleV3f(TVector3f* v, float f);
+float lengthV3f(TVector3f* v);
+float lengthSquaredV3f(TVector3f* v);
+void normalizeV3f(TVector3f* v);
+BOOL equalV3f(TVector3f* l, TVector3f* r);
+
+BOOL pointAbovePlane(TPlane* p, TVector3f* v);
+float intersectPlaneWithLine(TPlane* p, TLine* l);
+float intersectPlaneWithRay(TPlane* p, TRay* r);
+
+void setQ(TQuaternion* l, TQuaternion* r);
+void setAngleAndAxisQ(TQuaternion* q, float a, TVector3f* x);
+void mulQ(TQuaternion* l, TQuaternion* r);
+void conjugateQ(TQuaternion* q);
+void rotateQ(TQuaternion* q, TVector3f* v);
+
+TVector3f* rayPointAtDistance(TRay* r, float d);
+TVector3f* linePointAtDistance(TLine* l, float d);
+
+
 
 NSArray* makeCircle(float radius, int segments);
 NSArray* makeRing(float innerRadius, float outerRadius, int segments);
--- Math.m
+++ Math.m
@@ -61,6 +61,182 @@ BOOL finii(float v, float b1, float b2) {
     return b1 < b2 ? fgte(v, b1) && flte(v, b2) : fgte(v, b2) && flte(v, b1);
 }
 
+#pragma mark TVector3f functions
+
+TVector3f* newV3f(TVector3f* v) {
+    TVector3f* r = malloc(sizeof(TVector3f));
+    setV3f(r, v);
+    return r;
+}
+
+void setV3f(TVector3f* l, TVector3f* r) {
+    l->x = r->x;
+    l->y = r->y;
+    l->z = r->z;
+}
+
+void addV3f(TVector3f* l, TVector3f* r) {
+    l->x += r->x;
+    l->y += r->y;
+    l->z += r->z;
+}
+
+void subV3f(TVector3f* l, TVector3f* r) {
+    l->x -= r->x;
+    l->y -= r->y;
+    l->z -= r->z;
+}
+
+float dotV3f(TVector3f* l, TVector3f* r) {
+    return l->x * r->x + l->y * r->y + l->z * r->z;
+}
+
+void crossV3f(TVector3f* l, TVector3f* r) {
+    float xt = l->y * r->z - l->z * r->y;
+    float yt = l->z * r->x - l->x * r->z;
+    l->z = l->x * r->y - l->y * r->x;
+    l->x = xt;
+    l->y = yt;
+}
+
+void scaleV3f(TVector3f* v, float f) {
+    v->x *= f;
+    v->y *= f;
+    v->z *= f;
+}
+
+float lengthV3f(TVector3f* v) {
+    return sqrt(lengthSquaredV3f(v));
+}
+
+float lengthSquaredV3f(TVector3f* v) {
+    return dotV3f(v, v);
+}
+
+void normalizeV3f(TVector3f* v) {
+    float l = lengthV3f(v);
+    v->x /= l;
+    v->y /= l;
+    v->z /= l;
+}
+
+BOOL equalV3f(TVector3f* l, TVector3f* r) {
+    return feq(l->x, r->x) && feq(l->y, r->y) && feq(l->z, r->z);
+}
+
+# pragma mark TPlane functions
+
+BOOL pointAbovePlane(TPlane* p, TVector3f* v) {
+    TVector3f* t = newV3f(v);
+    subV3f(t, &p->point);
+
+    BOOL a = fpos(dotV3f(&p->norm, t));
+    free(t);
+    return a;
+}
+
+float intersectPlaneWithLine(TPlane* p, TLine* l) {
+    float d = dotV3f(&l->direction, &p->norm);
+    if (fzero(d))
+        return NAN;
+    
+    TVector3f* v = newV3f(&p->point);
+    subV3f(v, &l->point);
+    float r = dotV3f(v, &p->norm) / d;
+
+    free(v);
+    return r;
+}
+
+float intersectPlaneWithRay(TPlane* p, TRay* r) {
+    float d = dotV3f(&r->direction, &p->norm);
+    if (fzero(d))
+        return NAN;
+    
+    TVector3f* v = newV3f(&p->point);
+    subV3f(v, &r->origin);
+    float s = dotV3f(v, &p->norm) / d;
+    free(v);
+    
+    if (fneg(s))
+        return NAN;
+    
+    return s;
+}
+
+#pragma mark TRay functions
+
+TVector3f* rayPointAtDistance(TRay* r, float d) {
+    TVector3f* v = newV3f(&r->direction);
+    scaleV3f(v, d);
+    addV3f(v, &r->origin);
+    return v;
+}
+
+#pragma mark TLine functions
+
+TVector3f* linePointAtDistance(TLine* l, float d) {
+    TVector3f* v = newV3f(&l->direction);
+    scaleV3f(v, d);
+    addV3f(v, &l->point);
+    return v;
+}
+
+# pragma mark TQuaternion functions
+
+void setQ(TQuaternion* l, TQuaternion* r) {
+    l->scalar = r->scalar;
+    setV3f(&l->vector, &r->vector);
+}
+
+void setAngleAndAxisQ(TQuaternion* q, float a, TVector3f* x) {
+    q->scalar = cos(a / 2);
+    setV3f(&q->vector, x);
+    scaleV3f(&q->vector, sin(a / 2));
+}
+
+void mulQ(TQuaternion* l, TQuaternion* r) {
+    float a = l->scalar;
+    TVector3f* v = &l->vector;
+    float b = r->scalar;
+    TVector3f* w = &r->vector;
+    
+    float nx = a * w->x + b * v->x + v->y * w->z - v->z * w->y;
+    float ny = a * w->y + b * v->y + v->z * w->x - v->x * w->z;
+    float nz = a * w->z + b * v->z + v->x * w->y - v->y * w->x;
+    
+    a = a * b - dotV3f(v, w);
+    v->x = nx;
+    v->y = ny;
+    v->z = nz;
+}
+
+void conjugateQ(TQuaternion* q) {
+    scaleV3f(&q->vector, -1);
+}
+
+void rotateQ(TQuaternion* q, TVector3f* v) { 
+    TQuaternion* t = malloc(sizeof(TQuaternion));
+    setQ(t, q);
+
+    TQuaternion* p = malloc(sizeof(TQuaternion));
+    p->scalar = 0;
+    setV3f(&p->vector, v);
+    
+    TQuaternion* c = malloc(sizeof(TQuaternion));
+    setQ(c, q);
+    conjugateQ(c);
+    
+    mulQ(t, p);
+    mulQ(t, c);
+    setV3f(v, &t->vector);
+    
+    free(t);
+    free(p);
+    free(c);
+}
+
+
 NSArray* makeCircle(float radius, int segments) {
     NSMutableArray* points = [[NSMutableArray alloc] initWithCapacity:segments];
     
