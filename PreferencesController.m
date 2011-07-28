d5acd0ad06bf40c89aa8078cc15ead5d226c6c60
Kristian Duske <kristian.duske@gmail.com>
2011-07-28 15:42:16 +0200

    Cleanup and preferences.
    Signed-off-by: Kristian Duske <kristian.duske@gmail.com>

---
+++ PreferencesController.m
@@ -0,0 +1,30 @@
+//
+//  PreferencesWindowController.m
+//  TrenchBroom
+//
+//  Created by Kristian Duske on 28.07.11.
+//  Copyright 2011 TU Berlin. All rights reserved.
+//
+
+#import "PreferencesWindowController.h"
+
+@implementation PreferencesWindowController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
