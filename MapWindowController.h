6b5d3fe970ce0f6f83c066a831be88e61ecf0f1c
Kristian Duske <kristian.duske@gmail.com>
2011-10-02 22:30:20 +0200

    Added GPLv3 notice to every file.
    Signed-off-by: Kristian Duske <kristian.duske@gmail.com>

--- MapWindowController.h
+++ MapWindowController.h
@@ -1,10 +1,19 @@
-//
-//  MapWindowController.h
-//  TrenchBroom
-//
-//  Created by Kristian Duske on 15.03.10.
-//  Copyright 2010 __MyCompanyName__. All rights reserved.
-//
+/*
+This file is part of TrenchBroom.
+
+TrenchBroom is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+TrenchBroom is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with TrenchBroom.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #import <Cocoa/Cocoa.h>
 
--- PickingHit.m
+++ PickingHit.m
@@ -1,10 +1,19 @@
-//
-//  PickingHit.m
-//  TrenchBroom
-//
-//  Created by Kristian Duske on 29.01.11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
+/*
+This file is part of TrenchBroom.
+
+TrenchBroom is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+TrenchBroom is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with TrenchBroom.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #import "PickingHit.h"
 
