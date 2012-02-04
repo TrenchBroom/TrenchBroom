a753710d4ca4b99b96a6efe9b5a42c471d1b6854
Kristian Duske <kristian.duske@gmail.com>
2012-02-04 23:26:30 +0100

    Updated GPL header.
    Signed-off-by: Kristian Duske <kristian.duske@gmail.com>

--- DoubleArrowFigure.m
+++ DoubleArrowFigure.m
@@ -1,10 +1,21 @@
-//
-//  DoubleArrowFigure.m
-//  TrenchBroom
-//
-//  Created by Kristian Duske on 17.12.11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
-//
+/*
+Copyright (C) 2010-2012 Kristian Duske
+
+This file is part of TrenchBroom.
+
+TrenchBroom is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+TrenchBroom is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with TrenchBroom.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #import "DoubleArrowFigure.h"
 
