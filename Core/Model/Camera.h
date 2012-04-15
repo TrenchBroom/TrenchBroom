ad31f8caa6700d368150e9d26abe26b3401ad539
Kristian Duske <kristian.duske@gmail.com>
2012-04-15 11:11:55 +0200

    Added glew headers.
    Signed-off-by: Kristian Duske <kristian.duske@gmail.com>

--- Core/Model/Camera.h
+++ Core/Model/Camera.h
@@ -1,18 +1,18 @@
 /*
  Copyright (C) 2010-2012 Kristian Duske
- 
+
  This file is part of TrenchBroom.
- 
+
  TrenchBroom is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
- 
+
  TrenchBroom is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
- 
+
  You should have received a copy of the GNU General Public License
  along with TrenchBroom.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -20,13 +20,12 @@
 #ifndef TrenchBroom_Camera_h
 #define TrenchBroom_Camera_h
 
-#include "VecMath.h"
-#include <OpenGL/gl.h>
-#include <OpenGL/glu.h>
+#include "Utilities/glew.h"
+#include "Utilities/VecMath.h"
 
 namespace TrenchBroom {
     namespace Controller {
-        
+
         class Camera {
         protected:
             Vec3f m_position;
@@ -48,7 +47,7 @@ namespace TrenchBroom {
             float fov() const;
             float near() const;
             float far() const;
-            
+
             const Vec3f defaultPoint();
             const Vec3f unproject(float x, float y, float depth) const;
             const Ray pickRay(float x, float y) const;
@@ -57,15 +56,15 @@ namespace TrenchBroom {
 
             float distanceTo(const Vec3f& point);
             float squaredDistanceTo(const Vec3f& point);
-            
+
             void moveTo(Vec3f position);
             void moveBy(float forward, float right, float up);
             void lookAt(Vec3f point, Vec3f up);
             void setDirection(Vec3f direction, Vec3f up);
-            
+
             void rotate(float yawAngle, float pitchAngle);
             void orbit(Vec3f center, float hAngle, float vAngle);
-            
+
             void setFov(float fov);
             void setNear(float near);
             void setFar(float far);
