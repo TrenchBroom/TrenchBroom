4eaafd473975f59b052475dc162228c15adb4702
Kristian Duske <kristian.duske@gmail.com>
2012-04-15 10:50:11 +0200

    Minor change, committing before adding sources from TrenchBroom Core.
    Signed-off-by: Kristian Duske <kristian.duske@gmail.com>

--- Windows/TrenchBroom/DocumentCanvas.cpp
+++ Windows/TrenchBroom/DocumentCanvas.cpp
@@ -20,7 +20,7 @@
 #include "DocumentCanvas.h"
 
 void DocumentCanvas::render() {
-SetCurrent();
+    SetCurrent();
     wxPaintDC(this);
     glClearColor(0.0, 0.0, 0.0, 0.0);
     glClear(GL_COLOR_BUFFER_BIT);
