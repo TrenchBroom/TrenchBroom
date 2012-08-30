ec6c561b2dc889cf5ca8a6809b29f3dd2ad06715
Kristian Duske <kristian.duske@gmail.com>
2012-08-30 12:05:04 +0200

    Moved mmapped_streambuf to mmapped_fstream.h
    Signed-off-by: Kristian Duske <kristian.duske@gmail.com>

--- Source/IO/mmapped_fstream_posix.h
+++ Source/IO/mmapped_fstream_posix.h
@@ -21,84 +21,9 @@
 #define __TrenchBroom__mmapped_fstream_posix__
 
 #include <cassert>
-#include <iostream>
-#include <functional>
 
 #include <fcntl.h>
 #include <sys/mman.h>
-
-class mmapped_streambuf : public std::streambuf {
-private:
-    const char* const begin_;
-    const char* const end_;
-    const char* current_;
-
-    std::streampos seekoff (std::streamoff off, std::ios_base::seekdir way, std::ios_base::openmode which = std::ios_base::in | std::ios_base::out) {
-        if (way == std::ios_base::beg) {
-            if (begin_ + off > end_)
-                return -1;
-            current_ = begin_ + off;
-            return current_ - begin_;
-        } else if (way == std::ios_base::cur) {
-            if (current_ + off > end_)
-                return -1;
-            current_ += off;
-            return current_ - begin_;
-        } else if (way == std::ios_base::end) {
-            if (end_ - off < begin_)
-                return -1;
-            current_ = end_ - off;
-            return current_ - begin_;
-        } else {
-            return -1;
-        }
-    }
-    
-    std::streampos seekpos (std::streampos sp, std::ios_base::openmode which = std::ios_base::in | std::ios_base::out) {
-        if (begin_ + sp > end_)
-            return - 1;
-        current_ = begin_ + sp;
-        return current_ - begin_;
-    }
-    
-    int_type underflow() {
-        if (current_ == end_)
-            return traits_type::eof();
-        
-        return traits_type::to_int_type(*current_);
-    }
-    
-    int_type uflow() {
-        if (current_ == end_)
-            return traits_type::eof();
-        
-        return traits_type::to_int_type(*current_++);
-    }
-    
-    int_type pbackfail(int_type ch) {
-        if (current_ == begin_ || (ch != traits_type::eof() && ch != current_[-1]))
-            return traits_type::eof();
-        
-        return traits_type::to_int_type(*--current_);
-    }
-    
-    std::streamsize showmanyc() {
-        assert(std::less_equal<const char *>()(current_, end_));
-        return end_ - current_;
-    }
-    
-    // copy ctor and assignment not implemented;
-    // copying not allowed
-    mmapped_streambuf(const mmapped_streambuf &);
-    mmapped_streambuf &operator= (const mmapped_streambuf &);
-public:
-    mmapped_streambuf(const char* begin, const char* end) :
-    begin_(begin),
-    end_(end),
-    current_(begin_) {
-        assert(std::less_equal<const char *>()(begin_, end_));
-    }
-};
 
 class mmapped_fstream_posix : public std::istream {
 private:
