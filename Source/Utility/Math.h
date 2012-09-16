6b547c78610838ae96f942c8b428c54aa39376d0
Kristian Duske <kristian.duske@gmail.com>
2012-09-16 11:47:49 +0200

    Fixed some compiler warnings.

--- Source/Utility/Math.h
+++ Source/Utility/Math.h
@@ -1,18 +1,18 @@
 /*
  Copyright (C) 2010-2012 Kristian Duske
- 
+
  This file is part of TrenchBroom.
- 
+
  TrenchBroom is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
- 
+
  TrenchBroom is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
- 
+
  You should have received a copy of the GNU General Public License
  along with TrenchBroom.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -21,6 +21,7 @@
 #define TrenchBroom_Math_h
 
 #include <cmath>
+#include <cstddef>
 #include <limits>
 
 namespace TrenchBroom {
@@ -28,7 +29,7 @@ namespace TrenchBroom {
         static const float AlmostZero = 0.001f;
         static const float PointStatusEpsilon = 0.01f;
         static const float Pi = 3.141592f;
-        
+
         inline bool isnan(float f) {
 #ifdef _MSC_VER
             return _isnan(f) != 0;
@@ -36,55 +37,55 @@ namespace TrenchBroom {
             return std::isnan(f);
 #endif
         }
-        
+
         inline float nan() {
             return std::numeric_limits<float>::quiet_NaN();
         }
-        
+
         inline float radians(float d) {
             return Pi * d / 180.0f;
         }
-        
+
         inline float degrees(float r) {
             return 180.0f * r / Pi;
         }
-        
+
         inline float round(float f) {
             return f > 0.0f ? floor(f + 0.5f) : ceil(f - 0.5f);
         }
-        
+
         inline float correct(float f) {
             return round(1000.0f * f) / 1000.0f;
         }
-        
+
         inline bool zero(float f) {
             return fabsf(f) <= AlmostZero;
         }
-        
+
         inline bool pos(float f) {
             return f > AlmostZero;
         }
-        
+
         inline bool neg(float f) {
             return f < -AlmostZero;
         }
-        
+
         inline bool eq(float f1, float f2) {
             return fabsf(f1 - f2) < AlmostZero;
         }
-        
+
         inline bool gt(float f1, float f2) {
             return f1 > f2 + AlmostZero;
         }
-        
+
         inline bool lt(float f1, float f2) {
             return f1 < f2 - AlmostZero;
         }
-        
+
         inline bool gte(float f1, float f2) {
             return !lt(f1, f2);
         }
-        
+
         inline bool lte(float f1, float f2) {
             return !gt(f1, f2);
         }
@@ -92,7 +93,7 @@ namespace TrenchBroom {
         inline size_t succ(size_t index, size_t count, size_t offset = 1) {
             return (index + offset) % count;
         }
-        
+
         inline size_t pred(size_t index, size_t count, size_t offset = 1) {
             return ((index + count) - (offset % count)) % count;
         }
