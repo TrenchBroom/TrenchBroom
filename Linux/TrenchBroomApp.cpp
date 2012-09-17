97cb82dc9a338d743271b4cf25ca0f9ef93086f7
Kristian Duske <kristian.duske@gmail.com>
2012-09-17 23:18:53 +0200

    Use MDI mode.
    Signed-off-by: Kristian Duske <kristian.duske@gmail.com>

--- Linux/TrenchBroomApp.cpp
+++ Linux/TrenchBroomApp.cpp
@@ -1,18 +1,18 @@
 /*
  Copyright (C) 2010-2012 Kristian Duske
- 
+
  This file is part of TrenchBroom.
- 
+
  TrenchBroom is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
- 
+
  TrenchBroom is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
- 
+
  You should have received a copy of the GNU General Public License
  along with TrenchBroom.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -41,10 +41,10 @@ wxMenu* TrenchBroomApp::CreateViewMenu() {
 bool TrenchBroomApp::OnInit() {
     // set the locale to US so that we can parse floats property
     std::setlocale(LC_ALL, "us");
-    
+
 	if (AbstractApp::OnInit()) {
-		SetExitOnFrameDelete(true);
-		m_docManager->SetUseSDI(true);
+		SetExitOnFrameDelete(false);
+		m_docManager->SetUseSDI(false);
 		m_docManager->CreateNewDocument();
 		return true;
 	}
