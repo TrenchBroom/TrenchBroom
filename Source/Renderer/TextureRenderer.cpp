7058884dc5e8541b66d09eb85b91ec2dbaff0dd0
Kristian Duske <kristian.duske@gmail.com>
2013-01-21 21:51:53 +0100

    Use GL_NEAREST for texture minification and magnification filtering.

--- Source/Renderer/TextureRenderer.cpp
+++ Source/Renderer/TextureRenderer.cpp
@@ -74,8 +74,8 @@ namespace TrenchBroom {
                 if (m_textureBuffer != NULL) {
                     glGenTextures(1, &m_textureId);
                     glBindTexture(GL_TEXTURE_2D, m_textureId);
-                    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-                    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+                    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+                    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
                     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
                     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
                     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, static_cast<GLsizei>(m_width), static_cast<GLsizei>(m_height), 0, GL_RGB, GL_UNSIGNED_BYTE, m_textureBuffer);
