224c9a7301df13e926d0044767d31605fd00bce1
Kristian Duske <kristian.duske@gmail.com>
2013-02-06 12:13:21 +0100

    Allow double-click + drag when Alt is pressed to split an edge or face and move the new vertex vertically.

--- Source/Controller/MoveVerticesTool.cpp
+++ Source/Controller/MoveVerticesTool.cpp
@@ -227,7 +227,9 @@ namespace TrenchBroom {
 
         bool MoveVerticesTool::handleMouseUp(InputState& inputState) {
             if (inputState.mouseButtons() != MouseButtons::MBLeft ||
-                (inputState.modifierKeys() != ModifierKeys::MKNone && inputState.modifierKeys() != ModifierKeys::MKCtrlCmd))
+                (inputState.modifierKeys() != ModifierKeys::MKNone &&
+                 inputState.modifierKeys() != ModifierKeys::MKCtrlCmd &&
+                 inputState.modifierKeys() != ModifierKeys::MKAlt))
                 return false;
             
             Model::VertexHandleHit* hit = static_cast<Model::VertexHandleHit*>(inputState.pickResult().first(Model::HitType::VertexHandleHit | Model::HitType::EdgeHandleHit | Model::HitType::FaceHandleHit, true, view().filter()));
@@ -246,7 +248,8 @@ namespace TrenchBroom {
 
         bool MoveVerticesTool::handleMouseDClick(InputState& inputState) {
             if (inputState.mouseButtons() != MouseButtons::MBLeft ||
-                inputState.modifierKeys() != ModifierKeys::MKNone)
+                (inputState.modifierKeys() != ModifierKeys::MKNone &&
+                 inputState.modifierKeys() != ModifierKeys::MKAlt))
                 return false;
 
             Model::VertexHandleHit* hit = static_cast<Model::VertexHandleHit*>(inputState.pickResult().first(Model::HitType::EdgeHandleHit | Model::HitType::FaceHandleHit, true, view().filter()));
