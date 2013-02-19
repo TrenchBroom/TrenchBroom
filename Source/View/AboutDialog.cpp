7932e0867e9adda282f1cd51555f476125bf3076
Kristian Duske <kristian.duske@gmail.com>
2013-02-19 18:18:28 +0100

    New about dialog.
    Signed-off-by: Kristian Duske <kristian.duske@gmail.com>

--- Source/View/AboutDialog.cpp
+++ Source/View/AboutDialog.cpp
@@ -22,8 +22,11 @@
 #include "IO/FileManager.h"
 #include "Utility/String.h"
 
-#include <wx/html/htmlwin.h>
-#include <wx/sizer.h>
+#include <wx/bitmap.h>
+#include <wx/gbsizer.h>
+#include <wx/statbmp.h>
+#include <wx/statline.h>
+#include <wx/stattext.h>
 
 namespace TrenchBroom {
     namespace View {
@@ -31,18 +34,50 @@ namespace TrenchBroom {
         wxDialog(parent, wxID_ANY, wxT("About")) {
             IO::FileManager fileManager;
             
-            const String helpPath = fileManager.appendPath(fileManager.resourceDirectory(), "About");
-            const String aboutPath = fileManager.appendPath(helpPath, "about.html");
+            wxBitmap icon(fileManager.appendPath(fileManager.resourceDirectory(), "Icon.png"), wxBITMAP_TYPE_PNG);
             
-            wxHtmlWindow* htmlWindow = new wxHtmlWindow(this);
-            htmlWindow->LoadFile(wxFileName(aboutPath));
+            wxStaticBitmap* appIcon = new wxStaticBitmap(this, wxID_ANY, icon);
+            wxStaticLine* appLine = new wxStaticLine(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxLI_HORIZONTAL);
+            wxStaticText* appName = new wxStaticText(this, wxID_ANY, wxT("TrenchBroom"));
+            appName->SetFont(appName->GetFont().Larger().Larger().Larger().Larger().Bold());
             
-            wxSizer* sizer = new wxBoxSizer(wxVERTICAL);
-            sizer->Add(htmlWindow, 1, wxEXPAND);
+            wxStaticText* appClaim = new wxStaticText(this, wxID_ANY, wxT("A Modern Level Editor for Quake"));
+            
+            wxStaticText* devHeader = new wxStaticText(this, wxID_ANY, wxT("Development"));
+            devHeader->SetFont(devHeader->GetFont().Bold());
+            wxStaticText* devText = new wxStaticText(this, wxID_ANY, wxT("Kristian Duske"));
+            
+            wxStaticText* contrHeader = new wxStaticText(this, wxID_ANY, wxT("Contributions"));
+            contrHeader->SetFont(contrHeader->GetFont().Bold());
+            wxSizer* contrText = CreateTextSizer(wxT("Corey Jones (feedback, testing, documentation)\nAndré König (feedback, testing)\nHannes Kröger (testing)\nMorgan Allen (testing)\nForest Hale (fov code)"));
+            
+            wxSizer* copyright = CreateTextSizer(wxT("Copyright 2010-2013 Kristian Duske\nQuake is a registered trademark of id Software"));
+
+            wxGridBagSizer* sizer = new wxGridBagSizer();
+
+            int row = 0;
+            sizer->Add(0, 10, wxGBPosition(row++, 1));
+            sizer->AddGrowableRow(static_cast<size_t>(row - 1));
+            sizer->Add(appName, wxGBPosition(row++, 1));
+            sizer->Add(appLine, wxGBPosition(row++, 1), wxDefaultSpan, wxEXPAND);
+            sizer->Add(appClaim, wxGBPosition(row++, 1));
+            sizer->Add(0, 20, wxGBPosition(row++, 1));
+            sizer->Add(devHeader, wxGBPosition(row++, 1));
+            sizer->Add(devText, wxGBPosition(row++, 1));
+            sizer->Add(0, 20, wxGBPosition(row++, 1));
+            sizer->Add(contrHeader, wxGBPosition(row++, 1));
+            sizer->Add(contrText, wxGBPosition(row++, 1));
+            sizer->Add(0, 20, wxGBPosition(row++, 1));
+            sizer->Add(copyright, wxGBPosition(row++, 1));
+            sizer->Add(0, 10, wxGBPosition(row++, 1));
+            sizer->AddGrowableRow(static_cast<size_t>(row - 1));
+            sizer->Add(appIcon, wxGBPosition(0, 0), wxGBSpan(row, 1), wxALIGN_CENTER);
             
             SetSizer(sizer);
             SetSize(650, 360);
             CenterOnParent();
+            
+            SetBackgroundColour(*wxWHITE);
         }
     }
 }
