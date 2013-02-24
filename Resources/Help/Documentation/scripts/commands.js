85ecd434cfa9adbee37d8e4eb0320fa09c16aae1
Corey Jones <corey.michel.jones@gmail.com>
2013-02-24 18:18:55 -0500

    Fixed missing menu commands for Mac

--- Resources/Help/Documentation/scripts/commands.js
+++ Resources/Help/Documentation/scripts/commands.js
@@ -58,6 +58,8 @@ menu_commands["Mac"]["edit_actions_correct_verts"]		= "Edit &raquo; Actions &raq
 menu_commands["Mac"]["edit_actions_snap_verts"]			= "Edit &raquo; Actions &raquo; Snap Vertices";
 
 menu_commands["Mac"]["view_center_on_selection"]		= "View &raquo; Camera &raquo; Center on Selection - &#x2325;C";
+menu_commands["Mac"]["view_camera_move_to_next_pt"]	= "View &raquo; Camera &raquo; Move Camera to Next Point, &#x2325; +";
+menu_commands["Mac"]["view_camera_move_to_prev_pt"]	= "View &raquo; Camera &raquo; Move Camera to Previous Point, &#x2325; -";
 
 
 
@@ -144,7 +146,7 @@ if (navigator.platform.indexOf("Win")!=-1) platform="Windows";
 if (navigator.platform.indexOf("Mac")!=-1) platform="Mac";
 if (navigator.platform.indexOf("X11")!=-1) platform="Linux";
 if (navigator.platform.indexOf("Linux")!=-1) OSName="Linux";
-//var platform = "Mac"; //debug
+//platform = "Mac"; //debug
 
 function print_menu_command(name) {
 	document.write("<b>" + menu_commands[platform][name] + "</b>");
