7d5fc1466af6c84aea2c173e72a6b29d4e75ae87
Kristian Duske <kristian.duske@gmail.com>
2013-05-28 23:34:09 +0200

    Removed Linux project and updated CMakeLists.
    Signed-off-by: Kristian Duske <kristian.duske@gmail.com>

--- Linux/IncBuildNo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-version=$(<"../Source/Version")
-buildNo=$(<"BuildNo")
-buildNo=$(($buildNo + 1))
-echo $buildNo > "BuildNo"
-echo "#define VERSIONSTR" "\"$version Build $buildNo\0\"" > "Version.h"
-
