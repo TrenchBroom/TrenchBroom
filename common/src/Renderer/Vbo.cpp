b997b1242e9d3382a366837f8f22ce37bcbb6bfc
Kristian Duske <kristian.duske@gmail.com>
2014-05-21 22:33:06 +0200

    Minor change.
    Signed-off-by: Kristian Duske <kristian.duske@gmail.com>

--- common/src/Renderer/Vbo.cpp
+++ common/src/Renderer/Vbo.cpp
@@ -168,7 +168,6 @@ namespace TrenchBroom {
                 activate();
 #ifdef __APPLE__
             // fixes a crash on Mac OS X where a buffer could not be mapped after another windows was closed
-            // the error actually still happens under other circumstances (open map document, close the window and reopen the same document)
             glFinishObjectAPPLE(GL_BUFFER_OBJECT_APPLE, static_cast<GLint>(m_vboId));
 #endif
             m_buffer = reinterpret_cast<unsigned char *>(glMapBuffer(m_type, GL_WRITE_ONLY));
