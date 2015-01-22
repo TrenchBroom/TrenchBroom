cbbcc500f3daca17978ee197f314cfd3e90de507
Kristian Duske <kristian.duske@gmail.com>
2015-01-22 19:24:55 +0100

    Extend wxWidgets finding on Windows.

--- cmake/Modules/FindwxWidgetsTB.cmake
+++ cmake/Modules/FindwxWidgetsTB.cmake
@@ -432,6 +432,12 @@ if(wxWidgets_FIND_STYLE STREQUAL "win32")
       D:/
       ENV ProgramFiles
     PATH_SUFFIXES
+      wxWidgets-3.0.6
+      wxWidgets-3.0.5
+      wxWidgets-3.0.4
+      wxWidgets-3.0.3
+      wxWidgets-3.0.2
+      wxWidgets-3.0.1
       wxWidgets-3.0.0
       wxWidgets-2.9.5
       wxWidgets-2.9.4
