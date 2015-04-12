060a43e8ccdcb8ac737b8c6e39a5bff3f19be0f1
Kristian Duske <kristian.duske@gmail.com>
2015-04-12 11:45:45 +0200

    Fix a minor bug.

--- common/src/Model/BrushContentType.cpp
+++ common/src/Model/BrushContentType.cpp
@@ -25,7 +25,7 @@
 
 namespace TrenchBroom {
     namespace Model {
-        const BrushContentType::List BrushContentType::EmptyList = BrushContentType::EmptyList;
+        const BrushContentType::List BrushContentType::EmptyList = BrushContentType::List();
         
         BrushContentType::BrushContentType(const String& name, const bool transparent, const FlagType flagValue, BrushContentTypeEvaluator* evaluator) :
         m_name(name),
