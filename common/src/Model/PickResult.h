2ebf05d4189f297a3623bc5083e91ab20256f559
Kristian Duske <kristian.duske@gmail.com>
2015-08-10 10:15:27 +0200

    Add missing import.

--- common/src/Model/PickResult.h
+++ common/src/Model/PickResult.h
@@ -1,18 +1,18 @@
 /*
  Copyright (C) 2010-2014 Kristian Duske
- 
+
  This file is part of TrenchBroom.
- 
+
  TrenchBroom is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
- 
+
  TrenchBroom is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
- 
+
  You should have received a copy of the GNU General Public License
  along with TrenchBroom. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -21,6 +21,7 @@
 #define __TrenchBroom__PickResult__
 
 #include "MathUtils.h"
+#include "Model/CompareHits.h"
 #include "Model/Hit.h"
 #include "Model/HitQuery.h"
 
@@ -29,7 +30,7 @@ namespace TrenchBroom {
         class CompareHits;
         class HitFilter;
         class EditorContext;
-        
+
         class PickResult {
         public:
             typedef std::tr1::shared_ptr<CompareHits> ComparePtr;
@@ -42,17 +43,17 @@ namespace TrenchBroom {
             PickResult(const EditorContext& editorContext, CompareHits* compare) :
             m_editorContext(&editorContext),
             m_compare(compare) {}
-            
+
             PickResult();
-            
+
             static PickResult byDistance(const EditorContext& editorContext);
             static PickResult bySize(const EditorContext& editorContext, Math::Axis::Type axis);
-            
+
             bool empty() const;
             size_t size() const;
-            
+
             void addHit(const Hit& hit);
-            
+
             const Hit::List& all() const;
             HitQuery query() const;
         };
