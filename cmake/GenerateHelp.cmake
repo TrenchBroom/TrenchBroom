e0f6743d14fd95db40d320c8c4a269fa1e7f9a10
Kristian Duske <kristian.duske@gmail.com>
2015-11-16 12:11:41 +0100

    Some debug output.

--- cmake/GenerateHelp.cmake
+++ cmake/GenerateHelp.cmake
@@ -2,6 +2,11 @@
 SET(DOC_DIR "${CMAKE_SOURCE_DIR}/app/resources/documentation")
 SET(DOC_HELP_SOURCE_DIR "${DOC_DIR}/help")
 SET(DOC_HELP_TARGET_DIR "${CMAKE_CURRENT_BINARY_DIR}/help")
+
+MESSAGE(STATUS "Pandoc Template: ${DOC_HELP_SOURCE_DIR}/template.html")
+MESSAGE(STATUS "Pandoc Input   : ${DOC_HELP_SOURCE_DIR}/index.md")
+MESSAGE(STATUS "Pandoc Output  : ${DOC_HELP_TARGET_DIR}/index.html")
+
 ADD_CUSTOM_TARGET(GenerateHelp
     COMMAND ${CMAKE_COMMAND} -E make_directory "${DOC_HELP_TARGET_DIR}"
     COMMAND pandoc -s --toc --toc-depth=2 --template "${DOC_HELP_SOURCE_DIR}/template.html" -o "${DOC_HELP_TARGET_DIR}/index.html" "${DOC_HELP_SOURCE_DIR}/index.md"
