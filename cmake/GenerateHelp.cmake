3320fa2439746472bcfa5b8bbdcae4d755b7ebb5
Kristian Duske <kristian.duske@gmail.com>
2015-11-16 12:05:04 +0100

    Fix some problems with path that contain spaces during help generation.

--- cmake/GenerateHelp.cmake
+++ cmake/GenerateHelp.cmake
@@ -4,6 +4,6 @@ SET(DOC_HELP_SOURCE_DIR "${DOC_DIR}/help")
 SET(DOC_HELP_TARGET_DIR "${CMAKE_CURRENT_BINARY_DIR}/help")
 ADD_CUSTOM_TARGET(GenerateHelp
     COMMAND ${CMAKE_COMMAND} -E make_directory "${DOC_HELP_TARGET_DIR}"
-    COMMAND pandoc -s --toc --toc-depth=2 --template ${DOC_HELP_SOURCE_DIR}/template.html -o ${DOC_HELP_TARGET_DIR}/index.html ${DOC_HELP_SOURCE_DIR}/index.md
+    COMMAND pandoc -s --toc --toc-depth=2 --template "${DOC_HELP_SOURCE_DIR}/template.html" -o "${DOC_HELP_TARGET_DIR}/index.html" "${DOC_HELP_SOURCE_DIR}/index.md"
     COMMAND ${CMAKE_COMMAND} -DINPUT="${DOC_HELP_TARGET_DIR}/index.html" -DOUTPUT="${DOC_HELP_TARGET_DIR}/index.html" -P "${CMAKE_SOURCE_DIR}/cmake/TransformKeyboardShortcuts.cmake"
 )
