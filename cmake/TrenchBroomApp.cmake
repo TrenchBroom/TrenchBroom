578fe2308cd9581b4b2268f1bc8438a4c89d56f6
Kristian Duske <kristian.duske@gmail.com>
2016-03-22 22:09:44 +0100

    Revert "Revert erraneous commits."
    This reverts commit ddae1b28e9f49bc6e019ede10d89d87f2fc62411.

--- cmake/TrenchBroomApp.cmake
+++ cmake/TrenchBroomApp.cmake
@@ -149,32 +149,32 @@ ENDIF()
 IF(WIN32 OR ${CMAKE_SYSTEM_NAME} MATCHES "Linux")
 	# Copy button images to resources directory
 	ADD_CUSTOM_COMMAND(TARGET TrenchBroom POST_BUILD
-        COMMAND ${CMAKE_COMMAND} -E copy_directory "${APP_DIR}/resources/graphics/images" "$<TARGET_FILE_DIR:TrenchBroom>/Resources/images"
+        COMMAND ${CMAKE_COMMAND} -E copy_directory "${APP_DIR}/resources/graphics/images" "$<TARGET_FILE_DIR:TrenchBroom>/images"
 	)
 
     # Copy fonts to resources directory
     ADD_CUSTOM_COMMAND(TARGET TrenchBroom POST_BUILD
-        COMMAND ${CMAKE_COMMAND} -E copy_directory "${APP_DIR}/resources/fonts" "$<TARGET_FILE_DIR:TrenchBroom>/Resources/fonts"
+        COMMAND ${CMAKE_COMMAND} -E copy_directory "${APP_DIR}/resources/fonts" "$<TARGET_FILE_DIR:TrenchBroom>/fonts"
     )
 
 	# Copy game files to resources directory
 	ADD_CUSTOM_COMMAND(TARGET TrenchBroom POST_BUILD
-        COMMAND ${CMAKE_COMMAND} -E copy_directory "${APP_DIR}/resources/games/" "$<TARGET_FILE_DIR:TrenchBroom>/Resources/games"
+        COMMAND ${CMAKE_COMMAND} -E copy_directory "${APP_DIR}/resources/games/" "$<TARGET_FILE_DIR:TrenchBroom>/games"
 	)
 
 	# Copy shader files to resources directory
 	ADD_CUSTOM_COMMAND(TARGET TrenchBroom POST_BUILD
-        COMMAND ${CMAKE_COMMAND} -E copy_directory "${APP_DIR}/resources/shader" "$<TARGET_FILE_DIR:TrenchBroom>/Resources/shader"
+        COMMAND ${CMAKE_COMMAND} -E copy_directory "${APP_DIR}/resources/shader" "$<TARGET_FILE_DIR:TrenchBroom>/shader"
 	)
 
     # Copy help files to resource directory
 	ADD_CUSTOM_COMMAND(TARGET TrenchBroom POST_BUILD
-		COMMAND ${CMAKE_COMMAND} -E make_directory "$<TARGET_FILE_DIR:TrenchBroom>/Resources/help/"
+		COMMAND ${CMAKE_COMMAND} -E make_directory "$<TARGET_FILE_DIR:TrenchBroom>/help/"
 	)
 
     FOREACH(HELP_FILE ${DOC_HELP_TARGET_FILES})
         ADD_CUSTOM_COMMAND(TARGET TrenchBroom POST_BUILD
-            COMMAND ${CMAKE_COMMAND} -E copy ${HELP_FILE} "$<TARGET_FILE_DIR:TrenchBroom>/Resources/help/"
+            COMMAND ${CMAKE_COMMAND} -E copy ${HELP_FILE} "$<TARGET_FILE_DIR:TrenchBroom>/help/"
         )
     ENDFOREACH(HELP_FILE)
 ENDIF()
@@ -243,12 +243,12 @@ IF(WIN32)
         DESTINATION . COMPONENT TrenchBroom)
     INSTALL(FILES
         ${DOC_HELP_TARGET_FILES}
-        DESTINATION Resources/help COMPONENT TrenchBroom)
+        DESTINATION help COMPONENT TrenchBroom)
     INSTALL(DIRECTORY
         "${APP_DIR}/resources/graphics/images"
         "${APP_DIR}/resources/games"
         "${APP_DIR}/resources/shader"
-        DESTINATION Resources COMPONENT TrenchBroom)
+        DESTINATION . COMPONENT TrenchBroom)
     SET(CPACK_GENERATOR "7Z")
     SET(CPACK_INCLUDE_TOPLEVEL_DIRECTORY FALSE)
 ELSEIF(APPLE)
