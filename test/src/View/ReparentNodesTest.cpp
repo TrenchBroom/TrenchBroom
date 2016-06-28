67047ba302a671cd7ede66e143b453e35e0b63b4
Kristian Duske <kristian.duske@gmail.com>
2016-06-28 21:34:37 +0200

    Add test cases for removing nodes.

--- test/src/View/ReparentNodesTest.cpp
+++ test/src/View/ReparentNodesTest.cpp
@@ -19,147 +19,133 @@
 
 #include <gtest/gtest.h>
 
-#include "MathUtils.h"
 #include "Model/Brush.h"
-#include "Model/BrushBuilder.h"
 #include "Model/Entity.h"
 #include "Model/Group.h"
 #include "Model/Layer.h"
-#include "Model/MapFormat.h"
-#include "Model/TestGame.h"
 #include "Model/World.h"
+#include "View/MapDocumentTest.h"
 #include "View/MapDocument.h"
-#include "View/MapDocumentCommandFacade.h"
 
 namespace TrenchBroom {
     namespace View {
-        class ReparentNodesTest : public ::testing::Test {
-        protected:
-            MapDocumentSPtr m_document;
-            
-            void SetUp() {
-                m_document = MapDocumentCommandFacade::newMapDocument();
-                m_document->newDocument(Model::MapFormat::Standard, BBox3(8192.0), Model::GamePtr(new Model::TestGame()));
-            }
-        };
+        class ReparentNodesTest : public MapDocumentTest {};
         
         TEST_F(ReparentNodesTest, reparentLayerToLayer) {
-            Model::Layer* layer1 = new Model::Layer("Layer 1", m_document->worldBounds());
-            m_document->addNode(layer1, m_document->world());
+            Model::Layer* layer1 = new Model::Layer("Layer 1", document->worldBounds());
+            document->addNode(layer1, document->world());
             
-            Model::Layer* layer2 = new Model::Layer("Layer 2", m_document->worldBounds());
-            m_document->addNode(layer2, m_document->world());
+            Model::Layer* layer2 = new Model::Layer("Layer 2", document->worldBounds());
+            document->addNode(layer2, document->world());
             
-            ASSERT_FALSE(m_document->reparentNodes(layer2, Model::NodeList(1, layer1)));
+            ASSERT_FALSE(document->reparentNodes(layer2, Model::NodeList(1, layer1)));
         }
         
         TEST_F(ReparentNodesTest, reparentBetweenLayers) {
-            Model::Layer* oldParent = new Model::Layer("Layer 1", m_document->worldBounds());
-            m_document->addNode(oldParent, m_document->world());
+            Model::Layer* oldParent = new Model::Layer("Layer 1", document->worldBounds());
+            document->addNode(oldParent, document->world());
             
-            Model::Layer* newParent = new Model::Layer("Layer 2", m_document->worldBounds());
-            m_document->addNode(newParent, m_document->world());
+            Model::Layer* newParent = new Model::Layer("Layer 2", document->worldBounds());
+            document->addNode(newParent, document->world());
             
             Model::Entity* entity = new Model::Entity();
-            m_document->addNode(entity, oldParent);
+            document->addNode(entity, oldParent);
             
             assert(entity->parent() == oldParent);
-            ASSERT_TRUE(m_document->reparentNodes(newParent, Model::NodeList(1, entity)));
+            ASSERT_TRUE(document->reparentNodes(newParent, Model::NodeList(1, entity)));
             ASSERT_EQ(newParent, entity->parent());
             
-            m_document->undoLastCommand();
+            document->undoLastCommand();
             ASSERT_EQ(oldParent, entity->parent());
         }
         
         TEST_F(ReparentNodesTest, reparentGroupToItself) {
             Model::Group* group = new Model::Group("Group");
-            m_document->addNode(group, m_document->currentParent());
+            document->addNode(group, document->currentParent());
             
-            ASSERT_FALSE(m_document->reparentNodes(group, Model::NodeList(1, group)));
+            ASSERT_FALSE(document->reparentNodes(group, Model::NodeList(1, group)));
         }
         
         TEST_F(ReparentNodesTest, reparentGroupToChild) {
             Model::Group* outer = new Model::Group("Outer");
-            m_document->addNode(outer, m_document->currentParent());
+            document->addNode(outer, document->currentParent());
             
             Model::Group* inner = new Model::Group("Inner");
-            m_document->addNode(inner, outer);
+            document->addNode(inner, outer);
             
-            ASSERT_FALSE(m_document->reparentNodes(inner, Model::NodeList(1, outer)));
+            ASSERT_FALSE(document->reparentNodes(inner, Model::NodeList(1, outer)));
         }
         
         TEST_F(ReparentNodesTest, removeEmptyGroup) {
             Model::Group* group = new Model::Group("Group");
-            m_document->addNode(group, m_document->currentParent());
+            document->addNode(group, document->currentParent());
 
             Model::Entity* entity = new Model::Entity();
-            m_document->addNode(entity, group);
+            document->addNode(entity, group);
             
-            ASSERT_TRUE(m_document->reparentNodes(m_document->currentParent(), Model::NodeList(1, entity)));
-            ASSERT_EQ(m_document->currentParent(), entity->parent());
+            ASSERT_TRUE(document->reparentNodes(document->currentParent(), Model::NodeList(1, entity)));
+            ASSERT_EQ(document->currentParent(), entity->parent());
             ASSERT_TRUE(group->parent() == NULL);
             
-            m_document->undoLastCommand();
-            ASSERT_EQ(m_document->currentParent(), group->parent());
+            document->undoLastCommand();
+            ASSERT_EQ(document->currentParent(), group->parent());
             ASSERT_EQ(group, entity->parent());
         }
         
         TEST_F(ReparentNodesTest, recursivelyRemoveEmptyGroups) {
             Model::Group* outer = new Model::Group("Outer");
-            m_document->addNode(outer, m_document->currentParent());
+            document->addNode(outer, document->currentParent());
             
             Model::Group* inner = new Model::Group("Inner");
-            m_document->addNode(inner, outer);
+            document->addNode(inner, outer);
             
             Model::Entity* entity = new Model::Entity();
-            m_document->addNode(entity, inner);
+            document->addNode(entity, inner);
             
-            ASSERT_TRUE(m_document->reparentNodes(m_document->currentParent(), Model::NodeList(1, entity)));
-            ASSERT_EQ(m_document->currentParent(), entity->parent());
+            ASSERT_TRUE(document->reparentNodes(document->currentParent(), Model::NodeList(1, entity)));
+            ASSERT_EQ(document->currentParent(), entity->parent());
             ASSERT_TRUE(inner->parent() == NULL);
             ASSERT_TRUE(outer->parent() == NULL);
             
-            m_document->undoLastCommand();
-            ASSERT_EQ(m_document->currentParent(), outer->parent());
+            document->undoLastCommand();
+            ASSERT_EQ(document->currentParent(), outer->parent());
             ASSERT_EQ(outer, inner->parent());
             ASSERT_EQ(inner, entity->parent());
         }
         
         TEST_F(ReparentNodesTest, removeEmptyEntity) {
             Model::Entity* entity = new Model::Entity();
-            m_document->addNode(entity, m_document->currentParent());
+            document->addNode(entity, document->currentParent());
             
-            Model::BrushBuilder builder(m_document->world(), m_document->worldBounds());
-            Model::Brush* brush = builder.createCube(32.0, "texture");
-            m_document->addNode(brush, entity);
+            Model::Brush* brush = createBrush();
+            document->addNode(brush, entity);
             
-            ASSERT_TRUE(m_document->reparentNodes(m_document->currentParent(), Model::NodeList(1, brush)));
-            ASSERT_EQ(m_document->currentParent(), brush->parent());
+            ASSERT_TRUE(document->reparentNodes(document->currentParent(), Model::NodeList(1, brush)));
+            ASSERT_EQ(document->currentParent(), brush->parent());
             ASSERT_TRUE(entity->parent() == NULL);
             
-            m_document->undoLastCommand();
-            ASSERT_EQ(m_document->currentParent(), entity->parent());
+            document->undoLastCommand();
+            ASSERT_EQ(document->currentParent(), entity->parent());
             ASSERT_EQ(entity, brush->parent());
         }
         
         TEST_F(ReparentNodesTest, removeEmptyGroupAndEntity) {
             Model::Group* group = new Model::Group("Group");
-            m_document->addNode(group, m_document->currentParent());
+            document->addNode(group, document->currentParent());
             
             Model::Entity* entity = new Model::Entity();
-            m_document->addNode(entity, group);
+            document->addNode(entity, group);
             
-            Model::BrushBuilder builder(m_document->world(), m_document->worldBounds());
-            Model::Brush* brush = builder.createCube(32.0, "texture");
-            m_document->addNode(brush, entity);
+            Model::Brush* brush = createBrush();
+            document->addNode(brush, entity);
             
-            ASSERT_TRUE(m_document->reparentNodes(m_document->currentParent(), Model::NodeList(1, brush)));
-            ASSERT_EQ(m_document->currentParent(), brush->parent());
+            ASSERT_TRUE(document->reparentNodes(document->currentParent(), Model::NodeList(1, brush)));
+            ASSERT_EQ(document->currentParent(), brush->parent());
             ASSERT_TRUE(group->parent() == NULL);
             ASSERT_TRUE(entity->parent() == NULL);
             
-            m_document->undoLastCommand();
-            ASSERT_EQ(m_document->currentParent(), group->parent());
+            document->undoLastCommand();
+            ASSERT_EQ(document->currentParent(), group->parent());
             ASSERT_EQ(group, entity->parent());
             ASSERT_EQ(entity, brush->parent());
         }
