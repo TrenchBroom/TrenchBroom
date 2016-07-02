6103a390593b302251a2da7585f465e0e981c7bb
Kristian Duske <kristian.duske@gmail.com>
2016-07-02 14:40:45 +0200

    Remove border around radio groups.

--- common/src/View/PopupWindow.cpp
+++ common/src/View/PopupWindow.cpp
@@ -1,18 +1,18 @@
 /*
  Copyright (C) 2010-2016 Kristian Duske
- 
+
  This file is part of TrenchBroom.
- 
+
  TrenchBroom is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
- 
+
  TrenchBroom is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
- 
+
  You should have received a copy of the GNU General Public License
  along with TrenchBroom. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -28,11 +28,11 @@ namespace TrenchBroom {
         PopupWindow::PopupWindow(wxWindow* parent, wxWindowID id, const wxString& caption, const wxPoint& pos, const wxSize& size, const long style, const wxString& name) {
             Create(parent, id, caption, pos, size, style, name);
         }
-        
+
         bool PopupWindow::Create(wxWindow* parent, wxWindowID id, const wxString& caption, const wxPoint& pos, const wxSize& size, const long style, const wxString& name) {
             if (!wxFrame::Create(parent, id, caption, pos, size, style, name))
                 return false;
-            
+
             Bind(wxEVT_ACTIVATE, &PopupWindow::OnActivate, this);
             Bind(wxEVT_ACTIVATE_APP, &PopupWindow::OnActivate, this);
             return true;
@@ -42,7 +42,7 @@ namespace TrenchBroom {
             // determine the position and size of the screen we clamp the popup to
             wxPoint posScreen;
             wxSize sizeScreen;
-            
+
             const int displayNum = wxDisplay::GetFromPoint(origin);
             if ( displayNum != wxNOT_FOUND )
             {
@@ -56,10 +56,10 @@ namespace TrenchBroom {
                 posScreen = wxPoint(0, 0);
                 sizeScreen = wxGetDisplaySize();
             }
-            
-            
+
+
             const wxSize sizeSelf = GetSize();
-            
+
             // is there enough space to put the popup below the window (where we put it
             // by default)?
             wxCoord y = origin.y + size.y;
@@ -73,10 +73,10 @@ namespace TrenchBroom {
                 }
                 //else: not enough space below nor above, leave below
             }
-            
+
             // now check left/right too
             wxCoord x = origin.x;
-            
+
             if ( wxTheApp->GetLayoutDirection() == wxLayout_RightToLeft )
             {
                 // shift the window to the left instead of the right.
@@ -88,8 +88,8 @@ namespace TrenchBroom {
                 x += size.x;
                 x -= sizeSelf.x;
             }
-            
-            
+
+
             if ( x + sizeSelf.x > posScreen.x + sizeScreen.x )
             {
                 // check if there is enough space to the left
@@ -100,21 +100,23 @@ namespace TrenchBroom {
                 }
                 //else: not enough space there neither, leave in default position
             }
-            
+
             Move(x, y, wxSIZE_NO_ADJUSTMENTS);
         }
-        
+
         void PopupWindow::Popup() {
             Show();
         }
-        
+
         void PopupWindow::Dismiss() {
             Hide();
         }
 
         void PopupWindow::OnActivate(wxActivateEvent& event) {
-            if (!event.GetActive())
-                Dismiss();
+            if (!event.GetActive()) {
+                if (!FindFocus()->IsDescendant(this))
+                    Dismiss();
+            }
         }
     }
 }
--- common/src/View/RadioGroup.cpp
+++ common/src/View/RadioGroup.cpp
@@ -1,18 +1,18 @@
 /*
  Copyright (C) 2010-2016 Kristian Duske
- 
+
  This file is part of TrenchBroom.
- 
+
  TrenchBroom is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
- 
+
  TrenchBroom is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
- 
+
  You should have received a copy of the GNU General Public License
  along with TrenchBroom. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -35,9 +35,9 @@ namespace TrenchBroom {
         wxControl() {
             Create(parent, id, pos, size, n, choices);
         }
-        
+
         bool RadioGroup::Create(wxWindow* parent, const wxWindowID id, const wxPoint& pos, const wxSize& size, const size_t n, const wxString choices[]) {
-            if (!wxControl::Create(parent, id, pos, size))
+            if (!wxControl::Create(parent, id, pos, size, wxBORDER_NONE))
                 return false;
 
             m_buttons.reserve(n);
@@ -53,25 +53,25 @@ namespace TrenchBroom {
                 }
                 SetSizer(sizer);
             }
-            
+
             return true;
         }
 
         unsigned int RadioGroup::GetCount() const {
             return static_cast<unsigned int>(m_buttons.size());
         }
-        
+
         wxString RadioGroup::GetString(const unsigned int index) const {
             if (index > GetCount())
                 return "";
             return m_buttons[static_cast<size_t>(index)]->GetLabel();
         }
-        
+
         void RadioGroup::SetString(const unsigned int index, const wxString& str) {
             assert(index < GetCount());
             m_buttons[static_cast<size_t>(index)]->SetLabel(str);
         }
-        
+
         int RadioGroup::FindString(const wxString& str, const bool caseSensitive) const {
             for (size_t i = 0; i < m_buttons.size(); ++i) {
                 if (m_buttons[i]->GetLabel().IsSameAs(str, caseSensitive))
@@ -79,21 +79,21 @@ namespace TrenchBroom {
             }
             return wxNOT_FOUND;
         }
-        
+
         void RadioGroup::SetSelection(const int index) {
             assert(index >= 0);
             const size_t indexS = static_cast<size_t>(index);
             assert(indexS < m_buttons.size());
             m_buttons[indexS]->SetValue(true);
         }
-        
+
         int RadioGroup::GetSelection() const {
             for (size_t i = 0; i < m_buttons.size(); ++i)
                 if (m_buttons[i]->GetValue())
                     return static_cast<int>(i);
             return wxNOT_FOUND;
         }
-        
+
         wxString RadioGroup::GetStringSelection() const {
             const int index = GetSelection();
             if (index == wxNOT_FOUND)
