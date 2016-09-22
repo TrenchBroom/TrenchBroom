a01937e7f99b8d35a46d6ea946ad99aa68b00412
Kristian Duske <kristian.duske@gmail.com>
2016-09-22 22:59:19 +0200

    #1440: Minor changes.

--- common/src/IO/ELParser.h
+++ common/src/IO/ELParser.h
@@ -27,47 +27,53 @@
 
 #include <list>
 
+#ifdef _MSC_VER
+#include <cstdint>
+#elif defined __GNUC__
+#include <stdint.h>
+#endif
+
 namespace TrenchBroom {
     namespace IO {
         namespace ELToken {
-            typedef size_t Type;
-            static const Type Variable    = 1ul <<  1;
-            static const Type String = 1ul << 2;
-            static const Type Number = 1ul << 3;
-            static const Type Boolean = 1ul << 4;
-            static const Type OBracket = 1ul << 5;
-            static const Type CBracket = 1ul << 6;
-            static const Type OBrace = 1ul << 7;
-            static const Type CBrace = 1ul << 8;
-            static const Type OParen = 1ul << 9;
-            static const Type CParen = 1ul << 10;
-            static const Type Addition = 1ul << 11;
-            static const Type Subtraction = 1ul << 12;
-            static const Type Multiplication = 1ul << 13;
-            static const Type Division = 1ul << 14;
-            static const Type Colon = 1ul << 16;
-            static const Type Modulus = 1ul << 15;
-            static const Type Comma = 1ul << 17;
-            static const Type Range = 1ul << 18;
-            static const Type LogicalNegation = 1ul << 19;
-            static const Type LogicalAnd = 1ul << 20;
-            static const Type LogicalOr = 1ul << 21;
-            static const Type Less = 1ul << 22;
-            static const Type LessOrEqual = 1ul << 23;
-            static const Type Equal = 1ul << 24;
-            static const Type Inequal = 1ul << 25;
-            static const Type GreaterOrEqual = 1ul << 26;
-            static const Type Greater = 1ul << 27;
-            static const Type Case = 1ul << 28;
-            static const Type BitwiseNegation = 1ul << 29;
-            static const Type BitwiseAnd = 1ul << 30;
-            static const Type BitwiseXor = 1ul << 31;
-            static const Type BitwiseOr = 1ul << 32;
-            static const Type BitwiseShiftLeft = 1ul << 33;
-            static const Type BitwiseShiftRight = 1ul << 34;
-            static const Type DoubleOBrace = 1ul << 35;
-            static const Type DoubleCBrace = 1ul << 36;
-            static const Type Eof = 1ul << 37;
+            typedef uint64_t Type;
+            static const Type Variable    = 1ull <<  1;
+            static const Type String = 1ull << 2;
+            static const Type Number = 1ull << 3;
+            static const Type Boolean = 1ull << 4;
+            static const Type OBracket = 1ull << 5;
+            static const Type CBracket = 1ull << 6;
+            static const Type OBrace = 1ull << 7;
+            static const Type CBrace = 1ull << 8;
+            static const Type OParen = 1ull << 9;
+            static const Type CParen = 1ull << 10;
+            static const Type Addition = 1ull << 11;
+            static const Type Subtraction = 1ull << 12;
+            static const Type Multiplication = 1ull << 13;
+            static const Type Division = 1ull << 14;
+            static const Type Colon = 1ull << 16;
+            static const Type Modulus = 1ull << 15;
+            static const Type Comma = 1ull << 17;
+            static const Type Range = 1ull << 18;
+            static const Type LogicalNegation = 1ull << 19;
+            static const Type LogicalAnd = 1ull << 20;
+            static const Type LogicalOr = 1ull << 21;
+            static const Type Less = 1ull << 22;
+            static const Type LessOrEqual = 1ull << 23;
+            static const Type Equal = 1ull << 24;
+            static const Type Inequal = 1ull << 25;
+            static const Type GreaterOrEqual = 1ull << 26;
+            static const Type Greater = 1ull << 27;
+            static const Type Case = 1ull << 28;
+            static const Type BitwiseNegation = 1ull << 29;
+            static const Type BitwiseAnd = 1ull << 30;
+            static const Type BitwiseXor = 1ull << 31;
+            static const Type BitwiseOr = 1ull << 32;
+            static const Type BitwiseShiftLeft = 1ull << 33;
+            static const Type BitwiseShiftRight = 1ull << 34;
+            static const Type DoubleOBrace = 1ull << 35;
+            static const Type DoubleCBrace = 1ull << 36;
+            static const Type Eof = 1ull << 37;
             static const Type Literal = String | Number | Boolean;
             static const Type UnaryOperator = Addition | Subtraction | LogicalNegation | BitwiseNegation;
             static const Type SimpleTerm = Variable | Literal | OParen | OBracket | OBrace | UnaryOperator;
