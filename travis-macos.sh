f7fe54e8310e1700c6ea68244a8073544561e324
Eric Wasylishen <ewasylishen@gmail.com>
2016-12-20 12:54:57 -0700

    Appveyor: Build and package the application (#1514)
    * appveyor.yml: also build the main application
    
    Move FIND_PACKAGE(Git) to the correct location, so APP_BUILD_ID
    gets set.
    
    * travis scripts: try building + packaging on mac and linux
    
    * Upload build artifacts to GitHub Releases when a tag is created

--- travis-macos.sh
+++ travis-macos.sh
@@ -12,9 +12,9 @@ if [[ "daf03ed0006e41334f10ceeb3aa2d20c63aacd42" != $(openssl sha1 wxWidgets-3.1
 7z x -o"wxWidgets" -y wxWidgets-3.1.0.7z > /dev/null
 cd wxWidgets || exit 1
 patch -p0 < ../patches/wxWidgets/*.patch || exit 1
-mkdir build-debug
-cd build-debug
-../configure --quiet --enable-debug --with-osx_cocoa --disable-mediactrl --with-opengl --with-macosx-version-min=10.9 --with-cxx=11 --prefix=$(pwd)/install --disable-precomp-headers && make -j2 && make install
+mkdir build-release
+cd build-release
+../configure --quiet --with-osx_cocoa --disable-shared --disable-mediactrl --with-opengl --with-macosx-version-min=10.9 --with-cxx=11 --prefix=$(pwd)/install --disable-precomp-headers && make -j2 && make install
 cd ..
 cd ..
 
@@ -22,6 +22,7 @@ cd ..
 
 mkdir build
 cd build
-cmake .. -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS=-Werror -DwxWidgets_PREFIX=$(pwd)/../wxWidgets/build-debug/install || exit 1
+cmake .. -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_CXX_FLAGS=-Werror -DwxWidgets_PREFIX=$(pwd)/../wxWidgets/build-release/install || exit 1
 ninja || exit 1
+cpack || exit 1
 ./TrenchBroom-Test
