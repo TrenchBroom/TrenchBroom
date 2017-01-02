cee4032bd808ab6c176b625649c2c13b515e9f52
SnrNotHere16 <aquineda16@hotmail.com>
2017-01-02 15:58:11 -0800

    #1586 Rename std::vector based typedefs to Array instead of List

--- common/src/Model/CollectAttributableNodesVisitor.cpp
+++ common/src/Model/CollectAttributableNodesVisitor.cpp
@@ -26,7 +26,7 @@
 
 namespace TrenchBroom {
     namespace Model {
-        const AttributableNodeList& CollectAttributableNodesVisitor::nodes() const {
+        const AttributableNodeArray& CollectAttributableNodesVisitor::nodes() const {
             return m_nodes;
         }
 
