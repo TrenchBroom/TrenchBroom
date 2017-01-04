389068949c8ac00a0d1312ba85dc3ff69216afb8
SnrNotHere16 <aquineda16@hotmail.com>
2017-01-03 19:05:52 -0800

    #1586 Rename std::vector based typedefs to Array instead of List

--- common/src/Model/MissingClassnameIssueGenerator.h
+++ common/src/Model/MissingClassnameIssueGenerator.h
@@ -32,7 +32,7 @@ namespace TrenchBroom {
         public:
             MissingClassnameIssueGenerator();
         private:
-            void doGenerate(AttributableNode* node, IssueList& issues) const;
+            void doGenerate(AttributableNode* node, IssueArray& issues) const;
         };
     }
 }
