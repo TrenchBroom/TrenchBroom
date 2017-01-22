a2d2b9ddbd56b5988ecf38cb63561c70f2f1e826
Eric Wasylishen <ewasylishen@gmail.com>
2017-01-22 14:27:36 -0700

    #1681: Tell wxWidgets to build its own libpng/libtiff/libjpeg (#1682)
    * 1681: tell wxWidgets to build its own libpng/libtiff/libjpeg
    
    * 1682: Print out the shared libraries used at the end of travis-macos.sh

--- travis-macos.sh
+++ travis-macos.sh
@@ -14,7 +14,7 @@ cd wxWidgets || exit 1
 patch -p0 < ../patches/wxWidgets/*.patch || exit 1
 mkdir build-release
 cd build-release
-../configure --quiet --with-osx_cocoa --disable-shared --disable-mediactrl --with-opengl --with-macosx-version-min=10.9 --with-cxx=11 --prefix=$(pwd)/install --disable-precomp-headers && make -j2 && make install
+../configure --quiet --with-osx_cocoa --disable-shared --disable-mediactrl --with-opengl --with-macosx-version-min=10.9 --with-cxx=11 --prefix=$(pwd)/install --disable-precomp-headers --with-libpng=builtin --with-libtiff=builtin --with-libjpeg=builtin && make -j2 && make install
 cd ..
 cd ..
 
@@ -25,4 +25,7 @@ cd build
 cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-Werror -DwxWidgets_PREFIX=$(pwd)/../wxWidgets/build-release/install || exit 1
 ninja || exit 1
 cpack || exit 1
-./TrenchBroom-Test
+./TrenchBroom-Test || exit 1
+
+echo "Shared libraries used:"
+otool -L ./TrenchBroom.app/Contents/MacOS/TrenchBroom
