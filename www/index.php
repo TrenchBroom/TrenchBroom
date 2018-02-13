7f4b68ad8dc26449a105658db8cc9f33c9a4a0ac
Kristian Duske <kristian.duske@gmail.com>
2018-02-13 20:27:16 +0100

    Update website.

--- www/index.php
+++ www/index.php
@@ -1,37 +1,8 @@
 <?php include 'header.php' ?>
 <div id="content">
 	<p>TrenchBroom is a cross platform level editor for Quake-engine based games. It supports <b>Quake</b>, <b>Quake 2</b>, and <b>Hexen 2</b> and runs on <b>Windows</b> (XP and newer), <b>Mac OS X</b> (10.6 and newer) and <b>Linux</b>. TrenchBroom is easy to use and provides many simple and advanced tools to create complex and interesting levels with ease.</p>
-	<h3>Features</h3>
-	<h4>General</h4>
-	<ul>
-		<li>Full support for editing in 3D and in up to three 2D views</li>
-		<li>High performance renderer with support for huge maps</li>
-		<li>Unlimited Undo and Redo</li>
-		<li>Macro-like command repetition</li>
-		<li>Issue browser with automatic quick fixes</li>
-		<li>Point file support</li>
-		<li>Automatic backups</li>
-		<li>Free and cross platform</li>
-	</ul>
-	<h4>Brush Editing</h4>
-	<ul>
-		<li>Robust vertex editing with edge and face splitting and manipulating multiple vertices together</li>
-		<li>Clipping tool with two and three points</li>
-		<li>CSG operations: merge, subtract, intersect</li>
-		<li>UV view for easy texture manipulations</li>
-		<li>Precise texture lock for all brush editing operations</li>
-		<li>Multiple texture collections</li>
-	</ul>
-	<h4>Entity Editing</h4>
-	<ul>
-		<li>Entity browser with drag and drop support</li>
-		<li>Support for FGD and DEF files for entity definitions</li>
-		<li>Mod support</li>
-		<li>Entity link visualization</li>
-		<li>Displays 3D models in the editor</li>
-		<li>Smart entity property editors</li>
-	</ul>
-	<h3>Screenshots</h3>
-	<div class="screenshot"><img src="images/screenshot.png" width="900" height="578" /></div>
+	<div class="video">
+		<iframe width="900" height="506" src="https://www.youtube.com/embed/shcAvnYp9ow?ecver=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+	</div>
 </div>
 <?php include 'footer.php' ?>
