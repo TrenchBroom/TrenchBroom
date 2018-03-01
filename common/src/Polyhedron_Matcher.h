0988554763bf59fb042626ad8c0113578ebe2e95
Kristian Duske <kristian.duske@gmail.com>
2018-03-01 14:04:49 +0100

    2082: Add more documentation.

--- common/src/Polyhedron_Matcher.h
+++ common/src/Polyhedron_Matcher.h
@@ -26,6 +26,29 @@
 #include <limits>
 #include <list>
 
+/**
+ * This template is used to match the faces of two polyhedra. The two polyhedra are expected to have the majority of
+ * their vertices in common as a result of a vertex move / addition / removal operation.
+ *
+ * Two faces match if they have identical vertex positions or if they have an optimal matching score. The matching
+ * score is based on a relation over the vertices of the left and the vertices of the right polyhedron. The score of two
+ * faces is then the sum of all pairs of related vertices (l,r), where l is a vertex of the left face L, and r is
+ * a vertex of the right face R. Two vertices (l,r) are related if any of the following conditions apply:
+ *
+ * 1. l and r have identical positions
+ * 2. There is no vertex in the right polyhedron that corresponds to l, but there is a vertex l' in the left polyhedron
+ *    such that (l',r) are related, and l and l' are adjacent in the left polyhedron.
+ * 3. There is no vertex in the left polyhedron that corresponds to r, but there is a vertex r' in the right polyhedron
+ *    such that (l,r') are related, and r and r' are adjacent in the right polyhedron.
+ *
+ * Case 2. corresponds to a vertex removal, that is, a vertex was removed from the left polyhedron. Case 3. corresponds
+ * to a vertex addition, that is, a vertex was added to the right polyhedron. If a vertex is moved, both cases apply
+ * since the move can be regarded as a vertex removal and a subsequent addition.
+ *
+ * Using this relation over the vertices, the matcher will find the best matching face from the left polyhedron for
+ * each face of the right polyhedron. If multiple faces of the left polyhedron have a maximal matching score, the
+ * matcher selects a face such that its normal is closest to the normal of the right face.
+ */
 template <typename P>
 class PolyhedronMatcher {
 private:
@@ -62,6 +85,14 @@ public:
     m_right(right),
     m_vertexRelation(buildVertexRelation(m_left, m_right, vertexMap)) {}
 public:
+    /**
+     * Apply the given callback function to each pair of matching faces. The algorithm iterates over all faces of the
+     * right polyhedron and finds the best matching face of the left polyhedron. Then it applies the given function
+     * to that pair of faces.
+     *
+     * @tparam Callback the type of the callback function
+     * @param callback the callback function to apply to each pair of matching faces
+     */
     template <typename Callback>
     void processRightFaces(const Callback& callback) const {
         auto* firstRightFace = m_right.faces().front();
@@ -74,7 +105,16 @@ public:
     }
     
     typedef std::list<Face*> MatchingFaces;
-    
+private:
+    /**
+     * Find the best matching face from the left polyhedron for the given face of the right polyhedron. The best match
+     * is determined using the matching score (see function findMatchingLeftFaces). If multiple faces of the left
+     * polyhedron have a maximal matching score with the given face of the right polyhedron, this function selects a
+     * face based upon the dot products of the face normals.
+     *
+     * @param rightFace the face of the right polyhedron to find a match for
+     * @return a best matching face of the left polyhedron
+     */
     Face* findBestMatchingLeftFace(Face* rightFace) const {
         const auto matchingFaces = findMatchingLeftFaces(rightFace);
         ensure(!matchingFaces.empty(), "No matching face found");
@@ -84,7 +124,8 @@ public:
         
         auto* result = *it++;
         auto bestDot = rightFace->normal().dot(result->normal());
-        
+
+        // exit early if we find a face with an identical normal
         while (it != std::end(matchingFaces) && bestDot < 1.0) {
             auto* currentFace = *it;
             const auto dot = rightFace->normal().dot(currentFace->normal());
@@ -98,8 +139,12 @@ public:
         return result;
     }
 
-    /*
-     * Returns all left faces which have a maximal matching score with the given right face.
+    /**
+     * Find all faces of the left polyhedron that have a maximal matching score with the given face of the right
+     * polyhedron.
+     *
+     * @param rightFace the face of the right polyhedron
+     * @return the matching faces of the left polyhedron
      */
     MatchingFaces findMatchingLeftFaces(Face* rightFace) const {
         MatchingFaces result;
@@ -122,12 +167,16 @@ public:
         return result;
     }
 private:
-    /*
-     * Computes the matching score between the given two faces using the data stored in the vertex relation.
+    /**
+     * Computes the matching score between the given left and right faces the data stored in the vertex relation.
      *
      * The matching score between the given faces is the number of all pairs of a vertex of the given left face
      * and a vertex of the given right face which are also in the vertex relation, unless the faces are identical.
      * In that case, this function returns a perfect match score.
+     *
+     * @param leftFace a face of the left polyhedron
+     * @param rightFace a face of the right polyhedron
+     * @return the matching score
      */
     size_t computeMatchScore(Face* leftFace, Face* rightFace) const {
         if (leftFace->vertexCount() == rightFace->vertexCount() && leftFace->hasVertexPositions(rightFace->vertexPositions())) {
@@ -159,6 +208,17 @@ private:
         return result;
     }
 private:
+    /**
+     * Build the vertex relation for the given left and right polyhedra.
+     *
+     * The relation is built by inserting every pair of vertices (l,r) such that l is a vertex of the left polyhedron
+     * and r is a vertex of the given right polyhedron and (l,r) have identical positions. Then, the relation is
+     * expanded by calling the expandVertexRelation function.
+     *
+     * @param left the left polyhedron
+     * @param right the right polyhedron
+     * @return the vertex relation
+     */
     static VertexRelation buildVertexRelation(const P& left, const P& right) {
         VertexRelation result;
         
@@ -179,7 +239,21 @@ private:
     static VertexRelation buildVertexRelation(const P& left, const P& right, const typename V::List& vertices, const V& delta) {
         return buildVertexRelation(left, right, typename V::Set(std::begin(vertices), std::end(vertices)), delta);
     }
-    
+
+    /**
+     * Builds the vertex relation for a pair of polyhedra such that the right polyhedron is the result of moving the
+     * given vertices of the left polyhedron by the given delta.
+     *
+     * The function accounts for the moved vertices when attempting to find a vertex of the left polyhedron in the
+     * right polyhedron. If a vertex v is in the given set of moved vertices, then the algorithm attempts to find it
+     * at its new position in the right polyhedron.
+     *
+     * @param left the left polyhedron
+     * @param right the right polyhedron
+     * @param vertices the vertices that have been moved
+     * @param delta the move delta
+     * @return the vertex relation
+     */
     static VertexRelation buildVertexRelation(const P& left, const P& right, const typename V::Set& vertices, const V& delta) {
         typename V::Map vertexMap;
         
@@ -199,7 +273,15 @@ private:
         
         return buildVertexRelation(left, right, vertexMap);
     }
-    
+
+    /**
+     * Helper function to build a vertex relation using the given set of corresponding vertices.
+     *
+     * @param left the left polyhedron
+     * @param right the right polyhedron
+     * @param vertexMap a set of corresponding vertices for which to build the relation
+     * @return the vertex relation
+     */
     static VertexRelation buildVertexRelation(const P& left, const P& right, const typename V::Map& vertexMap) {
         VertexRelation result;
         
@@ -217,14 +299,38 @@ private:
 
         return expandVertexRelation(left, right, result);
     }
-    
+
+    /**
+     * Expand the given vertex relation of vertices of the given left and right polyhedra. Expanding a vertex relation
+     * is based on the given initial relation, and expands the relation by those vertices present only in the right
+     * polyhedron and by those vertices present only in the left polyhedron.
+     *
+     * @param left the left polyhedron
+     * @param right the right polyhedron
+     * @param initialRelation the initial vertex relation
+     * @return the expanded vertex relation
+     */
     static VertexRelation expandVertexRelation(const P& left, const P& right, const VertexRelation& initialRelation) {
         auto result = initialRelation;
         result.insert(addedVertexRelation(right, initialRelation));
         result.insert(removedVertexRelation(left, initialRelation));
         return result;
     }
-    
+
+    /**
+     * Returns a vertex relation that includes only vertices present in the given right polyhedron, but not in the
+     * given vertex relation. Such a vertex is related to the vertices which are related to its neighbours as follows.
+     *
+     * Let r be a vertex present in the given polyhedron that has no related vertices in the given relation. Let r'
+     * be adjacent to r in the given polyhedron and let r' be related to l in the given relation. l is a vertex
+     * of the left polyhedron under treatment. Then the pair (l,r) is added to the resulting relation.
+     *
+     * Note that the resulting relation does not include the given initial relation.
+     *
+     * @param right the polyhedron with additional vertices
+     * @param initialRelation the initial vertex relation
+     * @return a vertex relation containing only the newly discovered related vertices
+     */
     static VertexRelation addedVertexRelation(const P& right, const VertexRelation& initialRelation) {
         const auto addedVertices = findAddedVertices(right, initialRelation);
 
@@ -246,7 +352,22 @@ private:
         
         return result;
     }
-    
+
+
+    /**
+     * Returns a vertex relation that includes only vertices present in the given left polyhedron, but not in the
+     * given vertex relation. Such a vertex is related to the vertices which are related to its neighbours as follows.
+     *
+     * Let l be a vertex present in the given polyhedron that has no related vertices in the given relation. Let l'
+     * be adjacent to l in the given polyhedron and let l' be related to r in the given relation. r is a vertex
+     * of the left polyhedron under treatment. Then the pair (l,r) is added to the resulting relation.
+     *
+     * Note that the resulting relation does not include the given initial relation.
+     *
+     * @param right the polyhedron with removed vertices
+     * @param initialRelation the initial vertex relation
+     * @return a vertex relation containing only the newly discovered related vertices
+     */
     static VertexRelation removedVertexRelation(const P& left, const VertexRelation& initialRelation) {
         const auto removedVertices = findRemovedVertices(left, initialRelation);
 
@@ -268,7 +389,15 @@ private:
         
         return result;
     }
-    
+
+    /**
+     * Returns a set containing only those vertices of the given polyhedron for which there is no entry in the given
+     * vertex relation. These are vertices which have been added to the given polyhedron.
+     *
+     * @param right the polyhedron
+     * @param vertexRelation the vertex relation
+     * @return the added vertices
+     */
     static VertexSet findAddedVertices(const P& right, const VertexRelation& vertexRelation) {
         VertexSet result;
         
@@ -283,7 +412,15 @@ private:
         
         return result;
     }
-    
+
+    /**
+     * Returns a set containing only those vertices of the given polyhedron for there is no entry in the given vertex
+     * relation. These are vertices which have been removed from the given polyhedron.
+     *
+     * @param left the polyhedron
+     * @param vertexRelation the vertex relation
+     * @return the removed vertices
+     */
     static VertexSet findRemovedVertices(const P& left, const VertexRelation& vertexRelation) {
         VertexSet result;
         
