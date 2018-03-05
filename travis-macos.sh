96a876478ec93bf240a56d0d5cf8084eb96044dc
Eric Wasylishen <ewasylishen@gmail.com>
2018-03-04 19:05:47 -0700

    2092: tell 7z to overwrite files when extracting
    The Windows binary packages of 3.1.1 contain duplicate .dll's.
    
    Disable wx's builtin libtiff on macOS as it causes a duplicate symbol
    link error with freeimage.

--- travis-macos.sh
+++ travis-macos.sh
@@ -14,7 +14,7 @@ cd wxWidgets || exit 1
 #patch -p0 < ../patches/wxWidgets/*.patch || exit 1
 mkdir build-release
 cd build-release
-../configure --quiet --with-osx_cocoa --disable-shared --disable-mediactrl --with-opengl --with-macosx-version-min=10.9 --with-cxx=14 --prefix=$(pwd)/install --disable-precomp-headers --with-libpng=builtin --with-libtiff=builtin --with-libjpeg=builtin && make -j2 && make install
+../configure --quiet --with-osx_cocoa --disable-shared --disable-mediactrl --with-opengl --with-macosx-version-min=10.9 --with-cxx=14 --prefix=$(pwd)/install --disable-precomp-headers --with-libpng=builtin --without-libtiff --with-libjpeg=builtin && make -j2 && make install
 cd ..
 cd ..
 
