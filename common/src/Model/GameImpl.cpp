697bf1c7c95226f33c3a10d2ab50c4c03a36b1c2
Eric Wasylishen <ewasylishen@gmail.com>
2018-06-15 01:56:29 -0600

    2152: fix search path order to match Quake:
    highest to lowest:
    mod pakfiles
    mod loose files
    id1 pakfiles
    id1 loose files
    
    Fixes #2152

--- common/src/Model/GameImpl.cpp
+++ common/src/Model/GameImpl.cpp
@@ -64,12 +64,12 @@ namespace TrenchBroom {
             const GameConfig::FileSystemConfig& fileSystemConfig = m_config.fileSystemConfig();
             if (!m_gamePath.isEmpty() && IO::Disk::directoryExists(m_gamePath)) {
                 addSearchPath(fileSystemConfig.searchPath, logger);
-                for (const IO::Path& searchPath : m_additionalSearchPaths)
-                    addSearchPath(searchPath, logger);
-                
                 addPackages(m_gamePath + fileSystemConfig.searchPath);
-                for (const IO::Path& searchPath : m_additionalSearchPaths)
+
+                for (const IO::Path& searchPath : m_additionalSearchPaths) {
+                    addSearchPath(searchPath, logger);
                     addPackages(m_gamePath + searchPath);
+                }
             }
         }
 
