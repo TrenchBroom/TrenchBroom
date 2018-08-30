1429df22788de01222fa1c81e9c3bd39552f5853
Eric Wasylishen <ewasylishen@gmail.com>
2018-08-29 21:25:09 -0600

    2246: check for the required wxWidgets version
    The bit I added to FindwxWidgetsTB.cmake was copied from the
    FindwxWidgets.cmake in my cmake 3.11.3 installation.
    
    fixes #2246

--- cmake/Modules/FindwxWidgetsTB.cmake
+++ cmake/Modules/FindwxWidgetsTB.cmake
@@ -848,6 +848,27 @@ else()
   endif()
 endif()
 
+if(wxWidgets_FOUND)
+  find_file(_filename wx/version.h PATHS ${wxWidgets_INCLUDE_DIRS} NO_DEFAULT_PATH)
+  dbg_msg("_filename:  ${_filename}")
+
+  if(NOT _filename)
+    message(FATAL_ERROR "wxWidgets wx/version.h file not found in ${wxWidgets_INCLUDE_DIRS}.")
+  endif()
+
+  file(READ ${_filename} _wx_version_h)
+
+  string(REGEX REPLACE "^(.*\n)?#define +wxMAJOR_VERSION +([0-9]+).*"
+    "\\2" wxWidgets_VERSION_MAJOR "${_wx_version_h}" )
+  string(REGEX REPLACE "^(.*\n)?#define +wxMINOR_VERSION +([0-9]+).*"
+    "\\2" wxWidgets_VERSION_MINOR "${_wx_version_h}" )
+  string(REGEX REPLACE "^(.*\n)?#define +wxRELEASE_NUMBER +([0-9]+).*"
+    "\\2" wxWidgets_VERSION_PATCH "${_wx_version_h}" )
+  set(wxWidgets_VERSION_STRING
+    "${wxWidgets_VERSION_MAJOR}.${wxWidgets_VERSION_MINOR}.${wxWidgets_VERSION_PATCH}" )
+  dbg_msg("wxWidgets_VERSION_STRING:    ${wxWidgets_VERSION_STRING}")
+endif()
+
 # Debug output:
 DBG_MSG("wxWidgets_FOUND           : ${wxWidgets_FOUND}")
 DBG_MSG("wxWidgets_INCLUDE_DIRS    : ${wxWidgets_INCLUDE_DIRS}")
--- cmake/wxWidgets.cmake
+++ cmake/wxWidgets.cmake
@@ -31,6 +31,19 @@ SET(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_CURRENT_SOURCE_DIR}/cmake/Mo
 FIND_PACKAGE(wxWidgetsTB REQUIRED)
 INCLUDE("${wxWidgets_USE_FILE}")
 
+# Check version
+message(STATUS "wxWidgets ${wxWidgets_VERSION_MAJOR}.${wxWidgets_VERSION_MINOR}.${wxWidgets_VERSION_PATCH} found.")
+
+SET(TB_WX_MAJOR 3)
+SET(TB_WX_MINOR 1)
+SET(TB_WX_PATCH 1)
+
+IF ((${wxWidgets_VERSION_MAJOR} LESS ${TB_WX_MAJOR}) OR
+(${wxWidgets_VERSION_MAJOR} EQUAL ${TB_WX_MAJOR} AND ${wxWidgets_VERSION_MINOR} LESS ${TB_WX_MINOR}) OR
+(${wxWidgets_VERSION_MAJOR} EQUAL ${TB_WX_MAJOR} AND ${wxWidgets_VERSION_MINOR} EQUAL ${TB_WX_MINOR} AND ${wxWidgets_VERSION_PATCH} LESS ${TB_WX_PATCH}))
+    message(FATAL_ERROR "wxWidgets ${TB_WX_MAJOR}.${TB_WX_MINOR}.${TB_WX_PATCH} required.")
+ENDIF()
+
 # Remove QuickTime framework on OS X; it's not needed and produces a linker warning
 STRING(REPLACE "-framework QuickTime;" "" wxWidgets_LIBRARIES "${wxWidgets_LIBRARIES}")
 
