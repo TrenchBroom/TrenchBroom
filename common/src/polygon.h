e8f768a395c828ced862e1f44cd56935a681d949
Kristian Duske <kristian.duske@gmail.com>
2018-09-06 23:23:16 +0200

    2201: refactoring polygon

--- common/src/polygon.h
+++ common/src/polygon.h
@@ -39,18 +39,36 @@ public:
 private:
     typename vec<T,S>::List m_vertices;
 public:
+    /**
+     * Creates a new empty polygon.
+     */
     polygon() {}
 
+    /**
+     * Creates a new polygon with the given vertices. The given points are assumed to form a convex polygon.
+     *
+     *  @param i_vertices the vertices
+     */
     polygon(std::initializer_list<vec<T,S>> i_vertices) :
     m_vertices(i_vertices) {
         CollectionUtils::rotateMinToFront(m_vertices);
     }
 
+    /**
+     * Creates a new polygon with the given vertices. The given points are assumed to form a convex polygon.
+     *
+     * @param i_vertices the vertices
+     */
     polygon(const typename vec<T,S>::List& i_vertices) :
-    m_vertices(i_vertices) {
+            m_vertices(i_vertices) {
         CollectionUtils::rotateMinToFront(m_vertices);
     }
 
+    /**
+     * Creates a new polygon with the given vertices. The given points are assumed to form a convex polygon.
+     *
+     * @param i_vertices the vertices
+     */
     polygon(typename vec<T,S>::List& i_vertices) {
         using std::swap;
         swap(m_vertices, i_vertices);
@@ -59,43 +77,25 @@ public:
 
     // Copy and move constructors
     polygon(const polygon<T,S>& other) = default;
-    polygon(polygon<T,S>&& other) = default;
+    polygon(polygon<T,S>&& other) noexcept = default;
 
     // Assignment operators
     polygon<T,S>& operator=(const polygon<T,S>& other) = default;
-    polygon<T,S>& operator=(polygon<T,S>&& other) = default;
+    polygon<T,S>& operator=(polygon<T,S>&& other) noexcept = default;
 
-    // Conversion constructors
+    /**
+     * Creates a new polygon by copying the values from the given polygon. If the given polygon has a different component
+     * type, the values are converted using static_cast.
+     *
+     * @tparam U the component type of the given polygon
+     * @param other the polygon to copy the values from
+     */
     template <typename U>
     polygon(const polygon<U,S>& other) {
         m_vertices.reserve(other.vertexCount());
         for (const auto& vertex : other.vertices()) {
             m_vertices.push_back(vec<T,S>(vertex));
         }
-    }
-
-    bool operator==(const polygon<T,S>& rhs) const {
-        return compare(*this, rhs, T(0.0)) == 0;
-    }
-
-    bool operator!=(const polygon<T,S>& rhs) const {
-        return compare(*this, rhs, T(0.0)) != 0;
-    }
-
-    bool operator<(const polygon<T,S>& rhs) const {
-        return compare(*this, rhs, T(0.0)) < 0;
-    }
-
-    bool operator<=(const polygon<T,S>& rhs) const {
-        return compare(*this, rhs, T(0.0)) <= 0;
-    }
-
-    bool operator>(const polygon<T,S>& rhs) const {
-        return compare(*this, rhs, T(0.0)) > 0;
-    }
-
-    bool operator>=(const polygon<T,S>& rhs) const {
-        return compare(*this, rhs, T(0.0)) >= 0;
     }
 public:
     bool hasVertex(const vec<T,S>& vertex) const {
@@ -165,6 +165,20 @@ using polygon2d = polygon<double,2>;
 using polygon3f = polygon<float,3>;
 using polygon3d = polygon<double,3>;
 
+/**
+ * Compares the given polygons under the assumption that the first vertex of each polygon is the smallest of all
+ * vertices of that polygon. A polygon is considered less than another polygon if is has fewer vertices, and vice
+ * versa. If both polygons have the same number of vertices, the vertices are compared lexicographically until a pair
+ * of vertices is found which are not identical. The result of comparing these vertices is returned. If no such pair
+ * exists, 0 is returned.
+ *
+ * @tparam T the component type
+ * @tparam S the number of components
+ * @param lhs the first polygon
+ * @param rhs the second polygon
+ * @param epsilon an epsilon value
+ * @return -1 if the first polygon is less than the second polygon, +1 in the opposite case, and 0 otherwise
+ */
 template <typename T, size_t S>
 int compare(const polygon<T,S>& lhs, const polygon<T,S>& rhs, const T epsilon = static_cast<T>(0.0)) {
     const auto& lhsVerts = lhs.vertices();
@@ -180,6 +194,107 @@ int compare(const polygon<T,S>& lhs, const polygon<T,S>& rhs, const T epsilon = 
     }
 }
 
+/**
+ * Checks whether the first given polygon is identical to the second polygon.
+ *
+ * @tparam T the component type
+ * @tparam S the number of components
+ * @param lhs the first polygon
+ * @param rhs the second polygon
+ * @return true if the polygons are identical and false otherwise
+ */
+template <typename T, size_t S>
+bool operator==(const polygon<T,S>& lhs, const polygon<T,S>& rhs) {
+    return compare(lhs, rhs, T(0.0)) == 0;
+}
+
+/**
+ * Checks whether the first given polygon is identical to the second polygon.
+ *
+ * @tparam T the component type
+ * @tparam S the number of components
+ * @param lhs the first polygon
+ * @param rhs the second polygon
+ * @return false if the polygons are identical and true otherwise
+ */
+template <typename T, size_t S>
+bool operator!=(const polygon<T,S>& lhs, const polygon<T,S>& rhs) {
+    return compare(lhs, rhs, T(0.0)) != 0;
+}
+
+/**
+ * Checks whether the first given polygon is less than the second polygon.
+ *
+ * @tparam T the component type
+ * @tparam S the number of components
+ * @param lhs the first polygon
+ * @param rhs the second polygon
+ * @return true if the first polygon is less than the second polygon and false otherwise
+ */
+template <typename T, size_t S>
+bool operator<(const polygon<T,S>& lhs, const polygon<T,S>& rhs) {
+    return compare(lhs, rhs, T(0.0)) < 0;
+}
+
+/**
+ * Checks whether the first given polygon is less than or equal to the second polygon.
+ *
+ * @tparam T the component type
+ * @tparam S the number of components
+ * @param lhs the first polygon
+ * @param rhs the second polygon
+ * @return true if the first polygon is less than or equal to the second polygon and false otherwise
+ */
+ template <typename T, size_t S>
+bool operator<=(const polygon<T,S>& lhs, const polygon<T,S>& rhs) {
+    return compare(lhs, rhs, T(0.0)) <= 0;
+}
+
+/**
+ * Checks whether the first given polygon is greater than the second polygon.
+ *
+ * @tparam T the component type
+ * @tparam S the number of components
+ * @param lhs the first polygon
+ * @param rhs the second polygon
+ * @return true if the first polygon is greater than the second polygon and false otherwise
+ */
+template <typename T, size_t S>
+bool operator>(const polygon<T,S>& lhs, const polygon<T,S>& rhs) {
+    return compare(lhs, rhs, T(0.0)) > 0;
+}
+
+/**
+ * Checks whether the first given polygon is greater than or equal to the second polygon.
+ *
+ * @tparam T the component type
+ * @tparam S the number of components
+ * @param lhs the first polygon
+ * @param rhs the second polygon
+ * @return true if the first polygon is greater than or equal to the second polygon and false otherwise
+ */
+template <typename T, size_t S>
+bool operator>=(const polygon<T,S>& lhs, const polygon<T,S>& rhs) {
+    return compare(lhs, rhs, T(0.0)) >= 0;
+}
+
+/**
+ * Compares the given polygons under the assumption that the first vertex of each polygon is the smallest of all
+ * vertices of that polygon. A polygon is considered less than another polygon if is has fewer vertices, and vice
+ * versa. If both polygons have the same number of vertices, the vertices are compared lexicographically until a pair
+ * of vertices is found which are not identical. The result of comparing these vertices is returned. If no such pair
+ * exists, 0 is returned.
+ *
+ * In this comparison, the order of the vertices is relaxed. Two polygons can be identical even if the order of the
+ * vertices is reversed.
+ *
+ * @tparam T the component type
+ * @tparam S the number of components
+ * @param lhs the first polygon
+ * @param rhs the second polygon
+ * @param epsilon an epsilon value
+ * @return -1 if the first polygon is less than the second polygon, +1 in the opposite case, and 0 otherwise
+ */
 template <typename T, size_t S>
 int compareUnoriented(const polygon<T,S>& lhs, const polygon<T,S>& rhs, const T epsilon = static_cast<T>(0.0)) {
     const auto& lhsVerts = lhs.vertices();
