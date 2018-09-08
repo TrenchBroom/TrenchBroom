ff6cf2069fa3254070a3d6160c312ac2cf25c595
Kristian Duske <kristian.duske@gmail.com>
2018-09-09 01:01:15 +0200

    2201: Make vecmath a header only interface library.

--- cmake/vecmath.cmake
+++ cmake/vecmath.cmake
@@ -1,18 +1,10 @@
 SET(VECMATH_SOURCE_DIR ${CMAKE_SOURCE_DIR}/vecmath/src)
 
-FILE(GLOB_RECURSE VECMATH_SOURCE
-    "${VECMATH_SOURCE_DIR}/*.cpp"
+ADD_LIBRARY(vecmath INTERFACE)
+TARGET_INCLUDE_DIRECTORIES(vecmath INTERFACE
+	$<BUILD_INTERFACE:${VECMATH_SOURCE_DIR}>
+	$<INSTALL_INTERFACE:include/vecmath>
 )
-
-FILE(GLOB_RECURSE VECMATH_HEADER
-    "${VECMATH_SOURCE_DIR}/*.h"
-)
-
-# Unfortunately, Xcode still compiles OBJECT libraries as static libraries, so there's no real gain in build time.
-# But we can still use this on other platforms and in Release builds
-#IF(NOT CMAKE_GENERATOR STREQUAL "Xcode")
-	ADD_LIBRARY(vecmath OBJECT ${VECMATH_SOURCE} ${VECMATH_HEADER})
-	SET_XCODE_ATTRIBUTES(vecmath)
-#ENDIF()
+# SET_XCODE_ATTRIBUTES(vecmath)
 
 INCLUDE_DIRECTORIES(${VECMATH_SOURCE_DIR})
