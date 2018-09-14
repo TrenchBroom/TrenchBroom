fbd9328d715191133775d37faa9fd41d47250214
Kristian Duske <kristian.duske@gmail.com>
2018-09-14 14:37:23 +0200

    2201: Add tests for ray.

--- test/src/ray_test.cpp
+++ test/src/ray_test.cpp
@@ -28,15 +28,67 @@
 // TODO 2201: write more tests
 
 namespace vm {
-    TEST(RayTest, pointAtDistance) {
-        const ray3f ray(vec3f::zero, vec3f::pos_x);
-        ASSERT_VEC_EQ(vec3f(5.0f, 0.0f, 0.0f), ray.pointAtDistance(5.0f));
+    TEST(RayTest, defaultConstructor) {
+        const auto r = ray3d();
+        ASSERT_VEC_EQ(vec3d::zero, r.origin);
+        ASSERT_VEC_EQ(vec3d::zero, r.direction);
     }
-    
+
+    TEST(RayTest, constructWithOriginAndDirection) {
+        const auto r = ray3d(vec3d::one, vec3d::pos_z);
+        ASSERT_VEC_EQ(vec3d::one, r.origin);
+        ASSERT_VEC_EQ(vec3d::pos_z, r.direction);
+    }
+
+    TEST(RayTest, getOrigin) {
+        const auto r = ray3d(vec3d::one, vec3d::pos_z);
+        ASSERT_VEC_EQ(r.origin, r.getOrigin());
+    }
+
+    TEST(RayTest, getDirection) {
+        const auto r = ray3d(vec3d::one, vec3d::pos_z);
+        ASSERT_VEC_EQ(r.direction, r.getDirection());
+    }
+
+    TEST(RayTest, transform) {
+        const auto r = ray3d(vec3d::one, vec3d::pos_z);
+        const auto rm = rotationMatrix(radians(15.0), radians(20.0), radians(-12.0));
+        const auto tm = translationMatrix(vec3d::one);
+
+        const auto rt = r.transform(rm * tm);
+        ASSERT_TRUE(isUnit(r.direction));
+        ASSERT_VEC_EQ(r.origin * rm * tm, rt.origin);
+        ASSERT_VEC_EQ(r.direction * rm, rt.direction);
+    }
+
     TEST(RayTest, pointStatus) {
         const ray3f ray(vec3f::zero, vec3f::pos_z);
         ASSERT_EQ(point_status::above, ray.pointStatus(vec3f(0.0f, 0.0f, 1.0f)));
         ASSERT_EQ(point_status::inside, ray.pointStatus(vec3f(0.0f, 0.0f, 0.0f)));
         ASSERT_EQ(point_status::below, ray.pointStatus(vec3f(0.0f, 0.0f, -1.0f)));
+    }
+
+    TEST(RayTest, isEqual) {
+        ASSERT_TRUE(isEqual(ray3d(), ray3d(), 0.0));
+        ASSERT_TRUE(isEqual(ray3d(vec3d::zero, vec3d::pos_z), ray3d(vec3d::zero, vec3d::pos_z), 0.0));
+        ASSERT_FALSE(isEqual(ray3d(vec3d(0, 0, 0), vec3d(0, 0, 1)), ray3d(vec3d(1, 0, 0), vec3d(0, 0, 1)), 0.0));
+        ASSERT_TRUE(isEqual(ray3d(vec3d(0, 0, 0), vec3d(0, 0, 1)), ray3d(vec3d(1, 0, 0), vec3d(0, 0, 1)), 2.0));
+    }
+
+    TEST(RayTest, equal) {
+        ASSERT_TRUE(ray3d() == ray3d());
+        ASSERT_TRUE(ray3d(vec3d::zero, vec3d::pos_z) == ray3d(vec3d::zero, vec3d::pos_z));
+        ASSERT_FALSE(ray3d(vec3d(0, 0, 0), vec3d(0, 0, 1)) == ray3d(vec3d(1, 0, 0), vec3d(0, 0, 1)));
+    }
+
+    TEST(RayTest, notEqual) {
+        ASSERT_FALSE(ray3d() != ray3d());
+        ASSERT_FALSE(ray3d(vec3d::zero, vec3d::pos_z) != ray3d(vec3d::zero, vec3d::pos_z));
+        ASSERT_TRUE(ray3d(vec3d(0, 0, 0), vec3d(0, 0, 1)) != ray3d(vec3d(1, 0, 0), vec3d(0, 0, 1)));
+    }
+
+    TEST(RayTest, pointAtDistance) {
+        const ray3f ray(vec3f::zero, vec3f::pos_x);
+        ASSERT_VEC_EQ(vec3f(5.0f, 0.0f, 0.0f), ray.pointAtDistance(5.0f));
     }
 }
