9d7781889f591fec55a25c5f93af4586ab7c5709
Eric Wasylishen <ewasylishen@gmail.com>
2018-09-23 11:04:46 -0700

    2293: fix Entity::origin() caching issue (#2294)
    * 2293: add failing test
    
    * 2293: fix Entity cache invalidation order
    
    * 2293: fix memory leak in test

--- test/src/Model/EntityTest.cpp
+++ test/src/Model/EntityTest.cpp
@@ -24,6 +24,9 @@
 
 #include "Model/Entity.h"
 #include "Model/EntityAttributes.h"
+#include "Model/MapFormat.h"
+#include "Model/Layer.h"
+#include "Model/World.h"
 
 #include <vecmath/vec.h>
 
@@ -31,10 +34,20 @@ namespace TrenchBroom {
     namespace Model {
         class EntityTest : public ::testing::Test {
         protected:
-            std::unique_ptr<Entity> m_entity;
+            Entity* m_entity;
+            World* m_world;
 
             void SetUp() override {
-                m_entity = std::make_unique<Entity>();
+                m_entity = new Entity();
+                m_world = new World(MapFormat::Standard, nullptr, vm::bbox3d(8192.0));
+            }
+
+            void TearDown() override {
+                // Only some of the tests add the entity to the world
+                if (m_entity->parent() == nullptr) {
+                    delete m_entity;
+                }
+                delete m_world;
             }
         };
 
@@ -42,16 +55,40 @@ namespace TrenchBroom {
             EXPECT_EQ(vm::vec3::zero, m_entity->origin());
             EXPECT_EQ(vm::mat4x4::identity, m_entity->rotation());
             EXPECT_TRUE(m_entity->pointEntity());
+            EXPECT_EQ(Entity::DefaultBounds, m_entity->bounds());
         }
 
         TEST_F(EntityTest, originUpdateWithSetAttributes) {
+            const vm::vec3 newOrigin(10, 20, 30);
+            const vm::bbox3 newBounds(newOrigin - (Entity::DefaultBounds.size() / 2.0),
+                                      newOrigin + (Entity::DefaultBounds.size() / 2.0));
+
             m_entity->setAttributes({EntityAttribute("origin", "10 20 30")});
-            EXPECT_EQ(vm::vec3(10, 20, 30), m_entity->origin());
+            EXPECT_EQ(newOrigin, m_entity->origin());
+            EXPECT_EQ(newBounds, m_entity->bounds());
         }
 
         TEST_F(EntityTest, originUpdateWithAddOrUpdateAttributes) {
+            const vm::vec3 newOrigin(10, 20, 30);
+            const vm::bbox3 newBounds(newOrigin - (Entity::DefaultBounds.size() / 2.0),
+                                      newOrigin + (Entity::DefaultBounds.size() / 2.0));
+
             m_entity->addOrUpdateAttribute("origin", "10 20 30");
-            EXPECT_EQ(vm::vec3(10, 20, 30), m_entity->origin());
+            EXPECT_EQ(newOrigin, m_entity->origin());
+            EXPECT_EQ(newBounds, m_entity->bounds());
+        }
+
+        // Same as above, but add the entity to a world
+        TEST_F(EntityTest, originUpdateInWorld) {
+            m_world->defaultLayer()->addChild(m_entity);
+
+            const vm::vec3 newOrigin(10, 20, 30);
+            const vm::bbox3 newBounds(newOrigin - (Entity::DefaultBounds.size() / 2.0),
+                                      newOrigin + (Entity::DefaultBounds.size() / 2.0));
+
+            m_entity->addOrUpdateAttribute("origin", "10 20 30");
+            EXPECT_EQ(newOrigin, m_entity->origin());
+            EXPECT_EQ(newBounds, m_entity->bounds());
         }
     }
 }
