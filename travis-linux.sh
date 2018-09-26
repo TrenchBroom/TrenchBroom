e392cd6ebe1f3757dc1166d479f904810f32ecdc
Eric Wasylishen <ewasylishen@gmail.com>
2018-09-25 23:16:10 -0600

    2302: try caching wxWidgets builds on travis

--- travis-linux.sh
+++ travis-linux.sh
@@ -18,22 +18,28 @@ fi
 
 # Patch and build wxWidgets
 
-wget https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.1/wxWidgets-3.1.1.7z
-if [[ "8d98975eb9f81036261c0643755b98e4bb5ab776" != $(sha1sum wxWidgets-3.1.1.7z | cut -f1 -d' ') ]] ; then exit 1 ; fi
-7z x -o"wxWidgets" -y wxWidgets-3.1.1.7z > /dev/null
-cd wxWidgets || exit 1
-#patch -p0 < ../patches/wxWidgets/*.patch || exit 1
-mkdir build-release
-cd build-release
-../configure --quiet --disable-shared --with-opengl --with-cxx=17 --with-gtk=2 --prefix=$(pwd)/install --disable-precomp-headers --with-libpng=builtin --with-libtiff=builtin --with-libjpeg=builtin && make -j2 && make install
-cd ..
-cd ..
+if [[ ! -d wx-install-cache ]]; then
+    echo "wxwidgets cache directory not found. Building wxwidgets..."
+
+    wget https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.1/wxWidgets-3.1.1.7z
+    if [[ "8d98975eb9f81036261c0643755b98e4bb5ab776" != $(sha1sum wxWidgets-3.1.1.7z | cut -f1 -d' ') ]] ; then exit 1 ; fi
+    7z x -o"wxWidgets" -y wxWidgets-3.1.1.7z > /dev/null
+    cd wxWidgets || exit 1
+    #patch -p0 < ../patches/wxWidgets/*.patch || exit 1
+    mkdir build-release
+    cd build-release
+    ../configure --quiet --disable-shared --with-opengl --with-cxx=17 --with-gtk=2 --prefix=$(pwd)/../../wx-install-cache --disable-precomp-headers --with-libpng=builtin --with-libtiff=builtin --with-libjpeg=builtin && make -j2 && make install
+    cd ..
+    cd ..
+else
+    echo "using cached copy of wxwidgets"
+fi
 
 # Build TB
 
 mkdir build
 cd build
-cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-Werror -DwxWidgets_PREFIX=$(pwd)/../wxWidgets/build-release/install || exit 1
+cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-Werror -DwxWidgets_PREFIX=$(pwd)/../wx-install-cache || exit 1
 cmake --build . --config Release || exit 1
 cpack || exit 1
 
