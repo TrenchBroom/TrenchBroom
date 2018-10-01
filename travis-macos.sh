e87f1dbd35386f888e30997a2b50755fdf42d3eb
Eric Wasylishen <ewasylishen@gmail.com>
2018-10-01 02:21:12 -0600

    2311: Fix issues when built with macOS 10.14 SDK
    - applying https://github.com/wxWidgets/wxWidgets/commit/2ab430965c457de680d3a3ea37445b7dfa0090e0
      to wxWidgets 3.1.1 fixes the freeze.
    - disable dark mode for now since it is broken in TB.
      Note, there is a recent commit in wxWidgets for some dark mode support.
      I haven't tested it though.
      https://github.com/wxWidgets/wxWidgets/pull/935
    
    TB seems to work again with these changes

--- travis-macos.sh
+++ travis-macos.sh
@@ -16,7 +16,7 @@ if [[ ! -e wx-install-cache/bin/wx-config ]]; then
     if [[ "8d98975eb9f81036261c0643755b98e4bb5ab776" != $(openssl sha1 wxWidgets-3.1.1.7z | cut -f2 -d' ') ]] ; then exit 1 ; fi
     7z x -o"wxWidgets" -y wxWidgets-3.1.1.7z > /dev/null
     cd wxWidgets || exit 1
-    patch -p0 < ../patches/wxWidgets/*.patch || exit 1
+    cat ../patches/wxWidgets/*.patch | patch -p0 || exit 1
     mkdir build-release
     cd build-release
     ../configure --quiet --with-osx_cocoa --disable-shared --disable-mediactrl --with-opengl --with-macosx-version-min=10.9 --with-cxx=17 --prefix=$WX_CACHE_FULLPATH --disable-precomp-headers --with-libpng=builtin --without-libtiff --with-libjpeg=builtin && make -j2 && make install
