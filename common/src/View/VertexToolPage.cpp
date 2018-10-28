fffbf6e471a3a48e92d58935c48857c17ea2b6aa
Eric Wasylishen <ewasylishen@gmail.com>
2018-10-28 01:48:19 -0600

    2351: add vertex tool page

---
+++ common/src/View/VertexToolPage.cpp
@@ -0,0 +1,45 @@
+/*
+ Copyright (C) 2010-2017 Kristian Duske
+ 
+ This file is part of TrenchBroom.
+ 
+ TrenchBroom is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ TrenchBroom is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with TrenchBroom. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "VertexToolPage.h"
+
+#include "TrenchBroom.h"
+#include "View/MapDocument.h"
+#include "View/ViewConstants.h"
+
+namespace TrenchBroom {
+    namespace View {
+        VertexToolPage::VertexToolPage(wxWindow* parent, MapDocumentWPtr document) :
+        wxPanel(parent),
+        m_document(document) {
+            createGui();
+        }
+
+        void VertexToolPage::createGui() {
+            MapDocumentSPtr document = lock(m_document);
+
+            m_uvLockCheckBox = new wxCheckBox(this, wxID_ANY, "UV Lock");
+
+            wxBoxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
+            sizer->Add(m_uvLockCheckBox, 0, wxALIGN_CENTER_VERTICAL);
+
+            SetSizer(sizer);
+        }
+    }
+}
---
+++ common/src/View/VertexToolPage.h
@@ -0,0 +1,45 @@
+/*
+ Copyright (C) 2010-2017 Kristian Duske
+ 
+ This file is part of TrenchBroom.
+ 
+ TrenchBroom is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ TrenchBroom is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with TrenchBroom. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef TrenchBroom_VertexToolPage
+#define TrenchBroom_VertexToolPage
+
+#include "TrenchBroom.h"
+#include "View/ViewTypes.h"
+
+#include <wx/panel.h>
+
+class wxCheckBox;
+
+namespace TrenchBroom {
+    namespace View {
+        class VertexToolPage : public wxPanel {
+        private:
+            MapDocumentWPtr m_document;
+
+            wxCheckBox* m_uvLockCheckBox;
+        public:
+            VertexToolPage(wxWindow* parent, MapDocumentWPtr document);
+        private:
+            void createGui();
+        };
+    }
+}
+
+#endif /* defined(TrenchBroom_VertexToolPage) */
