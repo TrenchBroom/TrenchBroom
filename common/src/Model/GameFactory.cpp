92396df8d6e8d31ed4bea8059e8de1c3fb096ade
Matthew Borkowski <matthew.h.borkowski@gmail.com>
2019-01-04 15:33:11 -0500

    Add new subclass of WritableDiskFileSystem that writes to temporary files first then moves them, and use this for writing all game config files. (#2506)

--- common/src/Model/GameFactory.cpp
+++ common/src/Model/GameFactory.cpp
@@ -147,9 +147,9 @@ namespace TrenchBroom {
             const IO::Path userGameDir = IO::SystemPaths::userDataDirectory() + IO::Path("games");
             if (IO::Disk::directoryExists(resourceGameDir)) {
                 auto resourceFS = std::make_unique<IO::DiskFileSystem>(resourceGameDir);
-                m_configFS = std::make_unique<IO::WritableDiskFileSystem>(std::move(resourceFS), userGameDir, true);
+                m_configFS = std::make_unique<IO::WritableDiskFileSystemWithTemporaries>(std::move(resourceFS), userGameDir, true);
             } else {
-                m_configFS = std::make_unique<IO::WritableDiskFileSystem>(userGameDir, true);
+                m_configFS = std::make_unique<IO::WritableDiskFileSystemWithTemporaries>(userGameDir, true);
             }
         }
 
