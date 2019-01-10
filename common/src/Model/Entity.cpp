ab1ccbf93371942f5d8e6e7b3e2bb9cda97fa13e
Kristian Duske <kristian.duske@gmail.com>
2019-01-10 23:28:45 +0100

    2525: Handle nan in entity origin property.

--- common/src/Model/Entity.cpp
+++ common/src/Model/Entity.cpp
@@ -99,7 +99,10 @@ namespace TrenchBroom {
         }
 
         void Entity::cacheAttributes() {
-            m_cachedOrigin = vm::vec3::parse(attribute(AttributeNames::Origin, ""));
+            m_cachedOrigin = vm::vec3::parse(attribute(AttributeNames::Origin, ""), vm::vec3::zero);
+            if (vm::isNaN(m_cachedOrigin)) {
+                m_cachedOrigin = vm::vec3::zero;
+            }
             m_cachedRotation = EntityRotationPolicy::getRotation(this);
         }
 
