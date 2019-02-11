e94136c2a645082b2720dccde2156327a97f97d1
Eric Wasylishen <ewasylishen@gmail.com>
2019-02-10 17:53:28 -0700

    2587: update about dialog

--- common/src/View/AboutDialog.cpp
+++ common/src/View/AboutDialog.cpp
@@ -99,6 +99,7 @@ namespace TrenchBroom {
                                         "FreeImage (Image loading & manipulation library)\n"
                                         "GLEW (OpenGL extension library)\n"
                                         "tinyxml2 (XML parsing library)\n"
+                                        "miniz (Archive library)\n"
                                         "Google Test (C++ testing framework)\n"
                                         "Google Mock (C++ mocking framework)\n"
                                         "StackWalker (C++ stack trace analyzer)\n"
