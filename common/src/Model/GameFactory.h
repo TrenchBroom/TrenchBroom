f4237c61299e1135472610b30bda9841064ca094
Kristian Duske <kristian.duske@gmail.com>
2019-02-26 21:14:57 +0100

    2623: Don’t crash when loading game configs fails.

--- common/src/Model/GameFactory.h
+++ common/src/Model/GameFactory.h
@@ -1,18 +1,18 @@
 /*
  Copyright (C) 2010-2017 Kristian Duske
- 
+
  This file is part of TrenchBroom.
- 
+
  TrenchBroom is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
- 
+
  TrenchBroom is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
- 
+
  You should have received a copy of the GNU General Public License
  along with TrenchBroom. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -32,11 +32,11 @@
 
 namespace TrenchBroom {
     class Logger;
-    
+
     namespace IO {
         class WritableDiskFileSystem;
     }
-    
+
     namespace Model {
         class GameFactory {
         private:
@@ -51,13 +51,15 @@ namespace TrenchBroom {
             mutable GamePathMap m_defaultEngines;
         public:
             ~GameFactory();
-            
+
             static GameFactory& instance();
-            
+
+            void initialize();
+
             const StringList& gameList() const;
             size_t gameCount() const;
             GameSPtr createGame(const String& gameName, Logger& logger);
-            
+
             StringList fileFormats(const String& gameName) const;
             IO::Path iconPath(const String& gameName) const;
             IO::Path gamePath(const String& gameName) const;
@@ -66,16 +68,17 @@ namespace TrenchBroom {
 
             GameConfig& gameConfig(const String& gameName);
             const GameConfig& gameConfig(const String& gameName) const;
-            
+
             std::pair<String, MapFormat> detectGame(const IO::Path& path) const;
         private:
             GameFactory();
             void initializeFileSystem();
             void loadGameConfigs();
             void loadGameConfig(const IO::Path& path);
+            void doLoadGameConfig(const IO::Path& path);
             void loadCompilationConfig(GameConfig& gameConfig);
             void loadGameEngineConfig(GameConfig& gameConfig);
-            
+
             void writeCompilationConfigs();
             void writeCompilationConfig(const GameConfig& gameConfig);
 
