139f0555da0634e8890520880b4e83f9155aabe3
Kristian Duske <kristian.duske@gmail.com>
2019-03-23 12:01:49 +0100

    2669: Run cppcheck if installed

---
+++ cmake/CppCheck.cmake
@@ -0,0 +1,25 @@
+LIST(APPEND CPPCHECK_ARGS
+    --enable=warning,performance,portability
+    --verbose
+    --suppressions-list=${CMAKE_SOURCE_DIR}/cppcheck.suppr
+    --std=c++11
+    --language=c++
+    --error-exitcode=1
+    -DMAIN=main
+    -I ${VECMATH_INCLUDE_DIR}
+    ${COMMON_SOURCE_DIR}
+)
+
+FIND_FILE(CPPCHECK_EXE cppcheck
+)
+
+IF (${CPPCHECK_EXE-NOTFOUND})
+    MESSAGE(STATUS "Could not find cppcheck, skipping checks")
+ELSE()
+    MESSAGE(STATUS "Using cppcheck found at ${CPPCHECK_EXE}")
+    ADD_CUSTOM_TARGET(
+        cppcheck
+        COMMAND ${CPPCHECK_EXE} ${CPPCHECK_ARGS}
+        COMMENT "running cppcheck"
+    )
+ENDIF()
