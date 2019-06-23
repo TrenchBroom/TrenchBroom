55ddc73328ec47b060d4d45b437142e97b91f711
Kristian Duske <kristian.duske@gmail.com>
2019-06-23 14:33:53 +0200

    2041: fix appveyor build config

--- appveyor.bat
+++ appveyor.bat
@@ -3,7 +3,7 @@ PATH=%PATH%;C:\Program Files (x86)\Pandoc;C:\Program Files\Cppcheck
 mkdir cmakebuild
 cd cmakebuild
 
-cmake .. -G"Visual Studio 15 2017" -T v141_xp -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="C:\Qt\5.12.1\msvc2017"
+cmake .. -G"Visual Studio 15 2017" -DCMAKE_PREFIX_PATH="%QT5_INSTALL_DIR%" -T v141_xp -DCMAKE_BUILD_TYPE=Release
 
 REM  -DCMAKE_CXX_FLAGS=/WX
 
