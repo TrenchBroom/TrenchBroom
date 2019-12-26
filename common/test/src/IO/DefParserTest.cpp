6451cb45dbbdd16e5e43675cf018fdb0d8f53605
Eric Wasylishen <ewasylishen@gmail.com>
2019-12-26 12:28:22 -0700

    2885: fix unity build on Windows (#2886)
    * 2885: fix unity build on Windows
    
    * 2885: fix cppcheck log not displaying on macOS CI
    
    * Revert "2885: fix cppcheck log not displaying on macOS CI"
    
    This reverts commit 3523520da950a4be3a4f0d39cf0d4407b0961b11.
    
    * 2885: formatting changes

--- common/test/src/IO/DefParserTest.cpp
+++ common/test/src/IO/DefParserTest.cpp
@@ -356,7 +356,7 @@ namespace TrenchBroom {
             kdl::vec_clear_and_delete(definitions);
         }
 
-        static const std::string ModelDefinitionTemplate =
+        static const std::string DefModelDefinitionTemplate =
         "/*QUAKED monster_zombie (1.0 0.0 0.0) (-16 -16 -24) (16 16 32) Crucified ambush\n"
         "{\n"
         "model(${MODEL});\n"
@@ -370,10 +370,10 @@ namespace TrenchBroom {
 
             assertModelDefinition<DefParser>(Assets::ModelSpecification(IO::Path("maps/b_shell0.bsp")),
                                              ModelDefinition,
-                                             ModelDefinitionTemplate);
+                                             DefModelDefinitionTemplate);
             assertModelDefinition<DefParser>(Assets::ModelSpecification(IO::Path("maps/b_shell1.bsp")),
                                              ModelDefinition,
-                                             ModelDefinitionTemplate,
+                                             DefModelDefinitionTemplate,
                                              "{ 'spawnflags': 1 }");
         }
 
@@ -382,11 +382,11 @@ namespace TrenchBroom {
 
             assertModelDefinition<DefParser>(Assets::ModelSpecification(IO::Path("maps/b_shell1.bsp")),
                                              ModelDefinition,
-                                             ModelDefinitionTemplate,
+                                             DefModelDefinitionTemplate,
                                              "{ 'model': 'maps/b_shell1.bsp' }");
             assertModelDefinition<DefParser>(Assets::ModelSpecification(IO::Path("maps/b_shell1.bsp"), 1, 2),
                                              ModelDefinition,
-                                             ModelDefinitionTemplate,
+                                             DefModelDefinitionTemplate,
                                              "{ 'model': 'maps/b_shell1.bsp', 'skin': 1, 'frame': 2 }");
         }
 
@@ -395,14 +395,14 @@ namespace TrenchBroom {
 
             assertModelDefinition<DefParser>(Assets::ModelSpecification(IO::Path("maps/b_shell0.bsp")),
                                              ModelDefinition,
-                                             ModelDefinitionTemplate);
+                                             DefModelDefinitionTemplate);
             assertModelDefinition<DefParser>(Assets::ModelSpecification(IO::Path("maps/b_shell1.bsp")),
                                              ModelDefinition,
-                                             ModelDefinitionTemplate,
+                                             DefModelDefinitionTemplate,
                                              "{ 'spawnflags': 1 }");
             assertModelDefinition<DefParser>(Assets::ModelSpecification(IO::Path("maps/b_shell0.bsp")),
                                              ModelDefinition,
-                                             ModelDefinitionTemplate,
+                                             DefModelDefinitionTemplate,
                                              "{ 'spawnflags': 2 }");
         }
 
@@ -411,11 +411,11 @@ namespace TrenchBroom {
 
             assertModelDefinition<DefParser>(Assets::ModelSpecification(IO::Path("maps/b_shell1.bsp")),
                                              ModelDefinition,
-                                             ModelDefinitionTemplate,
+                                             DefModelDefinitionTemplate,
                                              "{ 'model': 'maps/b_shell1.bsp' }");
             assertModelDefinition<DefParser>(Assets::ModelSpecification(IO::Path("maps/b_shell1.bsp"), 1, 2),
                                              ModelDefinition,
-                                             ModelDefinitionTemplate,
+                                             DefModelDefinitionTemplate,
                                              "{ 'model': 'maps/b_shell1.bsp', 'skin': 1, 'frame': 2 }");
         }
 
