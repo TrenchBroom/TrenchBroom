31b04411c7f1be8712527b6368d7987aafd5f1ed
Kristian Duske <kristian.duske@gmail.com>
2019-12-27 20:16:43 +0100

    2841: rename glob_index to compact_trie, add documentation, minor changes

--- lib/kdl/include/kdl/string_compare_detail.h
+++ lib/kdl/include/kdl/string_compare_detail.h
@@ -148,12 +148,12 @@ namespace kdl {
      */
     template <typename CharEqual>
     bool str_matches_glob(const std::string_view& str, const std::string_view& pattern, const CharEqual& char_equal) {
-        using match_state = std::pair<std::size_t, std::size_t>;
-        std::vector<match_state> match_states({{ 0u, 0u }});
+        using match_task = std::pair<std::size_t, std::size_t>;
+        std::vector<match_task> match_tasks({{ 0u, 0u }});
 
-        while (!match_states.empty()) {
-            const auto [s_i, p_i] = match_states.back();
-            match_states.pop_back();
+        while (!match_tasks.empty()) {
+            const auto [s_i, p_i] = match_tasks.back();
+            match_tasks.pop_back();
 
             if (s_i == str.length() && p_i == pattern.length()) {
                 // both the string and the pattern are consumed, so we have a match
@@ -173,7 +173,7 @@ namespace kdl {
                     if (n == '*' || n == '?' || n == '%' || n == '\\') {
                         if (str[s_i] == n) {
                             // the string matches the escaped character, continue to match
-                            match_states.emplace_back(s_i + 1u, p_i + 2u);
+                            match_tasks.emplace_back(s_i + 1u, p_i + 2u);
                         }
                     } else {
                         // invalid escape sequence in pattern
@@ -189,40 +189,40 @@ namespace kdl {
 
                 if (s_i == str.length()) {
                     // the string is consumed, continue matching at the next char in the pattern
-                    match_states.emplace_back(s_i, p_i + 1u);
+                    match_tasks.emplace_back(s_i, p_i + 1u);
                 } else {
                     // '*' matches any character
                     // consume the '*' and continue matching at the current character of the string
-                    match_states.emplace_back(s_i, p_i + 1u);
+                    match_tasks.emplace_back(s_i, p_i + 1u);
                     // consume the current character of the string and continue matching at '*'
-                    match_states.emplace_back(s_i + 1u, p_i);
+                    match_tasks.emplace_back(s_i + 1u, p_i);
                 }
             } else if (pattern[p_i] == '?') {
                 // handle '?' in the pattern
                 if (s_i < str.length()) {
                     // '?' matches any character, continue at the next chars in both the pattern and the string
-                    match_states.emplace_back(s_i + 1u, p_i + 1u);
+                    match_tasks.emplace_back(s_i + 1u, p_i + 1u);
                 }
             } else if (pattern[p_i] == '%') {
                 // handle '%' in the pattern
                 if (p_i < pattern.length() - 1u && pattern[p_i + 1u] == '*') {
                     // handle "%*" in the pattern
                     // try to continue matching after "%*"
-                    match_states.emplace_back(s_i, p_i + 2u);
+                    match_tasks.emplace_back(s_i, p_i + 2u);
                     if (s_i < str.length() && str[s_i] >= '0' && str[s_i] <= '9') {
                         // try to match more digits
-                        match_states.emplace_back(s_i + 1u, p_i);
+                        match_tasks.emplace_back(s_i + 1u, p_i);
                     }
                 } else if (s_i < str.length()) {
                     // handle '%' in the pattern (not followed by '*')
                     if (str[s_i] >= '0' && str[s_i] <= '9') {
                         // continue matching after the digit
-                        match_states.emplace_back(s_i + 1u, p_i + 1u);
+                        match_tasks.emplace_back(s_i + 1u, p_i + 1u);
                     }
                 }
             } else if (s_i < str.length() && char_equal(pattern[p_i], str[s_i])) {
                 // handle a regular character in the pattern
-                match_states.emplace_back(s_i + 1u, p_i + 1u);
+                match_tasks.emplace_back(s_i + 1u, p_i + 1u);
             }
         }
 
