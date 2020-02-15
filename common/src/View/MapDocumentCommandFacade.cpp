87daac7a050b2728b2764eead2701ca02e7dc028
Eric Wasylishen <ewasylishen@gmail.com>
2020-02-15 12:42:49 -0700

    3001: undo allowing unselected brushes to be resized

--- common/src/View/MapDocumentCommandFacade.cpp
+++ common/src/View/MapDocumentCommandFacade.cpp
@@ -687,7 +687,7 @@ namespace TrenchBroom {
 
             for (auto* face : faces) {
                 auto* brush = face->brush();
-                //assert(brush->selected());
+                assert(brush->selected());
                 brush->moveBoundary(m_worldBounds, face, delta, pref(Preferences::TextureLock));
                 result.push_back(face->polygon());
             }
