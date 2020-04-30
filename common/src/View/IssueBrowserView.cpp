88db721f7570b16ec9cd458e5bc66eeabf89efd7
Eric Wasylishen <ewasylishen@gmail.com>
2020-04-30 12:05:33 -0600

    3142: fix crash when fixing empty attribute name issues (#3147)
    * 3142: fix crash when fixing empty attribute name issues
    
    Fixes #3142
    
    Also added a test of the issue generator. Doesn't actually test the cause of the crash
    but I thought the problem was something else until I finished the test.
    
    * 3142: reserve capacity in vector_set. add reserve method.
    
    * 3142: add cast

--- common/src/View/IssueBrowserView.cpp
+++ common/src/View/IssueBrowserView.cpp
@@ -28,6 +28,7 @@
 
 #include <kdl/memory_utils.h>
 #include <kdl/vector_utils.h>
+#include <kdl/vector_set.h>
 
 #include <vector>
 
@@ -154,14 +155,18 @@ namespace TrenchBroom {
         }
 
         std::vector<Model::Issue*> IssueBrowserView::collectIssues(const QList<QModelIndex>& indices) const {
-            std::vector<Model::Issue*> result;
+            // Use a vector_set to filter out duplicates.
+            // The QModelIndex list returned by getSelection() contains duplicates
+            // (not sure why, current row and selected row?)
+            kdl::vector_set<Model::Issue*> result;
+            result.reserve(static_cast<size_t>(indices.size()));
             for (QModelIndex index : indices) {
                 if (index.isValid()) {
                     const auto row = static_cast<size_t>(index.row());
-                    result.push_back(m_tableModel->issues().at(row));
+                    result.insert(m_tableModel->issues().at(row));
                 }
             }
-            return result;
+            return result.release_data();
         }
 
         std::vector<Model::IssueQuickFix*> IssueBrowserView::collectQuickFixes(const QList<QModelIndex>& indices) const {
