be8f285f0c58399d471bf6016ee050c40c508404
Eric Wasylishen <ewasylishen@gmail.com>
2020-05-07 13:27:04 -0600

    3193: fix ControlListBox colors on macOS

--- common/src/View/ControlListBox.cpp
+++ common/src/View/ControlListBox.cpp
@@ -57,11 +57,20 @@ namespace TrenchBroom {
         void ControlListBoxItemRenderer::updateItem() {}
 
         void ControlListBoxItemRenderer::setSelected(const bool selected, const QListWidget* listWidget) {
-            if (selected) {
-                setHighlightWindowColor(this);
-            } else {
-                setBaseWindowColor(this);
-            }
+            QPalette backgroundPalette;
+            backgroundPalette.setColor(QPalette::Active,   QPalette::Highlight, listWidget->palette().color(QPalette::Active,   QPalette::Highlight));
+            backgroundPalette.setColor(QPalette::Inactive, QPalette::Highlight, listWidget->palette().color(QPalette::Inactive, QPalette::Highlight));
+            backgroundPalette.setColor(QPalette::Disabled, QPalette::Highlight, listWidget->palette().color(QPalette::Disabled, QPalette::Highlight));
+
+            backgroundPalette.setColor(QPalette::Active,   QPalette::Base,      listWidget->palette().color(QPalette::Active,   QPalette::Base));
+            backgroundPalette.setColor(QPalette::Inactive, QPalette::Base,      listWidget->palette().color(QPalette::Inactive, QPalette::Base));
+            backgroundPalette.setColor(QPalette::Disabled, QPalette::Base,      listWidget->palette().color(QPalette::Disabled, QPalette::Base));
+            setPalette(backgroundPalette);
+            // macOS: we'd prefer setPalette(listWidget->palette()); but this doesn't work, whereas the above does.
+            // FIXME: the above setPalette call should be removed once we stop using QListWidget and make ControlListBox
+            // a standalone widget.
+
+            setBackgroundRole(selected ? QPalette::Highlight : QPalette::Base);
 
             // by default, we just change the appearance of all labels
             auto children = findChildren<QLabel*>();
@@ -70,11 +79,23 @@ namespace TrenchBroom {
                 if (dontUpdate.isValid() && dontUpdate.canConvert(QMetaType::Bool) && dontUpdate.toBool()) {
                     continue;
                 }
-                if (selected) {
-                    makeSelected(child, listWidget->palette());
-                } else {
-                    makeUnselected(child, listWidget->palette());
-                }
+
+                // The label colorRole automatically updates from QPalette::Text to QPalette::HighlightedText.
+                // However the macOS palette is different on listWidget and the app default QPalette, so
+                // we need to transfer the listWidget palette to the QLabel for good contrast.
+
+                QPalette labelPalette;
+                labelPalette.setColor(QPalette::Active,   QPalette::HighlightedText, listWidget->palette().color(QPalette::Active,   QPalette::HighlightedText));
+                labelPalette.setColor(QPalette::Inactive, QPalette::HighlightedText, listWidget->palette().color(QPalette::Inactive, QPalette::HighlightedText));
+                labelPalette.setColor(QPalette::Disabled, QPalette::HighlightedText, listWidget->palette().color(QPalette::Disabled, QPalette::HighlightedText));
+
+                labelPalette.setColor(QPalette::Active,   QPalette::Text,            listWidget->palette().color(QPalette::Active,   QPalette::Text));
+                labelPalette.setColor(QPalette::Inactive, QPalette::Text,            listWidget->palette().color(QPalette::Inactive, QPalette::Text));
+                labelPalette.setColor(QPalette::Disabled, QPalette::Text,            listWidget->palette().color(QPalette::Disabled, QPalette::Text));
+                child->setPalette(labelPalette);
+                // macOS: we'd prefer child->setPalette(listWidget->palette()); but this doesn't work, whereas the above does.
+                // FIXME: the above setPalette call should be removed once we stop using QListWidget and make ControlListBox
+                // a standalone widget.
             }
         }
 
