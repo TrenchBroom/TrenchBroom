7b706c4b2dfad2fb5e20d4a5d0e75485dcc69242
Kristian Duske <kristian.duske@gmail.com>
2020-05-19 07:43:47 +0200

    Rename BrushFaceSnapshot::m_attribs

--- common/src/Model/BrushFaceSnapshot.cpp
+++ common/src/Model/BrushFaceSnapshot.cpp
@@ -27,14 +27,14 @@ namespace TrenchBroom {
     namespace Model {
         BrushFaceSnapshot::BrushFaceSnapshot(BrushNode* node, const BrushFace* face) :
         m_faceRef(node, face),
-        m_attribs(face->attributes().takeSnapshot()),
+        m_faceAttributes(face->attributes().takeSnapshot()),
         m_coordSystemSnapshot(face->takeTexCoordSystemSnapshot()) {}
 
         BrushFaceSnapshot::~BrushFaceSnapshot() = default;
 
         void BrushFaceSnapshot::restore() {
             const auto faceHandle = m_faceRef.resolve();
-            faceHandle.face()->setAttributes(m_attribs);
+            faceHandle.face()->setAttributes(m_faceAttributes);
             if (m_coordSystemSnapshot != nullptr) {
                 faceHandle.face()->restoreTexCoordSystemSnapshot(*m_coordSystemSnapshot);
             }
