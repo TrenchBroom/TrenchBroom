c2f03ccccade9f3b7d23ec310d96da1a2974d046
Eric Wasylishen <ewasylishen@gmail.com>
2020-05-28 16:52:36 -0600

    206: cherry-pick ChangeEntityAttributesCommand changes from layers branch

--- common/src/View/ChangeEntityAttributesCommand.cpp
+++ common/src/View/ChangeEntityAttributesCommand.cpp
@@ -19,6 +19,7 @@
 
 #include "ChangeEntityAttributesCommand.h"
 
+#include "Ensure.h"
 #include "Macros.h"
 #include "Model/EntityAttributeSnapshot.h"
 #include "View/MapDocument.h"
@@ -29,22 +30,45 @@ namespace TrenchBroom {
         const Command::CommandType ChangeEntityAttributesCommand::Type = Command::freeType();
 
         std::unique_ptr<ChangeEntityAttributesCommand> ChangeEntityAttributesCommand::set(const std::string& name, const std::string& value) {
-            auto command = std::make_unique<ChangeEntityAttributesCommand>(Action::Set);
+            auto command = std::make_unique<ChangeEntityAttributesCommand>(Action::Set, Target::SelectedNodes);
             command->setName(name);
             command->setNewValue(value);
             return command;
         }
 
         std::unique_ptr<ChangeEntityAttributesCommand> ChangeEntityAttributesCommand::remove(const std::string& name) {
-            auto command = std::make_unique<ChangeEntityAttributesCommand>(Action::Remove);
+            auto command = std::make_unique<ChangeEntityAttributesCommand>(Action::Remove, Target::SelectedNodes);
             command->setName(name);
             return command;
         }
 
         std::unique_ptr<ChangeEntityAttributesCommand> ChangeEntityAttributesCommand::rename(const std::string& oldName, const std::string& newName) {
-            auto command = std::make_unique<ChangeEntityAttributesCommand>(Action::Rename);
+            auto command = std::make_unique<ChangeEntityAttributesCommand>(Action::Rename, Target::SelectedNodes);
             command->setName(oldName);
             command->setNewName(newName);
+            return command;
+        }
+
+        std::unique_ptr<ChangeEntityAttributesCommand> ChangeEntityAttributesCommand::setForNodes(const std::vector<Model::AttributableNode*>& nodes, const std::string& name, const std::string& value) {
+            auto command = std::make_unique<ChangeEntityAttributesCommand>(Action::Set, Target::NodeList);
+            command->setName(name);
+            command->setNewValue(value);
+            command->setTargetNodes(nodes);
+            return command;
+        }
+
+        std::unique_ptr<ChangeEntityAttributesCommand> ChangeEntityAttributesCommand::removeForNodes(const std::vector<Model::AttributableNode*>& nodes, const std::string& name) {
+            auto command = std::make_unique<ChangeEntityAttributesCommand>(Action::Remove, Target::NodeList);
+            command->setName(name);
+            command->setTargetNodes(nodes);
+            return command;
+        }
+
+        std::unique_ptr<ChangeEntityAttributesCommand> ChangeEntityAttributesCommand::renameForNodes(const std::vector<Model::AttributableNode*>& nodes, const std::string& oldName, const std::string& newName) {
+            auto command = std::make_unique<ChangeEntityAttributesCommand>(Action::Rename, Target::NodeList);
+            command->setName(oldName);
+            command->setNewName(newName);
+            command->setTargetNodes(nodes);
             return command;
         }
 
@@ -62,9 +86,15 @@ namespace TrenchBroom {
             m_newValue = newValue;
         }
 
-        ChangeEntityAttributesCommand::ChangeEntityAttributesCommand(const Action action) :
+        void ChangeEntityAttributesCommand::setTargetNodes(const std::vector<Model::AttributableNode*>& nodes) {
+            assert(m_target == Target::NodeList);
+            m_targetNodes = nodes;
+        }
+
+        ChangeEntityAttributesCommand::ChangeEntityAttributesCommand(const Action action, const Target target) :
         DocumentCommand(Type, makeName(action)),
-        m_action(action) {}
+        m_action(action),
+        m_target(target) {}
 
         ChangeEntityAttributesCommand::~ChangeEntityAttributesCommand() = default;
 
@@ -81,21 +111,39 @@ namespace TrenchBroom {
         }
 
         std::unique_ptr<CommandResult> ChangeEntityAttributesCommand::doPerformDo(MapDocumentCommandFacade* document) {
-            switch (m_action) {
-                case Action::Set:
-                    m_snapshots = document->performSetAttribute(m_oldName, m_newValue);
+            switch (m_target) {
+                case Target::SelectedNodes:
+                    switch (m_action) {
+                        case Action::Set:
+                            m_snapshots =    document->performSetAttribute(m_oldName, m_newValue);
+                            break;
+                        case Action::Remove:
+                            m_snapshots = document->performRemoveAttribute(m_oldName);
+                            break;
+                        case Action::Rename:
+                            m_snapshots = document->performRenameAttribute(m_oldName, m_newName);
+                            break;
+                    }
                     break;
-                case Action::Remove:
-                    m_snapshots = document->performRemoveAttribute(m_oldName);
+                case Target::NodeList:
+                    switch (m_action) {
+                        case Action::Set:
+                            m_snapshots =    document->performSetAttributeForNodes(m_targetNodes, m_oldName, m_newValue);
+                            break;
+                        case Action::Remove:
+                            m_snapshots = document->performRemoveAttributeForNodes(m_targetNodes, m_oldName);
+                            break;
+                        case Action::Rename:
+                            m_snapshots = document->performRenameAttributeForNodes(m_targetNodes, m_oldName, m_newName);
+                            break;
+                    }
                     break;
-                case Action::Rename:
-                    m_snapshots = document->performRenameAttribute(m_oldName, m_newName);
-                    break;
-            };
+            }
             return std::make_unique<CommandResult>(true);
         }
 
         std::unique_ptr<CommandResult> ChangeEntityAttributesCommand::doPerformUndo(MapDocumentCommandFacade* document) {
+            // NOTE: This is the same whether m_target is Target::SelectedNodes or Target::NodeList
             document->restoreAttributes(m_snapshots);
             m_snapshots.clear();
             return std::make_unique<CommandResult>(true);
@@ -106,10 +154,22 @@ namespace TrenchBroom {
         }
 
         bool ChangeEntityAttributesCommand::doCollateWith(UndoableCommand* command) {
-            ChangeEntityAttributesCommand* other = static_cast<ChangeEntityAttributesCommand*>(command);
+            auto* other = dynamic_cast<ChangeEntityAttributesCommand*>(command);            
+            ensure(other != nullptr, "attempted to collate with wrong node type");
+
+            // cppcheck-suppress nullPointerRedundantCheck
             if (other->m_action != m_action) {
                 return false;
             }
+            // cppcheck-suppress nullPointerRedundantCheck
+            if (other->m_target != m_target) {
+                return false;
+            }
+            // cppcheck-suppress nullPointerRedundantCheck
+            if (other->m_targetNodes != m_targetNodes) {
+                return false;
+            }
+            // cppcheck-suppress nullPointerRedundantCheck
             if (other->m_oldName != m_oldName) {
                 return false;
             }
--- common/src/View/MapDocumentCommandFacade.h
+++ common/src/View/MapDocumentCommandFacade.h
@@ -90,10 +90,13 @@ namespace TrenchBroom {
         public: // entity attributes
             using EntityAttributeSnapshotMap = std::map<Model::AttributableNode*, std::vector<Model::EntityAttributeSnapshot>>;
             EntityAttributeSnapshotMap performSetAttribute(const std::string& name, const std::string& value);
+            EntityAttributeSnapshotMap performSetAttributeForNodes(const std::vector<Model::AttributableNode*>& nodes, const std::string& name, const std::string& value);
             EntityAttributeSnapshotMap performRemoveAttribute(const std::string& name);
+            EntityAttributeSnapshotMap performRemoveAttributeForNodes(const std::vector<Model::AttributableNode*>& nodes, const std::string& name);
             EntityAttributeSnapshotMap performUpdateSpawnflag(const std::string& name, const size_t flagIndex, const bool setFlag);
             EntityAttributeSnapshotMap performConvertColorRange(const std::string& name, Assets::ColorRange::Type colorRange);
             EntityAttributeSnapshotMap performRenameAttribute(const std::string& oldName, const std::string& newName);
+            EntityAttributeSnapshotMap performRenameAttributeForNodes(const std::vector<Model::AttributableNode*>& nodes, const std::string& oldName, const std::string& newName);
             void restoreAttributes(const EntityAttributeSnapshotMap& attributes);
         public: // brush resizing
             std::vector<vm::polygon3> performResizeBrushes(const std::vector<vm::polygon3>& polygons, const vm::vec3& delta);
