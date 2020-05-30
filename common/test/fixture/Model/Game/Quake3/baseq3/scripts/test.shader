369c033d0a681adcc905f247d04aae43cae7d8b9
Kristian Duske <kristian.duske@gmail.com>
2020-05-30 21:28:39 +0200

    Find texture collections recursively

--- common/test/fixture/Model/Game/Quake3/baseq3/scripts/test.shader
+++ common/test/fixture/Model/Game/Quake3/baseq3/scripts/test.shader
@@ -23,3 +23,9 @@ textures/test/not_existing2 // does not override an existing name
 {
     // no editor image
 }
+
+textures/skies/hub1/dusk // is in a subdirectory
+{
+    // has existing editor image
+    qer_editorimage textures/test/editor_image.jpg
+}
--- common/test/src/Model/GameTest.cpp
+++ common/test/src/Model/GameTest.cpp
@@ -22,6 +22,7 @@
 #include "GTestCompat.h"
 
 #include "Logger.h"
+#include "TestUtils.h"
 #include "Assets/Texture.h"
 #include "Assets/TextureCollection.h"
 #include "Assets/TextureManager.h"
@@ -38,6 +39,40 @@
 
 namespace TrenchBroom {
     namespace Model {
+        TEST_CASE("GameTest.findTextureCollections", "[GameTest]") {
+            auto config = GameConfig(
+                "Quake2",
+                IO::Path(),
+                IO::Path(),
+                false,
+                std::vector<MapFormatConfig>(),
+                FileSystemConfig(
+                    IO::Path("baseq2"),
+                    PackageFormatConfig()
+                ),
+                TextureConfig(
+                    TexturePackageConfig(IO::Path("textures")),
+                    PackageFormatConfig("wal", "wal"),
+                    IO::Path("pics/colormap.pcx"),
+                    "_tb_textures",
+                    IO::Path(),
+                    std::vector<std::string>()
+                ),
+                EntityConfig(),
+                FaceAttribsConfig(),
+                std::vector<SmartTag>());
+            const auto gamePath = IO::Disk::getCurrentWorkingDir() + IO::Path("fixture/test/Model/Game/Quake2");
+            auto logger = NullLogger();
+            GameImpl game(config, gamePath, logger);
+            
+            EXPECT_COLLECTIONS_EQUIVALENT(std::vector<IO::Path>({
+                IO::Path("textures"),
+                IO::Path("textures/e1m1"),
+                IO::Path("textures/e1m1/f1")
+            }),
+            game.findTextureCollections());
+        }
+        
         TEST_CASE("GameTest.loadCorruptPackages", "[GameTest]") {
             // https://github.com/kduske/TrenchBroom/issues/2496
 
@@ -71,46 +106,68 @@ namespace TrenchBroom {
             auto game = GameImpl(config, gamePath, logger);
 
             const auto textureCollections = game.findTextureCollections();
-            ASSERT_EQ(1u, textureCollections.size());
-            ASSERT_EQ(IO::Path("textures/test"), textureCollections.front());
+            EXPECT_COLLECTIONS_EQUIVALENT(std::vector<IO::Path>({
+                IO::Path("textures"),
+                IO::Path("textures/skies"),
+                IO::Path("textures/skies/hub1"),
+                IO::Path("textures/test"),
+            }), textureCollections);
 
             auto worldspawn = EntityNode();
-            worldspawn.addOrUpdateAttribute("_tb_textures", textureCollections.front().asString());
+            worldspawn.addOrUpdateAttribute("_tb_textures", "textures/test;textures/skies/hub1");
 
             auto textureManager = Assets::TextureManager(0, 0, logger);
             game.loadTextureCollections(worldspawn, IO::Path(), textureManager, logger);
 
-            ASSERT_EQ(1u, textureManager.collections().size());
+            ASSERT_EQ(2u, textureManager.collections().size());
 
             /*
-             * The shader script contains four entries:
+             * The shader script contains five entries:
              * textures/test/test overrides an existing texture and points it to an editor image
              * textures/test/not_existing does not override an existing texture and points to an editor image
              * textures/test/test2 overrides an existing texture, but the editor image is missing
              * textures/test/not_existing2 does not override an existing texture, and no editor image
+             * textures/skies/hub1/dusk has a deeper directory structure, and has an editor image
+             *
+             * Due to the directory structure, the shader script induces four texture collections:
+             * - textures
+             * - textures/test
+             * - textures/skies
+             * - textures/skies/hub1
+             *
+             * Of these, we only load textures/test and textures/skies/hub1.
              *
              * The file system contains three textures:
              * textures/test/test.tga is overridden by the shader script
              * textures/test/test2.tga is overridden by the shader script
              * textures/test/editor_image.jpg is not overridden by a shader
              *
-             * In total, we expect the following entries:
+             * In total, we expect the following entries in texture collection textures/test:
              * test/test -> test/editor_image.jpg
              * test/not_existing -> test/editor_image.jpg
              * test/editor_image
              * test/not_existing2 -> __TB_empty.png
              * test/test2 -> __TB_empty.png
+             *
+             * and one entry in texture collection textures/skies/hub1:
+             * skies/hub1/dusk -> test/editor_image.jpg
              */
 
-            const auto* collection = textureManager.collections().front();
-            ASSERT_EQ(5u, collection->textureCount());
+            const auto* testCollection = textureManager.collections().front();
+            ASSERT_EQ(5u, testCollection->textureCount());
 
-            const auto& textures = collection->textures();
-            ASSERT_EQ(1, std::count_if(std::begin(textures), std::end(textures), [](const auto* t) { return t->name() == "test/test"; }));
-            ASSERT_EQ(1, std::count_if(std::begin(textures), std::end(textures), [](const auto* t) { return t->name() == "test/not_existing"; }));
-            ASSERT_EQ(1, std::count_if(std::begin(textures), std::end(textures), [](const auto* t) { return t->name() == "test/editor_image"; }));
-            ASSERT_EQ(1, std::count_if(std::begin(textures), std::end(textures), [](const auto* t) { return t->name() == "test/not_existing2"; }));
-            ASSERT_EQ(1, std::count_if(std::begin(textures), std::end(textures), [](const auto* t) { return t->name() == "test/test2"; }));
+            const auto& testTextures = testCollection->textures();
+            ASSERT_EQ(1, std::count_if(std::begin(testTextures), std::end(testTextures), [](const auto* t) { return t->name() == "test/test"; }));
+            ASSERT_EQ(1, std::count_if(std::begin(testTextures), std::end(testTextures), [](const auto* t) { return t->name() == "test/not_existing"; }));
+            ASSERT_EQ(1, std::count_if(std::begin(testTextures), std::end(testTextures), [](const auto* t) { return t->name() == "test/editor_image"; }));
+            ASSERT_EQ(1, std::count_if(std::begin(testTextures), std::end(testTextures), [](const auto* t) { return t->name() == "test/not_existing2"; }));
+            ASSERT_EQ(1, std::count_if(std::begin(testTextures), std::end(testTextures), [](const auto* t) { return t->name() == "test/test2"; }));
+            
+            const auto* skiesCollection = textureManager.collections().back();
+            ASSERT_EQ(1u, skiesCollection->textureCount());
+            
+            const auto& skiesTextures = testCollection->textures();
+            ASSERT_TRUE(skiesTextures.front()->name() == "test/editor_image");
         }
     }
 }
