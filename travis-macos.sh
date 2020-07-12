1d8891a8896c4806f00ef244f6abfc4120b13417
Eric Wasylishen <ewasylishen@gmail.com>
2020-07-12 14:18:08 -0600

    3335: don't run common-benchmark on debug builds (#3336)
    * 3335: don't run common-benchmark on debug builds
    
    Fixes #3335
    
    * 3335: fix typo

--- travis-macos.sh
+++ travis-macos.sh
@@ -55,8 +55,12 @@ cd "$BUILD_DIR/lib/kdl/test"
 cd "$BUILD_DIR/common/test"
 ./common-test || exit 1
 
-cd "$BUILD_DIR/common/benchmark"
-./common-benchmark || exit 1
+if [[ $TB_DEBUG_BUILD != "true" ]] ; then
+    cd "$BUILD_DIR/common/benchmark"
+    ./common-benchmark || exit 1
+else
+    echo "Skipping common-benmchark because this is a debug build"
+fi
 
 cd "$BUILD_DIR"
 
