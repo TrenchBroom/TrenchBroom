f025d2d686a15b8fcae72ea86fc04596acac3a4f
Kristian Duske <kristian.duske@gmail.com>
2020-11-15 20:43:40 +0100

    3485: Remove AttributableNode::hasNumberedAttribute

--- common/src/Model/AttributableNode.cpp
+++ common/src/Model/AttributableNode.cpp
@@ -122,10 +122,6 @@ namespace TrenchBroom {
             return definition == nullptr ? nullptr : definition->attributeDefinition(name);
         }
 
-        bool AttributableNode::hasNumberedAttribute(const std::string& prefix, const std::string& value) const {
-            return m_entity.hasNumberedAttribute(prefix, value);
-        }
-
         std::vector<EntityAttribute> AttributableNode::attributeWithName(const std::string& name) const {
             return m_entity.attributeWithName(name);
         }
