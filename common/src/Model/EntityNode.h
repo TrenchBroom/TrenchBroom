fda70a96665e32e0f85873962e8aa47008190054
Kristian Duske <kristian.duske@gmail.com>
2020-11-19 21:43:25 +0100

    3485: Remove EntityNode::hasPointEntityDefinition

--- common/src/Model/EntityNode.h
+++ common/src/Model/EntityNode.h
@@ -59,7 +59,6 @@ namespace TrenchBroom {
             EntityNode();
             EntityNode(Entity entity);
 
-            bool hasPointEntityDefinition() const;
             bool hasPointEntityModel() const;
 
             vm::bbox3 definitionBounds() const;
