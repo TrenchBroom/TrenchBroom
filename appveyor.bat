6e6aea362b7a9c8742a7196385a545f1d0b7d8d5
Eric Wasylishen <ewasylishen@gmail.com>
2020-12-12 11:56:01 -0700

    3628: fix appveyor succeeding on crashes
    Fixes #3628

--- appveyor.bat
+++ appveyor.bat
@@ -16,37 +16,37 @@ set CXXFLAGS="/WX"
 
 cmake .. -G"Visual Studio 16 2019" -T v142 -A "%TB_ARCH%" -DCMAKE_PREFIX_PATH="%QT5_INSTALL_DIR%" -DCMAKE_BUILD_TYPE=Release -DTB_SUPPRESS_PCH=1
 
-IF ERRORLEVEL 1 GOTO ERROR
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
 cmake --build . --config Release
 
-IF ERRORLEVEL 1 GOTO ERROR
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
 set BUILD_DIR="%cd%"
 
 cd lib\vecmath\test\Release
 vecmath-test.exe
-IF ERRORLEVEL 1 GOTO ERROR
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 cd "%BUILD_DIR%"
 
 cd lib\kdl\test\Release
 kdl-test.exe
-IF ERRORLEVEL 1 GOTO ERROR
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 cd "%BUILD_DIR%"
 
 cd common\test\Release
 common-test.exe
-IF ERRORLEVEL 1 GOTO ERROR
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 cd "%BUILD_DIR%"
 
 cd common\benchmark\Release
 common-benchmark.exe
-IF ERRORLEVEL 1 GOTO ERROR
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 cd "%BUILD_DIR%"
 
 cpack
 
-IF ERRORLEVEL 1 GOTO ERROR
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
 call generate_checksum.bat
 
