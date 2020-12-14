6f73d429cad5c75a550263be772912cee6a0bce6
Kristian Duske <kristian.duske@gmail.com>
2020-12-14 21:50:08 +0100

    3619: Rename AttributeNameWithDoubleQuotationMarksIssueGenerator files

--- common/src/Model/PropertyKeyWithDoubleQuotationMarksIssueGenerator.cpp
+++ common/src/Model/PropertyKeyWithDoubleQuotationMarksIssueGenerator.cpp
@@ -17,7 +17,7 @@
  along with TrenchBroom. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "AttributeNameWithDoubleQuotationMarksIssueGenerator.h"
+#include "PropertyKeyWithDoubleQuotationMarksIssueGenerator.h"
 
 #include "Model/BrushNode.h"
 #include "Model/Entity.h"
