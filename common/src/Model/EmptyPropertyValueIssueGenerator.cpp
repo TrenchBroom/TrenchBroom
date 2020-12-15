e7d0619c239cdfba183e2968308c9f6e23351579
Kristian Duske <kristian.duske@gmail.com>
2020-12-15 19:46:59 +0100

    3619: Rename EmptyAttributeNameIssueGenerator files

--- common/src/Model/EmptyPropertyValueIssueGenerator.cpp
+++ common/src/Model/EmptyPropertyValueIssueGenerator.cpp
@@ -17,7 +17,7 @@
  along with TrenchBroom. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "EmptyAttributeValueIssueGenerator.h"
+#include "EmptyPropertyValueIssueGenerator.h"
 
 #include "Model/Entity.h"
 #include "Model/EntityNode.h"
