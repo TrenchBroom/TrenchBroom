13377f04c744e037747bf693cef58763bbd3dc51
Kristian Duske <kristian.duske@gmail.com>
2020-12-15 21:16:03 +0100

    3619: Rename LongAttributeNameIssueGenerator files

--- common/src/Model/LongPropertyValueIssueGenerator.cpp
+++ common/src/Model/LongPropertyValueIssueGenerator.cpp
@@ -17,7 +17,7 @@
  along with TrenchBroom. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "LongAttributeValueIssueGenerator.h"
+#include "LongPropertyValueIssueGenerator.h"
 
 #include "Model/BrushNode.h"
 #include "Model/RemoveEntityAttributesQuickFix.h"
