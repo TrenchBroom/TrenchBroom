632e71cfc837022b4689a840e3d1ebb64721b547
Kristian Duske <kristian.duske@gmail.com>
2020-12-20 10:36:12 +0100

    3043: Remove GTest macros from collection_utils_test

--- lib/kdl/test/src/collection_utils_test.cpp
+++ lib/kdl/test/src/collection_utils_test.cpp
@@ -21,25 +21,23 @@
 
 #include "test_utils.h"
 
-#include "GTestCompat.h"
-
 #include <catch2/catch.hpp>
 
 namespace kdl {
     TEST_CASE("collection_utils_test.col_total_size", "[collection_utils_test]") {
         using vec = std::vector<int>;
 
-        ASSERT_EQ(0u, size(vec({})));
-        ASSERT_EQ(1u, size(vec({ 2 })));
-        ASSERT_EQ(2u, size(vec({ 2, 1 })));
-        ASSERT_EQ(2u, col_total_size(vec({ 2 }), vec({ 2 })));
-        ASSERT_EQ(3u, col_total_size(vec({ 2 }), vec({ 2, 1 })));
+        CHECK(size(vec({})) == 0u);
+        CHECK(size(vec({ 2 })) == 1u);
+        CHECK(size(vec({ 2, 1 })) == 2u);
+        CHECK(col_total_size(vec({ 2 }), vec({ 2 })) == 2u);
+        CHECK(col_total_size(vec({ 2 }), vec({ 2, 1 })) == 3u);
     }
 
     template <typename T>
     void test_range_remove_all(std::vector<T> col, const std::vector<T> rem, const std::vector<T> exp1) {
         auto it = range_remove_all(std::begin(col), std::end(col), std::begin(rem), std::end(rem));
-        ASSERT_EQ(exp1, std::vector<T>(std::begin(col), it));
+        CHECK(std::vector<T>(std::begin(col), it) == exp1);
     }
 
     TEST_CASE("collection_utils_test.range_remove_all", "[collection_utils_test]") {
@@ -55,14 +53,14 @@ namespace kdl {
         auto d = std::vector<deletable*>({ new deletable(d1), new deletable(d2), new deletable(d3) });
         range_delete_all(std::begin(d), std::end(d));
 
-        ASSERT_TRUE(d1);
-        ASSERT_TRUE(d2);
-        ASSERT_TRUE(d3);
+        CHECK(d1);
+        CHECK(d2);
+        CHECK(d3);
     }
 
     template <typename T>
     void test_range_lexicographical_compare(const std::vector<T>& lhs, const std::vector<T>& rhs, const int exp) {
-        ASSERT_EQ(exp, col_lexicographical_compare(lhs, rhs));
+        CHECK(col_lexicographical_compare(lhs, rhs) == exp);
     }
 
     TEST_CASE("collection_utils_test.range_lexicographical_compare", "[collection_utils_test]") {
@@ -79,8 +77,8 @@ namespace kdl {
     }
 
     TEST_CASE("collection_utils_test.col_size", "[collection_utils_test]") {
-        ASSERT_EQ(2, col_size<int>(std::vector<int>({ 1, 2 })));
-        ASSERT_EQ(2u, col_size<unsigned>(std::vector<int>({ 1, 2 })));
+        CHECK(col_size<int>(std::vector<int>({ 1, 2 })) == 2);
+        CHECK(col_size<unsigned>(std::vector<int>({ 1, 2 })) == 2u);
     }
 
     TEST_CASE("collection_utils_test.col_delete_all", "[collection_utils_test]") {
@@ -90,14 +88,14 @@ namespace kdl {
         auto d = std::vector<deletable*>({ new deletable(d1), new deletable(d2), new deletable(d3) });
         col_delete_all(d);
 
-        ASSERT_TRUE(d1);
-        ASSERT_TRUE(d2);
-        ASSERT_TRUE(d3);
+        CHECK(d1);
+        CHECK(d2);
+        CHECK(d3);
     }
 
     template <typename T>
     void test_col_is_equivalent(const std::vector<T>& lhs, const std::vector<T>& rhs, const bool exp) {
-        ASSERT_EQ(exp, col_is_equivalent(lhs, rhs));
+        CHECK(col_is_equivalent(lhs, rhs) == exp);
     }
 
     TEST_CASE("collection_utils_test.col_is_equivalent", "[collection_utils_test]") {
