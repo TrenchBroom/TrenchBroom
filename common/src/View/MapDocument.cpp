TrenchBroom/TrenchBroom

commit 6f14f077b343bb8c395910d73938792d95e5e56e

3485: Implement MapDocument::updateSpawnflag in terms of applyAndSwap

common/src/View/MapDocument.cpp
@@ -83,7 +83,6 @@
 #include "View/AddBrushVerticesCommand.h"
 #include "View/AddRemoveNodesCommand.h"
 #include "View/Actions.h"
-#include "View/UpdateEntitySpawnflagCommand.h"
 #include "View/ConvertEntityColorCommand.h"
 #include "View/CurrentGroupCommand.h"
 #include "View/DuplicateNodesCommand.h"
@@ -1968,8 +1967,19 @@ namespace TrenchBroom {
         }
 
         bool MapDocument::updateSpawnflag(const std::string& name, const size_t flagIndex, const bool setFlag) {
-            const auto result = executeAndStore(UpdateEntitySpawnflagCommand::update(name, flagIndex, setFlag));
-            return result->success();
+            return applyAndSwap(*this, setFlag ? "Set Spawnflag" : "Unset Spawnflag", m_selectedNodes.nodes(), kdl::overload(
+                [&](Model::Entity& entity) {
+                    const auto* strValue = entity.attribute(name);
+                    int intValue = strValue ? kdl::str_to_int(*strValue).value_or(0) : 0;
+                    const int flagValue = (1 << flagIndex);
+
+                    intValue = setFlag ? intValue | flagValue : intValue & ~flagValue;
+                    entity.addOrUpdateAttribute(name, kdl::str_to_string(intValue));
+                    
+                    return true;
+                },
+                [] (Model::Brush&) { return true; }
+            ));
         }
 
         bool MapDocument::resizeBrushes(const std::vector<vm::polygon3>& faces, const vm::vec3& delta) {

common/CMakeLists.txt
@@ -441,7 +441,6 @@ set(COMMON_SOURCE
         ${COMMON_SOURCE_DIR}/View/ToolController.cpp
         ${COMMON_SOURCE_DIR}/View/TwoPaneMapView.cpp
         ${COMMON_SOURCE_DIR}/View/UndoableCommand.cpp
-        ${COMMON_SOURCE_DIR}/View/UpdateEntitySpawnflagCommand.cpp
         ${COMMON_SOURCE_DIR}/View/UVCameraTool.cpp
         ${COMMON_SOURCE_DIR}/View/UVEditor.cpp
         ${COMMON_SOURCE_DIR}/View/UVOffsetTool.cpp
@@ -957,7 +956,6 @@ set(COMMON_HEADER
         ${COMMON_SOURCE_DIR}/View/ToolController.h
         ${COMMON_SOURCE_DIR}/View/TwoPaneMapView.h
         ${COMMON_SOURCE_DIR}/View/UndoableCommand.h
-        ${COMMON_SOURCE_DIR}/View/UpdateEntitySpawnflagCommand.h
         ${COMMON_SOURCE_DIR}/View/UVCameraTool.h
         ${COMMON_SOURCE_DIR}/View/UVEditor.h
         ${COMMON_SOURCE_DIR}/View/UVOffsetTool.h

common/src/View/MapDocumentCommandFacade.cpp
@@ -389,43 +389,6 @@ namespace TrenchBroom {
             groupWasClosedNotifier(previousGroup);
         }
 
-        MapDocumentCommandFacade::EntityAttributeSnapshotMap MapDocumentCommandFacade::performUpdateSpawnflag(const std::string& name, const size_t flagIndex, const bool setFlag) {
-            const std::vector<Model::AttributableNode*> attributableNodes = allSelectedAttributableNodes();
-            const std::vector<Model::Node*> nodes(attributableNodes.begin(), attributableNodes.end());
-            const std::vector<Model::Node*> parents = collectParents(nodes);
-            const std::vector<Model::Node*> descendants = collectDescendants(nodes);
-
-            Notifier<const std::vector<Model::Node*>&>::NotifyBeforeAndAfter notifyParents(nodesWillChangeNotifier, nodesDidChangeNotifier, parents);
-            Notifier<const std::vector<Model::Node*>&>::NotifyBeforeAndAfter notifyNodes(nodesWillChangeNotifier, nodesDidChangeNotifier, nodes);
-            Notifier<const std::vector<Model::Node*>&>::NotifyBeforeAndAfter notifyDescendants(nodesWillChangeNotifier, nodesDidChangeNotifier, descendants);
-
-            MapDocumentCommandFacade::EntityAttributeSnapshotMap snapshot;
-
-            std::vector<Model::AttributableNode*>::const_iterator it, end;
-            for (it = attributableNodes.begin(), end = attributableNodes.end(); it != end; ++it) {
-                Model::AttributableNode* node = *it;
-                snapshot[node].push_back(node->attributeSnapshot(name));
-
-                const auto* strValue = node->entity().attribute(name);
-                int intValue = strValue ? kdl::str_to_int(*strValue).value_or(0) : 0;
-                const int flagValue = (1 << flagIndex);
-
-                if (setFlag)
-                    intValue |= flagValue;
-                else
-                    intValue &= ~flagValue;
-
-                auto entity = node->entity();
-                entity.addOrUpdateAttribute(name, kdl::str_to_string(intValue));
-                node->setEntity(std::move(entity));
-            }
-
-            setEntityDefinitions(nodes);
-            setEntityModels(nodes);
-
-            return snapshot;
-        }
-
         MapDocumentCommandFacade::EntityAttributeSnapshotMap MapDocumentCommandFacade::performConvertColorRange(const std::string& name, Assets::ColorRange::Type colorRange) {
             const std::vector<Model::AttributableNode*> attributableNodes = allSelectedAttributableNodes();
             const std::vector<Model::Node*> nodes(std::begin(attributableNodes), std::end(attributableNodes));

common/src/View/MapDocumentCommandFacade.h
@@ -88,7 +88,6 @@ namespace TrenchBroom {
             void performPopGroup();
         public: // entity attributes
             using EntityAttributeSnapshotMap = std::map<Model::AttributableNode*, std::vector<Model::EntityAttributeSnapshot>>;
-            EntityAttributeSnapshotMap performUpdateSpawnflag(const std::string& name, const size_t flagIndex, const bool setFlag);
             EntityAttributeSnapshotMap performConvertColorRange(const std::string& name, Assets::ColorRange::Type colorRange);
             void restoreAttributes(const EntityAttributeSnapshotMap& attributes);
         public: // brush face attributes

common/src/View/UpdateEntitySpawnflagCommand.cpp
@@ -1,59 +1,0 @@
-/*
- Copyright (C) 2010-2017 Kristian Duske
-
- This file is part of TrenchBroom.
-
- TrenchBroom is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- TrenchBroom is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with TrenchBroom. If not, see <http://www.gnu.org/licenses/>.
- */
-
-#include "UpdateEntitySpawnflagCommand.h"
-
-#include "Model/EntityAttributeSnapshot.h"
-#include "View/MapDocumentCommandFacade.h"
-
-#include <string>
-
-namespace TrenchBroom {
-    namespace View {
-        const Command::CommandType UpdateEntitySpawnflagCommand::Type = Command::freeType();
-
-        std::unique_ptr<UpdateEntitySpawnflagCommand> UpdateEntitySpawnflagCommand::update(const std::string& attributeName, const size_t flagIndex, const bool setFlag) {
-            return std::make_unique<UpdateEntitySpawnflagCommand>(attributeName, flagIndex, setFlag);
-        }
-
-        UpdateEntitySpawnflagCommand::UpdateEntitySpawnflagCommand(const std::string& attributeName, const size_t flagIndex, const bool setFlag) :
-        DocumentCommand(Type, makeName(setFlag)),
-        m_setFlag(setFlag),
-        m_attributeName(attributeName),
-        m_flagIndex(flagIndex) {}
-
-        std::string UpdateEntitySpawnflagCommand::makeName(const bool setFlag) {
-            return setFlag ? "Set Spawnflag" : "Unset Spawnflag";
-        }
-
-        std::unique_ptr<CommandResult> UpdateEntitySpawnflagCommand::doPerformDo(MapDocumentCommandFacade* document) {
-            document->performUpdateSpawnflag(m_attributeName, m_flagIndex, m_setFlag);
-            return std::make_unique<CommandResult>(true);
-        }
-
-        std::unique_ptr<CommandResult> UpdateEntitySpawnflagCommand::doPerformUndo(MapDocumentCommandFacade* document) {
-            document->performUpdateSpawnflag(m_attributeName, m_flagIndex, !m_setFlag);
-            return std::make_unique<CommandResult>(true);
-        }
-
-        bool UpdateEntitySpawnflagCommand::doCollateWith(UndoableCommand*) {
-            return false;
-        }
-    }
-}

common/src/View/UpdateEntitySpawnflagCommand.h
@@ -1,53 +1,0 @@
-/*
- Copyright (C) 2010-2017 Kristian Duske
-
- This file is part of TrenchBroom.
-
- TrenchBroom is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- TrenchBroom is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with TrenchBroom. If not, see <http://www.gnu.org/licenses/>.
- */
-
-#pragma once
-
-#include "Macros.h"
-#include "View/DocumentCommand.h"
-
-#include <memory>
-#include <string>
-
-namespace TrenchBroom {
-    namespace View {
-        class UpdateEntitySpawnflagCommand : public DocumentCommand {
-        public:
-            static const CommandType Type;
-        private:
-            bool m_setFlag;
-            std::string m_attributeName;
-            size_t m_flagIndex;
-        public:
-            static std::unique_ptr<UpdateEntitySpawnflagCommand> update(const std::string& attributeName, const size_t flagIndex, const bool setFlag);
-
-            UpdateEntitySpawnflagCommand(const std::string& attributeName, const size_t flagIndex, const bool setFlag);
-        private:
-            static std::string makeName(const bool setFlag);
-
-            std::unique_ptr<CommandResult> doPerformDo(MapDocumentCommandFacade* document) override;
-            std::unique_ptr<CommandResult> doPerformUndo(MapDocumentCommandFacade* document) override;
-
-            bool doCollateWith(UndoableCommand* command) override;
-
-            deleteCopyAndMove(UpdateEntitySpawnflagCommand)
-        };
-    }
-}
-

commit 7d9797fa3a1f7dbdaa8b98bd6ac1d126b5583274

3691: fix setting spawnflags on brush entities
Fixes #3691

Bug was introduced in 6f14f077b343bb8c395910d73938792d95e5e56e

The previous code in MapDocumentCommandFacade::performUpdateSpawnflag
was using allSelectedAttributableNodes() to get this list of entities
to update, which got lost in the refactor

common/src/View/MapDocument.cpp
@@ -1963,7 +1963,7 @@ namespace TrenchBroom {
         }
 
         bool MapDocument::updateSpawnflag(const std::string& key, const size_t flagIndex, const bool setFlag) {
-            return applyAndSwap(*this, setFlag ? "Set Spawnflag" : "Unset Spawnflag", m_selectedNodes.nodes(), kdl::overload(
+            return applyAndSwap(*this, setFlag ? "Set Spawnflag" : "Unset Spawnflag", allSelectedEntityNodes(), kdl::overload(
                 [] (Model::Layer&) { return true; },
                 [] (Model::Group&) { return true; },
                 [&](Model::Entity& entity) {