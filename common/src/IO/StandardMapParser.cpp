a764ff03370b30824cb4268a56e8fd8914a76a10
Robert Beckebans <robert.beckebans@gmail.com>
2021-02-06 18:32:35 +0100

    Call onBeginBrush() for Doom 3 primitives

--- common/src/IO/StandardMapParser.cpp
+++ common/src/IO/StandardMapParser.cpp
@@ -326,7 +326,7 @@ namespace TrenchBroom {
                         break;
                     case QuakeMapToken::OParenthesis:
                         // TODO 2427: handle brush primitives
-                        if (!beginBrushCalled && !primitive) {
+                        if (!beginBrushCalled /*&& !primitive*/) {
                             onBeginBrush(startLine, status);
                             beginBrushCalled = true;
                         }
