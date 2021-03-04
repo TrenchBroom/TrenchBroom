50579e9dabeb45d140d0b5c8127f37c1908c57d3
Eric Wasylishen <ewasylishen@gmail.com>
2021-03-03 23:45:30 -0700

    3023: adjust GET_GIT_DESCRIBE macro behaviour:
    - If "git describe" fails (e.g. shallow clone), try to use
      GITHUB_SHA and GITHUB_REF.
    
    - If those are not set, set the GIT_DESCRIBE variable to "unknown"
    
    For GET_APP_VERSION:
    
    - Whichever of the above methods was used, make it not a fatal error
      if the version description isn't in the v0000.0 format
    
    Fixes #3023

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -159,16 +159,15 @@ macro(ADD_TARGET_PROPERTY TARGET PROPERTY VALUE)
 endmacro(ADD_TARGET_PROPERTY)
 
 macro(GET_APP_VERSION GIT_DESCRIBE VERSION_YEAR VERSION_NUMBER)
-    if(NOT ${GIT_DESCRIBE})
-        set(${GIT_DESCRIBE} "v0000.0")
-    endif()
     STRING(REGEX MATCH "v([0-9][0-9][0-9][0-9])[.]([0-9]+)" GIT_DESCRIBE_MATCH "${${GIT_DESCRIBE}}")
 
     if(GIT_DESCRIBE_MATCH)
         set(${VERSION_YEAR} ${CMAKE_MATCH_1})
         set(${VERSION_NUMBER} ${CMAKE_MATCH_2})
     else()
-        message(FATAL_ERROR "Couldn't parse version from git describe output '${${GIT_DESCRIBE}}'")
+        # GIT_DESCRIBE is not a release tag following the "v0000.0" format.
+        set(${VERSION_YEAR} "0")
+        set(${VERSION_NUMBER} "0")
     endif()
 endmacro(GET_APP_VERSION)
 
@@ -176,6 +175,26 @@ macro(GET_GIT_DESCRIBE GIT SOURCE_DIR GIT_DESCRIBE)
     # When building tags, GitHub Actions checks them out as lightweight tags even if the original tag was annotated.
     # Pass --tags to enable git describe to match non-annotated tags.
     execute_process(COMMAND ${GIT} describe --dirty --tags WORKING_DIRECTORY ${SOURCE_DIR} OUTPUT_VARIABLE ${GIT_DESCRIBE} OUTPUT_STRIP_TRAILING_WHITESPACE)
+    if(${GIT_DESCRIBE})
+        message(STATUS "Using version description \"${${GIT_DESCRIBE}}\" from git describe")
+    endif()
+
+    if(NOT ${GIT_DESCRIBE})
+        # On GitHub Actions, "git describe" will fail due to it being a shallow clone (for PR and branch builds).
+        # In this case, use GITHUB_SHA and GITHUB_REF to construct a descriptive version string.
+        # See: https://docs.github.com/en/actions/reference/environment-variables#default-environment-variables
+        if((DEFINED ENV{GITHUB_SHA}) AND (DEFINED ENV{GITHUB_REF}))
+            set(${GIT_DESCRIBE} "$ENV{GITHUB_REF}@$ENV{GITHUB_SHA}")
+            # Replace / with _ because we need GIT_DESCRIBE to be valid to put in a filename for the final package
+            string(REPLACE "/" "_" ${GIT_DESCRIBE} ${${GIT_DESCRIBE}})
+            message(STATUS "Using version description \"${${GIT_DESCRIBE}}\" from environment variables GITHUB_SHA and GITHUB_REF")
+        endif()
+    endif()
+
+    if(NOT ${GIT_DESCRIBE})
+        set(${GIT_DESCRIBE} "unknown")
+        message(WARNING "Unable to determine version description; using \"${${GIT_DESCRIBE}}\"")
+    endif()
 endmacro(GET_GIT_DESCRIBE)
 
 macro(GET_BUILD_PLATFORM PLATFORM_NAME)
