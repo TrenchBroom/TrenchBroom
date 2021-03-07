ff18a79b35474143f878973a0c0d1168a6809193
Eric Wasylishen <ewasylishen@gmail.com>
2021-03-07 15:01:14 -0700

    3023: give Github Actions PR and master branch builds descriptive version names (#3771)
    * 3023: adjust GET_GIT_DESCRIBE macro behaviour:
    
    - If "git describe" fails (e.g. shallow clone), try to use
      GITHUB_SHA and GITHUB_REF.
    
    - If those are not set, set the GIT_DESCRIBE variable to "unknown"
    
    For GET_APP_VERSION:
    
    - Whichever of the above methods was used, make it not a fatal error
      if the version description isn't in the v0000.0 format
    
    Fixes #3023
    
    * 3023: change separator from @ to - because it causes Linux cpack to fail
    
    * 3023: remove debug hack
    
    * 3023: add "unstable-" prefix to version name
    
    * 3023: record PR head commit SHA
    
    * 3023: try different way of saving PR head sha
    
    * 3023: handle TB_PULL_REQUEST_HEAD_SHA being set to ""
    
    * 3023: add word wrap to handle too-long label
    
    * 3023: remove test of TB_PULL_REQUEST_HEAD_SHA (didn't work anyway)
    
    * 3023: test case when TB_PULL_REQUEST_HEAD_SHA is an invalid value
    
    * Revert "3023: test case when TB_PULL_REQUEST_HEAD_SHA is an invalid value"
    
    This reverts commit 022b2c2ec0f9007b320cd5a26789be7d9343bad1.

--- common/src/View/AppInfoPanel.cpp
+++ common/src/View/AppInfoPanel.cpp
@@ -57,6 +57,8 @@ namespace TrenchBroom {
             makeInfo(version);
             makeInfo(build);
             makeInfo(qtVersion);
+            build->setWordWrap(true);
+            build->setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
 
             const auto tooltip = tr("Click to copy to clipboard");
             version->setToolTip(tooltip);
