f473e28b889bcb90c0843e7fcc9f2f8601171e75
Kristian Duske <kristian.duske@gmail.com>
2021-05-02 21:34:05 +0200

    3799: Build all in measure_buildtime.sh

--- measure_buildtime.sh
+++ measure_buildtime.sh
@@ -9,6 +9,6 @@ fi
 mkdir "$BUILD_DIR"
 cd "$BUILD_DIR"
 
-cmake -GNinja .. -DTB_SUPPRESS_PCH=1 -DTB_ENABLE_CCACHE=0
+cmake -GNinja .. -DTB_SUPPRESS_PCH=1 -DTB_ENABLE_CCACHE=0 -DCMAKE_PREFIX_PATH=/Users/kristian/Qt/5.15.2/clang_64/lib/cmake/Qt5
 
-time cmake --build . --target TrenchBroom-nomanual --config Release
+time cmake --build . --config Release
