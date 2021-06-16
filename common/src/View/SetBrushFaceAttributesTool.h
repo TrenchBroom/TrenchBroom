956f36780104d6d7ad7de6a55acc0c57c6dbfc4b
Kristian Duske <kristian.duske@gmail.com>
2021-06-16 22:35:17 +0200

    3781: Implement SetBrushFaceAttributesTool in terms of ToolController

--- common/src/View/SetBrushFaceAttributesTool.h
+++ common/src/View/SetBrushFaceAttributesTool.h
@@ -44,7 +44,7 @@ namespace TrenchBroom {
          * - LMB Drag: applies to all faces dragged over
          * - LMB Double click: applies to all faces of target brush
          */
-        class SetBrushFaceAttributesTool : public ToolControllerBase<NoPickingPolicy, NoKeyPolicy, MousePolicy, NoRenderPolicy>, public Tool {
+        class SetBrushFaceAttributesTool : public ToolController, public Tool {
         private:
             std::weak_ptr<MapDocument> m_document;
         public:
@@ -53,10 +53,10 @@ namespace TrenchBroom {
             Tool* doGetTool() override;
             const Tool* doGetTool() const override;
 
-            bool doMouseClick(const InputState& inputState) override;
-            bool doMouseDoubleClick(const InputState& inputState) override;
+            bool mouseClick(const InputState& inputState) override;
+            bool mouseDoubleClick(const InputState& inputState) override;
 
-            bool doCancel() override;
+            bool cancel() override;
             
             std::unique_ptr<DragTracker> acceptMouseDrag(const InputState& inputState) override;
 
