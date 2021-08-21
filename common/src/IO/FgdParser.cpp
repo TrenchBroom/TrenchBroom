4f61849fbba4988430d372f5f8ee0c255b42a48a
Eric Wasylishen <ewasylishen@gmail.com>
2021-08-20 22:23:58 -0600

    3859: initialize sub-parser with correct line/column
    Fixes #3859

--- common/src/IO/FgdParser.cpp
+++ common/src/IO/FgdParser.cpp
@@ -357,7 +357,7 @@ namespace TrenchBroom {
             const auto column = m_tokenizer.column();
 
             try {
-                ELParser parser(ELParser::Mode::Lenient, m_tokenizer.remainder());
+                ELParser parser(ELParser::Mode::Lenient, m_tokenizer.remainder(), line, column);
                 auto expression = parser.parse();
 
                 // advance our tokenizer by the amount that `parser` parsed
@@ -370,7 +370,7 @@ namespace TrenchBroom {
                 try {
                     m_tokenizer.restore(snapshot);
 
-                    LegacyModelDefinitionParser parser(m_tokenizer.remainder());
+                    LegacyModelDefinitionParser parser(m_tokenizer.remainder(), line, column);
                     auto expression = parser.parse(status);
 
                     // advance our tokenizer by the amount that `parser` parsed
