ef414efc27561744dcdc413222d70040567135f6
Kristian Duske <kristian.duske@gmail.com>
2021-11-14 17:36:05 +0100

    3902: Add PointTrace class
    Together with the free function loadPointFile, PointTrace implements the
    requirements for PointFile, but with better separation of concerns.

---
+++ common/src/Model/PointTrace.h
@@ -0,0 +1,55 @@
+/*
+ Copyright (C) 2021 Kristian Duske
+
+ This file is part of TrenchBroom.
+
+ TrenchBroom is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ TrenchBroom is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with TrenchBroom. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <vecmath/forward.h>
+#include <vecmath/vec.h>
+
+#include <iosfwd>
+#include <optional>
+#include <vector>
+
+namespace TrenchBroom::Model {
+class PointTrace {
+private:
+  std::vector<vm::vec3f> m_points;
+  size_t m_current;
+
+public:
+  explicit PointTrace(std::vector<vm::vec3f> points);
+
+  bool hasNextPoint() const;
+  bool hasPreviousPoint() const;
+
+  const std::vector<vm::vec3f>& points() const;
+  const vm::vec3f& currentPoint() const;
+  const vm::vec3f currentDirection() const;
+
+  void advance();
+  void retreat();
+
+  friend bool operator==(const PointTrace& lhs, const PointTrace& rhs);
+  friend bool operator!=(const PointTrace& lhs, const PointTrace& rhs);
+
+  friend std::ostream& operator<<(std::ostream& lhs, const PointTrace& rhs);
+};
+
+std::optional<PointTrace> loadPointFile(std::istream& stream);
+} // namespace TrenchBroom::Model
---
+++ common/test/src/Model/PointTraceTest.cpp
@@ -0,0 +1,117 @@
+/*
+ Copyright (C) 2021 Kristian Duske
+
+ This file is part of TrenchBroom.
+
+ TrenchBroom is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ TrenchBroom is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with TrenchBroom. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "Model/PointTrace.h"
+
+#include <vecmath/vec.h>
+#include <vecmath/vec_io.h>
+
+#include <sstream>
+
+#include "Catch2.h"
+
+namespace TrenchBroom::Model {
+TEST_CASE("PointTrace") {
+  const auto points = std::vector<vm::vec3f>{
+    {1, 1, 1},
+    {1, 1, 2},
+    {1, 2, 2},
+  };
+
+  auto trace = PointTrace{points};
+  CHECK(trace.points() == points);
+
+  CHECK(trace.hasNextPoint());
+  CHECK_FALSE(trace.hasPreviousPoint());
+  CHECK(trace.currentPoint() == vm::vec3f{1, 1, 1});
+  CHECK(trace.currentDirection() == vm::vec3f{0, 0, 1});
+
+  trace.advance();
+
+  CHECK(trace.hasNextPoint());
+  CHECK(trace.hasPreviousPoint());
+  CHECK(trace.currentPoint() == vm::vec3f{1, 1, 2});
+  CHECK(trace.currentDirection() == vm::vec3f{0, 1, 0});
+
+  trace.advance();
+
+  CHECK_FALSE(trace.hasNextPoint());
+  CHECK(trace.hasPreviousPoint());
+  CHECK(trace.currentPoint() == vm::vec3f{1, 2, 2});
+  CHECK(trace.currentDirection() == vm::vec3f{0, 1, 0});
+
+  trace.advance();
+  CHECK(trace.currentPoint() == vm::vec3f{1, 2, 2});
+
+  trace.retreat();
+  CHECK(trace.currentPoint() == vm::vec3f{1, 1, 2});
+
+  trace.retreat();
+  CHECK(trace.currentPoint() == vm::vec3f{1, 1, 1});
+
+  trace.retreat();
+  CHECK(trace.currentPoint() == vm::vec3f{1, 1, 1});
+}
+
+TEST_CASE("loadPointFile") {
+  using T = std::tuple<std::string, std::optional<PointTrace>>;
+  // clang-format off
+  const auto
+  [file,       expectedTrace] = GENERATE(values<T>({
+  {R"()",      std::nullopt},
+  {R"(asdf)",  std::nullopt},
+  {R"(1)",     std::nullopt},
+  {R"(1 2)",   std::nullopt},
+  {R"(1 2 3)", std::nullopt},
+  {R"(
+    1 2 3
+    4 5 6
+  )",          
+               PointTrace{{
+                 {1, 2, 3},
+                 {4, 5, 6},
+               }}},
+  {R"(
+    0 0 1
+    0 0 1
+    4 5 6
+    4 5 6
+  )",          
+               PointTrace{{
+                 {0, 0, 1},
+                 {4, 5, 6},
+               }}},
+  {R"(
+    0 0 1
+    0 0 2
+    0 0 3
+    4 5 6
+  )",          
+               PointTrace{{
+                 {0, 0, 1},
+                 {0, 0, 3},
+                 {4, 5, 6},
+               }}},
+  }));
+  // clang-format on
+
+  auto stream = std::istringstream{file};
+  CHECK(loadPointFile(stream) == expectedTrace);
+}
+} // namespace TrenchBroom::Model
