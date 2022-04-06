a7ba41005aab242e5e470239af75efa43eed72e2
Kristian Duske <kristian.duske@gmail.com>
2022-04-06 15:02:21 +0200

    Add tests for entityRotationInfo function

--- common/test/src/Model/EntityRotationPolicyTest.cpp
+++ common/test/src/Model/EntityRotationPolicyTest.cpp
@@ -17,8 +17,15 @@
  along with TrenchBroom. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "Model/EntityRotationPolicy.h"
 #include "FloatType.h"
+#include "Macros.h"
+
+#include "Assets/EntityDefinition.h"
+#include "Assets/EntityModel.h"
+#include "Assets/PropertyDefinition.h"
+#include "Model/Entity.h"
+#include "Model/EntityProperties.h"
+#include "Model/EntityRotationPolicy.h"
 
 #include <vecmath/approx.h>
 #include <vecmath/mat.h>
@@ -80,6 +87,120 @@ TEST_CASE("EntityRotationPolicy.getYawPitchRoll_flip") {
   const auto yawPitchRoll = EntityRotationPolicy::getYawPitchRoll(scaleMat, rotMat);
 
   CHECK(yawPitchRoll == vm::approx(vm::vec3(180, 45, -10)));
+}
+
+namespace {
+struct EntityDefinitionInfo {
+  Assets::EntityDefinitionType type;
+  vm::bbox3 bounds = vm::bbox3{16.0};
+};
+
+std::unique_ptr<Assets::EntityDefinition> createEntityDefinition(
+  const std::optional<EntityDefinitionInfo>& info) {
+  if (!info.has_value()) {
+    return nullptr;
+  }
+
+  switch (info->type) {
+    case Assets::EntityDefinitionType::PointEntity:
+      return std::make_unique<Assets::PointEntityDefinition>(
+        "", Color{}, info->bounds, "", std::vector<std::shared_ptr<Assets::PropertyDefinition>>{},
+        Assets::ModelDefinition{});
+    case Assets::EntityDefinitionType::BrushEntity:
+      return std::make_unique<Assets::BrushEntityDefinition>(
+        "", Color{}, "", std::vector<std::shared_ptr<Assets::PropertyDefinition>>{});
+      switchDefault();
+  }
+}
+} // namespace
+
+TEST_CASE("EntityRotationPolicy.entityRotationInfo") {
+  using namespace Assets;
+
+  auto normalPitch = EntityModelLoadedFrame{0, "", {}, PitchType::Normal, Orientation::Oriented};
+  auto invertedPitch =
+    EntityModelLoadedFrame{0, "", {}, PitchType::MdlInverted, Orientation::Oriented};
+
+  using T = std::tuple<
+    std::vector<EntityProperty>, bool, std::optional<EntityDefinitionInfo>, EntityModelFrame*,
+    EntityRotationInfo>;
+
+  // clang-format off
+  const auto
+  [entityProperties,              point, entityDefinitionInfo,            entityModel,    expectedRotationInfo] = GENERATE_REF(values<T>({
+  {{},                            false, std::nullopt,                    nullptr,        {EntityRotationType::None, "", EntityRotationUsage::Allowed}},
+  // a light with a mangle key
+  {{{"classname", "light"},
+    {"mangle",    "0 0 0"}},      true,  std::nullopt,                    nullptr,        {EntityRotationType::Mangle, "mangle", EntityRotationUsage::Allowed}},
+
+  // a light without a target key and with an angles key, type is controlled by the model's pitch type (default is normal)
+  {{{"classname", "light"},
+    {"angles",    "0 0 0"}},      true,  std::nullopt,                    nullptr,        {EntityRotationType::Euler_PositivePitchDown, "angles", EntityRotationUsage::Allowed}},
+
+  // a light without a target key and with an angle key
+  {{{"classname", "light"},
+    {"angle",    "0"}},           true,  std::nullopt,                    nullptr,        {EntityRotationType::Angle, "angle", EntityRotationUsage::Allowed}},
+
+  // a light without a target key and with an angles key, type is controlled by the model's pitch type (normal)
+  {{{"classname", "light"},
+    {"angles",    "0 0 0"}},      true,  std::nullopt,                    &normalPitch,   {EntityRotationType::Euler_PositivePitchDown, "angles", EntityRotationUsage::Allowed}},
+
+  // a light without a target key and with an angles key, type is controlled by the model's pitch type (inverted)
+  {{{"classname", "light"},
+    {"angles",    "0 0 0"}},      true,  std::nullopt,                    &invertedPitch, {EntityRotationType::Euler, "angles", EntityRotationUsage::Allowed}},
+
+  // a light without a target key and without an angles key
+  {{{"classname", "light"}},      true,  std::nullopt,                    nullptr,        {EntityRotationType::Angle, "angle", EntityRotationUsage::Allowed}},
+
+  // a light with a target key
+  {{{"classname", "light"},
+    {"target", "xyz"}},           true,  std::nullopt,                    nullptr,        {EntityRotationType::None, "", EntityRotationUsage::Allowed}},
+
+  // non-light brush entity without additional keys
+  {{{"classname", "other"}},      false, std::nullopt,                    nullptr,        {EntityRotationType::None, "", EntityRotationUsage::Allowed}},
+
+  // non-light brush entity with angles key
+  {{{"classname", "other"},
+    {"angles", "0 0 0"}},         false, std::nullopt,                    nullptr,        {EntityRotationType::Euler_PositivePitchDown, "angles", EntityRotationUsage::Allowed}},
+
+  // non-light brush entity with mangle key
+  {{{"classname", "other"},
+    {"mangle", "0 0 0"}},         false, std::nullopt,                    &invertedPitch, {EntityRotationType::Euler, "mangle", EntityRotationUsage::Allowed}},
+
+  // non-light brush entity with mangle key (model controls the euler type)
+  {{{"classname", "other"},
+    {"mangle", "0 0 0"}},         false, std::nullopt,                    nullptr,        {EntityRotationType::Euler_PositivePitchDown, "mangle", EntityRotationUsage::Allowed}},
+
+  // non-light brush entity with angle key
+  {{{"classname", "other"},
+    {"angle", "0"}},              false, std::nullopt,                    nullptr,        {EntityRotationType::AngleUpDown, "angle", EntityRotationUsage::Allowed}},
+
+  // non-light point entity without additional keys
+  {{{"classname", "other"}},      true,  std::nullopt,                    nullptr,        {EntityRotationType::AngleUpDown, "angle", EntityRotationUsage::Allowed}},
+
+  // non-light point entity with angles key
+  {{{"classname", "other"},
+    {"angles", "0 0 0"}},         true,  std::nullopt,                    nullptr,        {EntityRotationType::Euler_PositivePitchDown, "angles", EntityRotationUsage::Allowed}},
+
+  // non-light point entity with angles key (model controls the euler type)
+  {{{"classname", "other"},
+    {"angles", "0 0 0"}},         true,  std::nullopt,                    &invertedPitch, {EntityRotationType::Euler, "angles", EntityRotationUsage::Allowed}},
+
+  // non-light point entity with mangle key
+  {{{"classname", "other"},
+    {"mangle", "0 0 0"}},         true,  std::nullopt,                    nullptr,        {EntityRotationType::Euler_PositivePitchDown, "mangle", EntityRotationUsage::Allowed}},
+  }));
+  // clang-format on
+
+  CAPTURE(entityProperties, point, entityDefinitionInfo, entityModel);
+
+  auto entityDefinition = createEntityDefinition(entityDefinitionInfo);
+  auto entity = Entity{{}, entityProperties};
+  entity.setDefinition({}, entityDefinition.get());
+  entity.setModel({}, entityModel);
+  entity.setPointEntity({}, point);
+
+  CHECK(entityRotationInfo(entity) == expectedRotationInfo);
 }
 } // namespace Model
 } // namespace TrenchBroom
