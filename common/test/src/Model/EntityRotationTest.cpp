820a33fc59e4dcd504b91c5fc2dddb553f497f72
Kristian Duske <kristian.duske@gmail.com>
2022-05-04 22:43:48 +0200

    Rename tests

--- common/test/src/Model/EntityRotationTest.cpp
+++ common/test/src/Model/EntityRotationTest.cpp
@@ -39,7 +39,7 @@
 
 namespace TrenchBroom {
 namespace Model {
-TEST_CASE("EntityRotation.entityYawPitchRoll") {
+TEST_CASE("entityYawPitchRoll") {
   const auto roll = vm::to_radians(12.0);
   const auto pitch = vm::to_radians(13.0);
   const auto yaw = vm::to_radians(14.0);
@@ -50,7 +50,7 @@ TEST_CASE("EntityRotation.entityYawPitchRoll") {
   CHECK(yawPitchRoll == vm::approx(vm::vec3(14, 13, 12)));
 }
 
-TEST_CASE("EntityRotation.entityYawPitchRoll_uniformScale") {
+TEST_CASE("entityYawPitchRoll_uniformScale") {
   const auto roll = vm::to_radians(12.0);
   const auto pitch = vm::to_radians(13.0);
   const auto yaw = vm::to_radians(14.0);
@@ -62,7 +62,7 @@ TEST_CASE("EntityRotation.entityYawPitchRoll_uniformScale") {
   CHECK(yawPitchRoll == vm::approx(vm::vec3(14, 13, 12)));
 }
 
-TEST_CASE("EntityRotation.entityYawPitchRoll_nonUniformScale") {
+TEST_CASE("entityYawPitchRoll_nonUniformScale") {
   const auto roll = vm::to_radians(0.0);
   const auto pitch = vm::to_radians(45.0);
   const auto yaw = vm::to_radians(0.0);
@@ -76,7 +76,7 @@ TEST_CASE("EntityRotation.entityYawPitchRoll_nonUniformScale") {
   CHECK(yawPitchRoll == vm::approx(vm::vec3(0.0, expectedPitch, 0.0)));
 }
 
-TEST_CASE("EntityRotation.entityYawPitchRoll_flip") {
+TEST_CASE("entityYawPitchRoll_flip") {
   const auto roll = vm::to_radians(10.0);
   const auto pitch = vm::to_radians(45.0);
   const auto yaw = vm::to_radians(0.0);
@@ -114,7 +114,7 @@ std::unique_ptr<Assets::EntityDefinition> createEntityDefinition(
 }
 } // namespace
 
-TEST_CASE("EntityRotation.entityRotationInfo") {
+TEST_CASE("entityRotationInfo") {
   using namespace Assets;
 
   auto manglePropertyDef = std::make_shared<StringPropertyDefinition>("mangle", "", "", false);
