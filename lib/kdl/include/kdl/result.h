f06aceb7aa5ad8fee34afb14147071fc5b39507a
Kristian Duske <kristian.duske@gmail.com>
2023-02-27 22:19:13 +0100

    Add result::release()

--- lib/kdl/include/kdl/result.h
+++ lib/kdl/include/kdl/result.h
@@ -529,31 +529,10 @@ public:
       [](const auto&) -> value_type { throw bad_result_access{}; }));
   }
 
-  auto value_or(const Value& x) const&
+  auto value_or(Value x) const&
   {
     return visit(kdl::overload(
       [](const value_type& v) -> value_type { return v; },
-      [&](const auto&) -> value_type { return x; }));
-  }
-
-  auto value_or(Value&& x) const&
-  {
-    return visit(kdl::overload(
-      [](const value_type& v) -> value_type { return v; },
-      [&](const auto&) -> value_type { return std::move(x); }));
-  }
-
-  auto value_or(const Value& x) &&
-  {
-    return std::move(*this).visit(kdl::overload(
-      [](value_type&& v) -> value_type { return std::move(v); },
-      [&](const auto&) -> value_type { return x; }));
-  }
-
-  auto value_or(Value&& x) &&
-  {
-    return std::move(*this).visit(kdl::overload(
-      [](value_type&& v) -> value_type { return std::move(v); },
       [&](const auto&) -> value_type { return std::move(x); }));
   }
 
@@ -571,6 +550,25 @@ public:
       [](value_type&& v) -> value_type { return std::move(v); },
       [](const auto&) -> value_type { throw bad_result_access{}; }));
   }
+
+  auto value_or(Value x) &&
+  {
+    return std::move(*this).visit(kdl::overload(
+      [](value_type&& v) -> value_type { return std::move(v); },
+      [&](const auto&) -> value_type { return std::move(x); }));
+  }
+
+  /**
+   * Returns the value contained in this result if it is successful. Otherwise, throws
+   * `bad_result_access`.
+   *
+   * @return the value in this result
+   *
+   * @throw bad_result_access if this result is an error
+   */
+  auto release() { return std::move(*this).value(); }
+
+  auto release_or(Value x) { return std::move(*this).value_or(std::move(x)); }
 
   /**
    * Returns a the error contained in this result if it not successful. Otherwise, throws
