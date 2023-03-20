79b5eb27f0f70fb17bc9b2072ea70607d6f7760f
Kristian Duske <kristian.duske@gmail.com>
2023-03-20 21:48:47 +0100

    Cleanup: code style

--- common/src/View/CompilationVariables.cpp
+++ common/src/View/CompilationVariables.cpp
@@ -48,66 +48,65 @@ const std::string APP_DIR_PATH = "APP_DIR_PATH";
 
 CommonVariables::CommonVariables(std::shared_ptr<MapDocument> document)
 {
-  const IO::Path filename = document->path().lastComponent();
-  const IO::Path gamePath = document->game()->gamePath();
+  const auto filename = document->path().lastComponent();
+  const auto gamePath = document->game()->gamePath();
 
-  std::vector<std::string> mods;
+  auto mods = std::vector<std::string>{};
   mods.push_back(document->defaultMod());
   mods = kdl::vec_concat(std::move(mods), document->mods());
 
   using namespace CompilationVariableNames;
-  declare(MAP_BASE_NAME, EL::Value(filename.deleteExtension().asString()));
-  declare(GAME_DIR_PATH, EL::Value(gamePath.asString()));
+  declare(MAP_BASE_NAME, EL::Value{filename.deleteExtension().asString()});
+  declare(GAME_DIR_PATH, EL::Value{gamePath.asString()});
   declare(
     MODS,
     EL::Value{kdl::vec_transform(mods, [](const auto& mod) { return EL::Value{mod}; })});
 
   const auto& factory = Model::GameFactory::instance();
-  for (const Model::CompilationTool& tool : document->game()->compilationTools())
+  for (const auto& tool : document->game()->compilationTools())
   {
-    const IO::Path toolPath =
+    const auto toolPath =
       factory.compilationToolPath(document->game()->gameName(), tool.name);
     // e.g. variable name might be "qbsp", and the value is the path to the user's local
     // qbsp executable
-    declare(tool.name, EL::Value(toolPath.asString()));
+    declare(tool.name, EL::Value{toolPath.asString()});
   }
 }
 
 CommonCompilationVariables::CommonCompilationVariables(
   std::shared_ptr<MapDocument> document)
-  : CommonVariables(document)
+  : CommonVariables{document}
 {
-  const IO::Path filename = document->path().lastComponent();
-  const IO::Path filePath = document->path().deleteLastComponent();
-  const IO::Path appPath = IO::SystemPaths::appDirectory();
+  const auto filename = document->path().lastComponent();
+  const auto filePath = document->path().deleteLastComponent();
+  const auto appPath = IO::SystemPaths::appDirectory();
 
   using namespace CompilationVariableNames;
-  declare(MAP_FULL_NAME, EL::Value(filename.asString()));
-  declare(MAP_DIR_PATH, EL::Value(filePath.asString()));
-  declare(APP_DIR_PATH, EL::Value(appPath.asString()));
+  declare(MAP_FULL_NAME, EL::Value{filename.asString()});
+  declare(MAP_DIR_PATH, EL::Value{filePath.asString()});
+  declare(APP_DIR_PATH, EL::Value{appPath.asString()});
 }
 
 CompilationWorkDirVariables::CompilationWorkDirVariables(
   std::shared_ptr<MapDocument> document)
-  : CommonCompilationVariables(document)
+  : CommonCompilationVariables{std::move(document)}
 {
 }
 
 CompilationVariables::CompilationVariables(
   std::shared_ptr<MapDocument> document, const std::string& workDir)
-  : CommonCompilationVariables(document)
+  : CommonCompilationVariables{std::move(document)}
 {
-  const auto cpuCount =
-    static_cast<size_t>(std::max(std::thread::hardware_concurrency(), 1u));
+  const auto cpuCount = size_t(std::max(std::thread::hardware_concurrency(), 1u));
 
   using namespace CompilationVariableNames;
-  declare(CPU_COUNT, EL::Value(cpuCount));
-  declare(WORK_DIR_PATH, EL::Value(workDir));
+  declare(CPU_COUNT, EL::Value{cpuCount});
+  declare(WORK_DIR_PATH, EL::Value{workDir});
 }
 
 LaunchGameEngineVariables::LaunchGameEngineVariables(
   std::shared_ptr<MapDocument> document)
-  : CommonVariables(document)
+  : CommonVariables{std::move(document)}
 {
 }
 } // namespace View
