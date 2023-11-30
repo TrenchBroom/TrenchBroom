8b78b762932efd5a60a537ce2e479faae182414b
Kristian Duske <kristian.duske@gmail.com>
2023-11-30 17:35:55 +0100

    Don't run abort test on Windows

--- common/test/src/View/tst_CompilationRunner.cpp
+++ common/test/src/View/tst_CompilationRunner.cpp
@@ -162,9 +162,8 @@ TEST_CASE_METHOD(
   CHECK(exec.ended == !treatNonZeroResultCodeAsError);
 }
 
-#if (defined(_WIN64) && defined(NDEBUG)) || !defined(_WIN32)
-// std::abort pops up a dialog when run in debug mode on Windows
-// the test is unreliable on Windows 32bit
+#if !defined(_WIN32) && !defined(_WIN64)
+// the test is unreliable on Windows
 TEST_CASE_METHOD(MapDocumentTest, "CompilationRunToolTaskRunner.toolAborts")
 {
   auto variables = EL::NullVariableStore{};
