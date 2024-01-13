a5adb4f13dd0457c9af20d15df0a16ee5c3157f0
Kristian Duske <kristian.duske@gmail.com>
2024-01-13 17:27:48 +0100

    Cleanup: code style

--- common/test/src/Model/tst_BrushBuilder.cpp
+++ common/test/src/Model/tst_BrushBuilder.cpp
@@ -30,18 +30,17 @@
 
 #include "Catch2.h"
 
-namespace TrenchBroom
+namespace TrenchBroom::Model
 {
-namespace Model
-{
+
 TEST_CASE("BrushBuilderTest.createCube")
 {
-  const vm::bbox3 worldBounds(8192.0);
+  const auto worldBounds = vm::bbox3{8192.0};
 
-  BrushBuilder builder(MapFormat::Standard, worldBounds);
-  const Brush cube = builder.createCube(128.0, "someName").value();
+  auto builder = BrushBuilder{MapFormat::Standard, worldBounds};
+  const auto cube = builder.createCube(128.0, "someName").value();
   CHECK(cube.fullySpecified());
-  CHECK(cube.bounds() == vm::bbox3d(-64.0, +64.0));
+  CHECK(cube.bounds() == vm::bbox3d{-64.0, +64.0});
 
   const auto faces = cube.faces();
   CHECK(faces.size() == 6u);
@@ -54,21 +53,21 @@ TEST_CASE("BrushBuilderTest.createCube")
 
 TEST_CASE("BrushBuilderTest.createCubeDefaults")
 {
-  const vm::bbox3 worldBounds(8192.0);
+  const auto worldBounds = vm::bbox3{8192.0};
 
-  BrushFaceAttributes defaultAttribs("defaultTexture");
-  defaultAttribs.setOffset(vm::vec2f(0.5f, 0.5f));
-  defaultAttribs.setScale(vm::vec2f(0.5f, 0.5f));
+  auto defaultAttribs = BrushFaceAttributes{"defaultTexture"};
+  defaultAttribs.setOffset({0.5f, 0.5f});
+  defaultAttribs.setScale({0.5f, 0.5f});
   defaultAttribs.setRotation(45.0f);
   defaultAttribs.setSurfaceContents(1);
   defaultAttribs.setSurfaceFlags(2);
   defaultAttribs.setSurfaceValue(0.1f);
-  defaultAttribs.setColor(Color(255, 255, 255, 255));
+  defaultAttribs.setColor(Color{255, 255, 255, 255});
 
-  BrushBuilder builder(MapFormat::Standard, worldBounds, defaultAttribs);
-  const Brush cube = builder.createCube(128.0, "someName").value();
+  auto builder = BrushBuilder{MapFormat::Standard, worldBounds, defaultAttribs};
+  const auto cube = builder.createCube(128.0, "someName").value();
   CHECK(cube.fullySpecified());
-  CHECK(cube.bounds() == vm::bbox3d(-64.0, +64.0));
+  CHECK(cube.bounds() == vm::bbox3d{-64.0, +64.0});
 
   const auto faces = cube.faces();
   CHECK(faces.size() == 6u);
@@ -76,14 +75,14 @@ TEST_CASE("BrushBuilderTest.createCubeDefaults")
   for (size_t i = 0; i < faces.size(); ++i)
   {
     CHECK(faces[i].attributes().textureName() == "someName");
-    CHECK(faces[i].attributes().offset() == vm::vec2f(0.5f, 0.5f));
-    CHECK(faces[i].attributes().scale() == vm::vec2f(0.5f, 0.5f));
+    CHECK(faces[i].attributes().offset() == vm::vec2f{0.5f, 0.5f});
+    CHECK(faces[i].attributes().scale() == vm::vec2f{0.5f, 0.5f});
     CHECK(faces[i].attributes().rotation() == 45.0f);
     CHECK(faces[i].attributes().surfaceContents() == 1);
     CHECK(faces[i].attributes().surfaceFlags() == 2);
     CHECK(faces[i].attributes().surfaceValue() == 0.1f);
-    CHECK(faces[i].attributes().color() == Color(255, 255, 255, 255));
+    CHECK(faces[i].attributes().color() == Color{255, 255, 255, 255});
   }
 }
-} // namespace Model
-} // namespace TrenchBroom
+
+} // namespace TrenchBroom::Model
