1771f38b7d4d077724b4619066e3c123bdcf1faa
Kristian Duske <kristian.duske@gmail.com>
2024-06-02 22:59:40 +0200

    Adapt naming in tst_SetEntityProperties

--- common/test/src/View/tst_SetEntityProperties.cpp
+++ common/test/src/View/tst_SetEntityProperties.cpp
@@ -426,7 +426,7 @@ TEST_CASE_METHOD(MapDocumentTest, "EntityNodesTest.updateSpawnflagOnBrushEntity"
     Model::BrushBuilder{document->world()->mapFormat(), document->worldBounds()};
 
   auto* brushNode = new Model::BrushNode{
-    builder.createCuboid(vm::bbox3{{0, 0, 0}, {64, 64, 64}}, "texture").value()};
+    builder.createCuboid(vm::bbox3{{0, 0, 0}, {64, 64, 64}}, "material").value()};
   document->addNodes({{document->parentForNodes(), {brushNode}}});
 
   document->selectAllNodes();
