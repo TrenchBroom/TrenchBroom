TrenchBroom/TrenchBroom

commit 0e465ce6a59e8df61e7d73ac1ffcd03316559d7b

Upgrade to c++20 and fix build errors

common/src/View/CompilationProfileEditor.cpp
@@ -136,12 +136,12 @@ Variables are allowed.)");
 
       auto menu = QMenu{this};
       auto* moveUpAction =
-        menu.addAction(tr("Move Up"), this, [=]() { moveTaskUp(index); });
+        menu.addAction(tr("Move Up"), this, [this, index]() { moveTaskUp(index); });
       auto* moveDownAction =
-        menu.addAction(tr("Move Down"), this, [=]() { moveTaskDown(index); });
+        menu.addAction(tr("Move Down"), this, [this, index]() { moveTaskDown(index); });
       menu.addSeparator();
-      menu.addAction(tr("Duplicate"), this, [=]() { duplicateTask(index); });
-      menu.addAction(tr("Remove"), this, [=]() { removeTask(index); });
+      menu.addAction(tr("Duplicate"), this, [this, index]() { duplicateTask(index); });
+      menu.addAction(tr("Remove"), this, [this, index]() { removeTask(index); });
 
       moveUpAction->setEnabled(index > 0);
       moveDownAction->setEnabled(

common/src/View/IssueBrowserView.cpp
@@ -295,8 +295,8 @@ void IssueBrowserView::itemRightClicked(const QPoint& pos)
     for (const auto* quickFix : quickFixes)
     {
       quickFixMenu->addAction(
-        QString::fromStdString(quickFix->description()), this, [=]() {
-          this->applyQuickFix(*quickFix);
+        QString::fromStdString(quickFix->description()), this, [&]() {
+          applyQuickFix(*quickFix);
         });
     }
 

CMakeLists.txt
@@ -29,7 +29,7 @@ set_property(GLOBAL PROPERTY USE_FOLDERS ON)
 # Set TrenchBroom item as default single startup project (NOTE: CMake 3.6 and newer)
 set_property(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} PROPERTY VS_STARTUP_PROJECT TrenchBroom)
 
-set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
 

common/src/IO/File.cpp
@@ -64,7 +64,7 @@ Result<kdl::resource<std::FILE*>> openPathAsFILE(
 #ifdef _WIN32
     _wfopen(path.wstring().c_str(), std::wstring{mode.begin(), mode.end()}.c_str());
 #else
-    fopen(path.u8string().c_str(), mode.c_str());
+    fopen(path.string().c_str(), mode.c_str());
 #endif
 
   if (!file)

common/src/IO/ZipFileSystem.cpp
@@ -77,7 +77,7 @@ Result<void> ZipFileSystem::doReadDirectory()
     if (!mz_zip_reader_is_file_a_directory(&m_archive, i))
     {
       const auto path = std::filesystem::path{filename(m_archive, i)};
-      addFile(path, [=]() -> Result<std::shared_ptr<File>> {
+      addFile(path, [&, i, path]() -> Result<std::shared_ptr<File>> {
         auto loadFileGoard = std::lock_guard{m_mutex};
 
         auto stat = mz_zip_archive_file_stat{};

common/src/View/CollapsibleTitledPanel.cpp
@@ -46,7 +46,7 @@ CollapsibleTitledPanel::CollapsibleTitledPanel(
   sizer->addWidget(m_panel, 1);
   setLayout(sizer);
 
-  connect(m_titleBar, &ClickableTitleBar::titleBarClicked, this, [=]() {
+  connect(m_titleBar, &ClickableTitleBar::titleBarClicked, this, [&]() {
     setExpanded(!m_expanded);
   });
 

common/src/View/CompilationProfileListBox.cpp
@@ -93,10 +93,11 @@ ControlListBoxItemRenderer* CompilationProfileListBox::createItemRenderer(
 {
   auto& profile = m_config.profiles[index];
   auto* renderer = new CompilationProfileItemRenderer{profile, parent};
-  connect(renderer, &QWidget::customContextMenuRequested, this, [=](const QPoint& pos) {
-    emit this->profileContextMenuRequested(
-      renderer->mapToGlobal(pos), m_config.profiles[index]);
-  });
+  connect(
+    renderer, &QWidget::customContextMenuRequested, this, [&, index](const QPoint& pos) {
+      emit this->profileContextMenuRequested(
+        renderer->mapToGlobal(pos), m_config.profiles[index]);
+    });
   return renderer;
 }
 } // namespace View

commit 8695bc1d20f1679782c9af43674f0b30ad7d8466

Don't capture local variables by reference
This error was introduced in 0e465ce6 when we changed the default
capture mode to silence warnings in C++20.

common/src/View/CompilationProfileEditor.cpp
@@ -136,12 +136,12 @@ Variables are allowed.)");
 
       auto menu = QMenu{this};
       auto* moveUpAction =
-        menu.addAction(tr("Move Up"), this, [this, index]() { moveTaskUp(index); });
+        menu.addAction(tr("Move Up"), this, [&, index]() { moveTaskUp(index); });
       auto* moveDownAction =
-        menu.addAction(tr("Move Down"), this, [this, index]() { moveTaskDown(index); });
+        menu.addAction(tr("Move Down"), this, [&, index]() { moveTaskDown(index); });
       menu.addSeparator();
-      menu.addAction(tr("Duplicate"), this, [this, index]() { duplicateTask(index); });
-      menu.addAction(tr("Remove"), this, [this, index]() { removeTask(index); });
+      menu.addAction(tr("Duplicate"), this, [&, index]() { duplicateTask(index); });
+      menu.addAction(tr("Remove"), this, [&, index]() { removeTask(index); });
 
       moveUpAction->setEnabled(index > 0);
       moveDownAction->setEnabled(

common/src/View/IssueBrowserView.cpp
@@ -295,7 +295,7 @@ void IssueBrowserView::itemRightClicked(const QPoint& pos)
     for (const auto* quickFix : quickFixes)
     {
       quickFixMenu->addAction(
-        QString::fromStdString(quickFix->description()), this, [&]() {
+        QString::fromStdString(quickFix->description()), this, [&, quickFix]() {
           applyQuickFix(*quickFix);
         });
     }