1b12ba3e4e5d9acb58adc6176c150bca9e33814a
Kristian Duske <kristian.duske@gmail.com>
2024-10-13 22:25:35 +0200

    Add snapping for UV shearing

--- common/src/ui/UVViewHelper.cpp
+++ common/src/ui/UVViewHelper.cpp
@@ -165,14 +165,13 @@ void UVViewHelper::pickUVGrid(
       // (i.e. so the X component is the distance to the closest vertical gridline, and
       // the Y the distance to the closest horizontal gridline.)
       const auto distanceFromGridUVCoords =
-        computeDistanceFromUVGrid(vm::vec3d(hitPointInUVCoords, 0.0f));
+        computeDistanceFromUVGrid(vm::vec3d{hitPointInUVCoords, 0});
       const vm::vec2f closestPointsOnGridInUVCoords[2] = {
-        hitPointInUVCoords
-          + vm::vec2f{distanceFromGridUVCoords.x(), 0.0f}, // closest point on a vertical
-                                                           // gridline
-        hitPointInUVCoords
-          + vm::vec2f{0.0f, distanceFromGridUVCoords.y()}, // closest point on a
-                                                           // horizontal gridline
+        // closest point on a vertical gridline
+        hitPointInUVCoords + vm::vec2f{distanceFromGridUVCoords.x(), 0},
+        // closest point on a horizontal gridline
+        hitPointInUVCoords + vm::vec2f{0, distanceFromGridUVCoords.y()},
+
       };
 
       // FIXME: should be measured in points so the grid isn't harder to hit with high-DPI
