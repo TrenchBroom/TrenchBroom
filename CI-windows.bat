e61d2d2e1beef928fa59c1d7d624f6387abb0dd4
Kristian Duske <kristian.duske@gmail.com>
2025-04-07 09:48:16 +0200

    Use ninja on Windows CI

--- CI-windows.bat
+++ CI-windows.bat
@@ -1,6 +1,7 @@
 REM Check versions
 qmake -v
 cmake --version
+ninja --version
 pandoc --version
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
@@ -11,7 +12,8 @@ REM Treat warnings as errors
 REM Don't pass -DCMAKE_CXX_FLAGS="/WX" on the cmake command line; doing so wipes out necessary cmake-provided defaults such as "/EHsc"
 set CXXFLAGS="/WX"
 
-cmake .. -G"Visual Studio 17 2022" -T v143 -A "%TB_ARCH%" -DCMAKE_PREFIX_PATH="%QT_ROOT_DIR%" -DCMAKE_BUILD_TYPE=Release -DTB_ENABLE_PCH=0 -DTB_RUN_WINDEPLOYQT=1
+call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
+cmake .. -GNinja -DCMAKE_PREFIX_PATH="%QT_ROOT_DIR%" -DCMAKE_BUILD_TYPE=Release -DTB_ENABLE_PCH=0 -DTB_ENABLE_CCACHE=0 -DTB_RUN_WINDEPLOYQT=1
 
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
@@ -21,22 +23,22 @@ IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
 set BUILD_DIR="%cd%"
 
-cd lib\vm\test\Release
+cd lib\vm\test
 vm-test.exe
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 cd "%BUILD_DIR%"
 
-cd lib\kdl\test\Release
+cd lib\kdl\test
 kdl-test.exe
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 cd "%BUILD_DIR%"
 
-cd lib\upd\test\Release
+cd lib\upd\test
 upd-test.exe
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 cd "%BUILD_DIR%"
 
-cd common\test\Release
+cd common\test
 common-test.exe
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
@@ -44,7 +46,7 @@ common-regression-test.exe
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 cd "%BUILD_DIR%"
 
-cd common\benchmark\Release
+cd common\benchmark
 common-benchmark.exe
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 cd "%BUILD_DIR%"
