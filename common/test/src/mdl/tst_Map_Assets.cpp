36fd7339f14206cf9a435926e9b18c1112bdcb03
Kristian Duske <kristian.duske@gmail.com>
2025-09-08 22:22:05 +0200

    Add tests for Map_Assets

--- common/test/src/mdl/tst_Map_Assets.cpp
+++ common/test/src/mdl/tst_Map_Assets.cpp
@@ -19,29 +19,256 @@
 
 #include "MapFixture.h"
 #include "TestUtils.h"
+#include "io/TestEnvironment.h"
 #include "mdl/BrushFace.h"
 #include "mdl/BrushNode.h"
 #include "mdl/LayerNode.h"
 #include "mdl/Map.h"
 #include "mdl/Map_Assets.h"
+#include "mdl/Map_Entities.h"
+#include "mdl/MaterialManager.h"
+#include "mdl/Observer.h"
 #include "mdl/WorldNode.h"
 
-#include "kdl/vector_utils.h"
-
+#include <algorithm>
+#include <optional>
 #include <ranges>
+#include <string>
 
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+#include <catch2/matchers/catch_matchers_range_equals.hpp>
 
 namespace tb::mdl
 {
+using namespace Catch::Matchers;
 
 TEST_CASE("Map_Assets")
 {
   auto fixture = MapFixture{};
   auto& map = fixture.map();
 
+  SECTION("entityDefinitionFile")
+  {
+    using T = std::tuple<std::optional<std::string>, EntityDefinitionFileSpec>;
+
+    const auto [entityProperty, expectedEntityDefinitionFileSpec] = GENERATE(values<T>({
+      {std::nullopt, EntityDefinitionFileSpec::makeBuiltin("Quake.def")},
+      {"", EntityDefinitionFileSpec::makeBuiltin("Quake.def")},
+      {"asdf", EntityDefinitionFileSpec::makeBuiltin("Quake.def")},
+      {"builtin:ad.fgd", EntityDefinitionFileSpec::makeBuiltin("ad.fgd")},
+      {"builtin:Quake.def", EntityDefinitionFileSpec::makeBuiltin("Quake.def")},
+      {"external:/Applications/Quake/Quake.fgd",
+       EntityDefinitionFileSpec::makeExternal("/Applications/Quake/Quake.fgd")},
+    }));
+
+    CAPTURE(entityProperty);
+
+    auto mockGameConfig = MockGameConfig{};
+    mockGameConfig.entityConfig.defFilePaths = std::vector<std::filesystem::path>{
+      "Quake.def",
+      "ad.fgd",
+      "Quoth.fgd",
+    };
+
+    fixture.create({.game = MockGameFixture{std::move(mockGameConfig)}});
+
+    if (entityProperty)
+    {
+      setEntityProperty(map, EntityPropertyKeys::EntityDefinitions, *entityProperty);
+    }
+
+    CHECK(entityDefinitionFile(map) == expectedEntityDefinitionFileSpec);
+  }
+
+  SECTION("setEntityDefinitionFile")
+  {
+    auto entityDefinitionsWillChange =
+      Observer<void>{map.entityDefinitionsWillChangeNotifier};
+    auto entityDefinitionsDidChange =
+      Observer<void>{map.entityDefinitionsDidChangeNotifier};
+
+    using T = std::tuple<EntityDefinitionFileSpec, std::string>;
+
+    const auto [entityDefinitionFileSpec, expectedPropertyValue] = GENERATE(values<T>({
+      {EntityDefinitionFileSpec::makeBuiltin("Quake.def"), "builtin:Quake.def"},
+      {EntityDefinitionFileSpec::makeBuiltin("ad.fgd"), "builtin:ad.fgd"},
+      {
+        EntityDefinitionFileSpec::makeExternal("/Applications/Quake/Quake.fgd"),
+        "external:/Applications/Quake/Quake.fgd",
+      },
+    }));
+
+    CAPTURE(entityDefinitionFileSpec);
+
+    auto mockGameConfig = MockGameConfig{};
+    mockGameConfig.entityConfig.defFilePaths = std::vector<std::filesystem::path>{
+      "Quake.def",
+      "ad.fgd",
+      "Quoth.fgd",
+    };
+
+    fixture.create({.game = MockGameFixture{std::move(mockGameConfig)}});
+
+    setEntityDefinitionFile(map, entityDefinitionFileSpec);
+
+    CHECK(entityDefinitionsWillChange.called);
+    CHECK(entityDefinitionsDidChange.called);
+
+    const auto* worldNode = map.world();
+    const auto& entity = worldNode->entity();
+    const auto* propertyValue = entity.property(EntityPropertyKeys::EntityDefinitions);
+
+    REQUIRE(propertyValue);
+    CHECK(*propertyValue == expectedPropertyValue);
+  }
+
+  SECTION("enabledMaterialCollections")
+  {
+    fixture.create({.mapFormat = MapFormat::Quake2, .game = LoadGameFixture{"Quake2"}});
+
+    REQUIRE(map.materialManager().collections().size() == 3);
+
+    const auto* worldNode = map.world();
+    REQUIRE(worldNode);
+
+    SECTION("When no material collections are explicitly enabled")
+    {
+      REQUIRE(
+        worldNode->entity().property(EntityPropertyKeys::EnabledMaterialCollections)
+        == nullptr);
+
+      CHECK(
+        enabledMaterialCollections(map)
+        == std::vector<std::filesystem::path>{
+          "textures",
+          "textures/e1m1",
+          "textures/e1m1/f1",
+        });
+    }
+
+    SECTION("When a material collection is explicitly enabled")
+    {
+      setEntityProperty(
+        map,
+        EntityPropertyKeys::EnabledMaterialCollections,
+        "textures/e1m1;textures/e1m1/f1");
+
+      CHECK(
+        enabledMaterialCollections(map)
+        == std::vector<std::filesystem::path>{
+          "textures/e1m1",
+          "textures/e1m1/f1",
+        });
+    }
+
+    SECTION("Enabled material collections are sorted and unique")
+    {
+      setEntityProperty(
+        map,
+        EntityPropertyKeys::EnabledMaterialCollections,
+        "textures/e1m1/f1;textures/e1m1;textures/e1m1");
+
+      CHECK(
+        enabledMaterialCollections(map)
+        == std::vector<std::filesystem::path>{
+          "textures/e1m1",
+          "textures/e1m1/f1",
+        });
+    }
+
+    SECTION("Unknown material collections are returned")
+    {
+      setEntityProperty(
+        map,
+        EntityPropertyKeys::EnabledMaterialCollections,
+        "textures/unknown;textures/e1m1");
+
+      CHECK(
+        enabledMaterialCollections(map)
+        == std::vector<std::filesystem::path>{
+          "textures/e1m1",
+          "textures/unknown",
+        });
+    }
+  }
+
+  SECTION("disabledMaterialCollections")
+  {
+    fixture.create({.mapFormat = MapFormat::Quake2, .game = LoadGameFixture{"Quake2"}});
+
+    REQUIRE(map.materialManager().collections().size() == 3);
+
+    const auto* worldNode = map.world();
+    REQUIRE(worldNode);
+
+    SECTION("When no material collections are explicitly enabled")
+    {
+      REQUIRE(
+        worldNode->entity().property(EntityPropertyKeys::EnabledMaterialCollections)
+        == nullptr);
+
+      CHECK(disabledMaterialCollections(map) == std::vector<std::filesystem::path>{});
+    }
+
+    SECTION("When a material collection is explicitly enabled")
+    {
+      setEntityProperty(
+        map, EntityPropertyKeys::EnabledMaterialCollections, "textures/e1m1");
+
+      CHECK(
+        disabledMaterialCollections(map)
+        == std::vector<std::filesystem::path>{"textures", "textures/e1m1/f1"});
+    }
+  }
+
+  SECTION("setEnabledMaterialCollections")
+  {
+    fixture.create({.mapFormat = MapFormat::Quake2, .game = LoadGameFixture{"Quake2"}});
+
+    REQUIRE(map.materialManager().collections().size() == 3);
+
+    const auto* worldNode = map.world();
+    REQUIRE(worldNode);
+
+    const auto getEnabledMaterialCollections = [&] {
+      return worldNode->entity().property(EntityPropertyKeys::EnabledMaterialCollections);
+    };
+
+    REQUIRE(!getEnabledMaterialCollections());
+
+    SECTION("Setting an empty vector resets the property")
+    {
+      setEnabledMaterialCollections(map, {"textures/e1m1/f1"});
+      REQUIRE(getEnabledMaterialCollections());
+
+      setEnabledMaterialCollections(map, {});
+      CHECK(!getEnabledMaterialCollections());
+    }
+
+    SECTION("Setting a non-empty vector sets the property")
+    {
+      setEnabledMaterialCollections(map, {"textures/e1m1/f1"});
+      REQUIRE(getEnabledMaterialCollections());
+      CHECK(*getEnabledMaterialCollections() == "textures/e1m1/f1");
+    }
+
+    SECTION("Enabled material collections are sorted and unique")
+    {
+      setEnabledMaterialCollections(
+        map, {"textures/e1m1/f1", "textures/e1m1", "textures/e1m1"});
+      REQUIRE(getEnabledMaterialCollections());
+      CHECK(*getEnabledMaterialCollections() == "textures/e1m1;textures/e1m1/f1");
+    }
+  }
+
   SECTION("reloadMaterialCollections")
   {
+    auto materialCollectionsWillChange =
+      Observer<void>{map.materialCollectionsWillChangeNotifier};
+    auto materialCollectionsDidChange =
+      Observer<void>{map.materialCollectionsDidChangeNotifier};
+
     fixture.load(
       "fixture/test/mdl/Map/reloadMaterialCollectionsQ2.map",
       {.mapFormat = MapFormat::Quake2, .game = LoadGameFixture{"Quake2"}});
@@ -56,19 +283,54 @@ TEST_CASE("Map_Assets")
                        | kdl::ranges::to<std::vector>();
 
     REQUIRE(faces.size() == 4);
-    REQUIRE(
-      kdl::vec_transform(
-        faces, [](const auto* face) { return face->attributes().materialName(); })
-      == std::vector<std::string>{
-        "b_pv_v1a1", "e1m1/b_pv_v1a2", "e1m1/f1/b_rc_v4", "lavatest"});
+    REQUIRE_THAT(
+      faces | std::views::transform([](const auto* face) {
+        return face->attributes().materialName();
+      }),
+      RangeEquals(std::vector<std::string>{
+        "b_pv_v1a1", "e1m1/b_pv_v1a2", "e1m1/f1/b_rc_v4", "lavatest"}));
+
+    REQUIRE(std::ranges::none_of(
+      faces, [](const auto* face) { return face->material() == nullptr; }));
+
+    reloadMaterialCollections(map);
+    CHECK(materialCollectionsWillChange.called);
+    CHECK(materialCollectionsDidChange.called);
+
+    CHECK(std::ranges::none_of(
+      faces, [](const auto* face) { return face->material() == nullptr; }));
+  }
+
+  SECTION("reloadEntityDefinitions")
+  {
+    auto entityDefinitionsWillChange =
+      Observer<void>{map.entityDefinitionsWillChangeNotifier};
+    auto entityDefinitionsDidChange =
+      Observer<void>{map.entityDefinitionsDidChangeNotifier};
+
+    const auto fgdFilename = "Test.fgd";
+
+    auto env = io::TestEnvironment{};
+    env.createFile(fgdFilename, R"x(
+@SolidClass = worldspawn : "World entity"
+[
+  message(string) : "Text on entering the world"
+]
+    )x");
+
+    fixture.create();
+
+    setEntityDefinitionFile(
+      map, EntityDefinitionFileSpec::makeExternal(env.dir() / fgdFilename));
 
     REQUIRE(
-      kdl::none_of(faces, [](const auto* face) { return face->material() == nullptr; }));
+      entityDefinitionFile(map)
+      == EntityDefinitionFileSpec::makeExternal(env.dir() / fgdFilename));
 
-    CHECK_NOTHROW(reloadMaterialCollections(map));
+    reloadEntityDefinitions(map);
 
-    REQUIRE(
-      kdl::none_of(faces, [](const auto* face) { return face->material() == nullptr; }));
+    CHECK(entityDefinitionsWillChange.called);
+    CHECK(entityDefinitionsDidChange.called);
   }
 }
 
