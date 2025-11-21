f4139d6c1db371976c28cb1d5ee8d77fb3b6c97b
Kristian Duske <kristian.duske@gmail.com>
2025-11-21 10:39:24 +0100

    Remove unused AssetException

--- common/src/Exceptions.h
+++ common/src/Exceptions.h
@@ -37,12 +37,6 @@ public:
   const char* what() const noexcept override;
 };
 
-class AssetException : public Exception
-{
-public:
-  using Exception::Exception;
-};
-
 class RenderException : public Exception
 {
 public:
