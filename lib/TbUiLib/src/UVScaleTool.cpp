b8b37660ff60e4b4fe77c1fcf85dd53a8834bd20
Kristian Duske <kristian.duske@gmail.com>
2026-01-04 13:16:14 +0100

    Cleanup: remove unnecessary parameter

--- lib/TbUiLib/src/UVScaleTool.cpp
+++ lib/TbUiLib/src/UVScaleTool.cpp
@@ -77,8 +77,7 @@ std::optional<vm::vec2f> getHitPoint(const UVViewHelper& helper, const vm::ray3d
   return vm::intersect_ray_plane(pickRay, boundary)
          | kdl::optional_transform([&](const auto facePointDist) {
              const auto facePoint = vm::point_at_distance(pickRay, facePointDist);
-             const auto toTex =
-               helper.face()->toUVCoordSystemMatrix({0, 0}, {1, 1}, true);
+             const auto toTex = helper.face()->toUVCoordSystemMatrix({0, 0}, {1, 1});
 
              return vm::vec2f{toTex * facePoint};
            });
@@ -92,9 +91,9 @@ vm::vec2f getScaledTranslatedHandlePos(const UVViewHelper& helper, const vm::vec
 vm::vec2f getHandlePos(const UVViewHelper& helper, const vm::vec2i handle)
 {
   const auto toWorld = helper.face()->fromUVCoordSystemMatrix(
-    helper.face()->attributes().offset(), helper.face()->attributes().scale(), true);
+    helper.face()->attributes().offset(), helper.face()->attributes().scale());
   const auto toTex =
-    helper.face()->toUVCoordSystemMatrix(vm::vec2f{0, 0}, vm::vec2f{1, 1}, true);
+    helper.face()->toUVCoordSystemMatrix(vm::vec2f{0, 0}, vm::vec2f{1, 1});
 
   return vm::vec2f{
     toTex * toWorld * vm::vec3d{getScaledTranslatedHandlePos(helper, handle)}};
@@ -103,7 +102,7 @@ vm::vec2f getHandlePos(const UVViewHelper& helper, const vm::vec2i handle)
 vm::vec2f snap(const UVViewHelper& helper, const vm::vec2f& position)
 {
   const auto toTex =
-    helper.face()->toUVCoordSystemMatrix(vm::vec2f{0, 0}, vm::vec2f{1, 1}, true);
+    helper.face()->toUVCoordSystemMatrix(vm::vec2f{0, 0}, vm::vec2f{1, 1});
 
   const auto vertices = helper.face()->vertices();
   auto distance = std::accumulate(
