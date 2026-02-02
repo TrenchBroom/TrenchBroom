c94d634aad422e1495453c6f1fccd0aa49a998ec
Kristian Duske <kristian.duske@gmail.com>
2026-02-02 16:57:20 +0000

    Add alignUV, justifyUV, fitUV functions

--- lib/TbMdlLib/src/Map_Brushes.cpp
+++ lib/TbMdlLib/src/Map_Brushes.cpp
@@ -30,11 +30,147 @@
 #include "mdl/Map_Nodes.h"
 #include "mdl/Map_Selection.h"
 #include "mdl/Transaction.h"
+#include "mdl/UVUtils.h"
 #include "mdl/UpdateBrushFaceAttributes.h"
 #include "mdl/WorldNode.h"
 
 namespace tb::mdl
 {
+namespace
+{
+
+auto invertHorizontalAxis(
+  const mdl::UvAxis horizontalUvAxis,
+  const vm::vec3f& uAxis,
+  const vm::vec3f& vAxis,
+  const vm::vec3f& rightAxis)
+{
+  switch (horizontalUvAxis)
+  {
+  case mdl::UvAxis::u:
+    return vm::dot(uAxis, rightAxis) >= 0.0f;
+  case mdl::UvAxis::v:
+    return vm::dot(vAxis, rightAxis) >= 0.0f;
+    switchDefault();
+  }
+}
+
+auto invertVerticalAxis(
+  const mdl::UvAxis horizontalUvAxis,
+  const vm::vec3f& uAxis,
+  const vm::vec3f& vAxis,
+  const vm::vec3f& upAxis)
+{
+  switch (horizontalUvAxis)
+  {
+  case mdl::UvAxis::u:
+    return vm::dot(vAxis, upAxis) >= 0.0f;
+  case mdl::UvAxis::v:
+    return vm::dot(uAxis, upAxis) >= 0.0f;
+    switchDefault();
+  }
+}
+
+auto getDirectionAxes(
+  const vm::vec3f& uAxis,
+  const vm::vec3f& vAxis,
+  const vm::vec3f& upAxis,
+  const vm::vec3f& rightAxis)
+{
+  // Which UV axis corresponds is horizontal?
+  const auto [horizontalUvAxis, verticalUvAxis] =
+    vm::abs(vm::dot(uAxis, rightAxis)) >= vm::abs(vm::dot(vAxis, rightAxis))
+      ? std::tuple{mdl::UvAxis::u, mdl::UvAxis::v}
+      : std::tuple{mdl::UvAxis::v, mdl::UvAxis::u};
+
+  const auto invertH = invertHorizontalAxis(horizontalUvAxis, uAxis, vAxis, rightAxis);
+  const auto invertV = invertVerticalAxis(horizontalUvAxis, uAxis, vAxis, upAxis);
+
+  return std::tuple{horizontalUvAxis, verticalUvAxis, invertH, invertV};
+}
+
+std::tuple<mdl::UvAxis, mdl::UvSign> convertJustifyDirection(
+  const BrushFace& brushFace, const UvJustifyDirection uvJustifyDirection)
+{
+  const auto [cameraUp, cameraRight] = computeCameraAxesForFaceNormal(brushFace.normal());
+
+  const auto uAxis = vm::vec3f{brushFace.uAxis()};
+  const auto vAxis = vm::vec3f{brushFace.vAxis()};
+
+  const auto [horizontalUvAxis, verticalUvAxis, invertH, invertV] =
+    getDirectionAxes(uAxis, vAxis, vm::vec3f{cameraUp}, vm::vec3f{cameraRight});
+
+  switch (uvJustifyDirection)
+  {
+  case UvJustifyDirection::Left:
+    return std::tuple{horizontalUvAxis, invertH ? mdl::UvSign::minus : mdl::UvSign::plus};
+  case UvJustifyDirection::Right:
+    return std::tuple{horizontalUvAxis, invertH ? mdl::UvSign::plus : mdl::UvSign::minus};
+  case UvJustifyDirection::Up:
+    return std::tuple{verticalUvAxis, invertV ? mdl::UvSign::minus : mdl::UvSign::plus};
+  case UvJustifyDirection::Down:
+    return std::tuple{verticalUvAxis, invertV ? mdl::UvSign::plus : mdl::UvSign::minus};
+    switchDefault();
+  }
+}
+
+std::tuple<mdl::UvAxis, mdl::UvSign> convertFitDirection(
+  const BrushFace& brushFace, const UvFitDirection fitDirection)
+{
+  const auto [cameraUp, cameraRight] = computeCameraAxesForFaceNormal(brushFace.normal());
+
+  const auto uAxis = vm::vec3f{brushFace.uAxis()};
+  const auto vAxis = vm::vec3f{brushFace.vAxis()};
+
+  const auto [horizontalUvAxis, verticalUvAxis, invertH, invertV] =
+    getDirectionAxes(uAxis, vAxis, vm::vec3f{cameraUp}, vm::vec3f{cameraRight});
+
+  switch (fitDirection)
+  {
+  case UvFitDirection::Horizontal:
+    return std::tuple{horizontalUvAxis, invertH ? mdl::UvSign::minus : mdl::UvSign::plus};
+  case UvFitDirection::Vertical:
+    return std::tuple{verticalUvAxis, invertV ? mdl::UvSign::minus : mdl::UvSign::plus};
+    switchDefault();
+  }
+}
+
+template <typename F>
+void withInvariantUvCoords(
+  Map& map,
+  const BrushFaceHandle faceHandle,
+  const std::optional<vm::vec3d>& vertex,
+  const F& f)
+{
+  if (vertex)
+  {
+    const auto previousUvCoords = vm::vec2f{
+      faceHandle.face().toUVCoordSystemMatrix(
+        faceHandle.face().attributes().offset(), faceHandle.face().attributes().scale())
+      * *vertex};
+
+    f(faceHandle.face());
+
+    const auto newUvCoords = vm::vec2f{
+      faceHandle.face().toUVCoordSystemMatrix(
+        faceHandle.face().attributes().offset(), faceHandle.face().attributes().scale())
+      * *vertex};
+    const auto delta = previousUvCoords - newUvCoords;
+
+    setBrushFaceAttributes(
+      map,
+      {
+        .xOffset = mdl::AddValue{delta.x()},
+        .yOffset = mdl::AddValue{delta.y()},
+      });
+  }
+  else
+  {
+    f(faceHandle.face());
+  }
+}
+
+} // namespace
 
 bool createBrush(Map& map, const std::vector<vm::vec3d>& points)
 {
@@ -137,6 +273,52 @@ bool flipUV(
         vm::vec3d{cameraUp}, vm::vec3d{cameraRight}, cameraRelativeFlipDirection);
       return true;
     });
+}
+
+void alignUV(Map& map, const UvPolicy uvPolicy)
+{
+  auto transaction = Transaction{map, "Align Texture"};
+
+  for (const auto& faceHandle : map.selection().brushFaces)
+  {
+    setBrushFaceAttributes(map, mdl::align(faceHandle.face(), uvPolicy));
+  }
+
+  transaction.commit();
+}
+
+void justifyUV(
+  Map& map, const UvJustifyDirection uvJustifyDirection, const UvPolicy uvPolicy)
+{
+  auto transaction = Transaction{map, "Justify Texture"};
+
+  for (const auto& faceHandle : map.selection().brushFaces)
+  {
+    const auto [uvAxis, uvSign] =
+      convertJustifyDirection(faceHandle.face(), uvJustifyDirection);
+
+    setBrushFaceAttributes(
+      map, mdl::justify(faceHandle.face(), uvAxis, uvSign, uvPolicy));
+  }
+
+  transaction.commit();
+}
+
+void fitUV(Map& map, const UvFitDirection uvFitDirection, const UvPolicy uvPolicy)
+{
+  auto transaction = Transaction{map, "Fit Texture"};
+
+  for (const auto& faceHandle : map.selection().brushFaces)
+  {
+    const auto [uvAxis, uvSign] = convertFitDirection(faceHandle.face(), uvFitDirection);
+
+    const auto invariantVertex = anchorVertex(faceHandle.face(), uvAxis, uvSign);
+    withInvariantUvCoords(map, faceHandle, invariantVertex, [&](const auto& brushFace) {
+      setBrushFaceAttributes(map, mdl::fit(brushFace, uvAxis, uvPolicy));
+    });
+  }
+
+  transaction.commit();
 }
 
 } // namespace tb::mdl
