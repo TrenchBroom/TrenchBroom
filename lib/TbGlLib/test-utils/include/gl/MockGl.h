290d4c9b87ca453d2ff5a2d716e7d758ba8cbce6
Kristian Duske <kristian.duske@gmail.com>
2026-07-31 12:04:34 +0200

    Introduce MockGl

---
+++ lib/TbGlLib/test-utils/include/gl/MockGl.h
@@ -0,0 +1,371 @@
+/*
+ Copyright (C) 2026 Kristian Duske
+
+ This file is part of TrenchBroom.
+
+ TrenchBroom is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ TrenchBroom is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with TrenchBroom. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "gl/GlInterface.h"
+
+#include <functional>
+#include <stdexcept>
+#include <string_view>
+
+namespace tb::gl
+{
+
+/**
+ * Thrown by MockGl when a function is called for which no slot has been assigned.
+ */
+class MockGlUnexpectedCall : public std::runtime_error
+{
+public:
+  explicit MockGlUnexpectedCall(std::string_view function);
+};
+
+/**
+ * A Gl implementation for tests that need to observe or control calls to the Gl
+ * interface. Each virtual function delegates to a public std::function slot named
+ * `on<FunctionName>`. Assign a slot before a test exercises the corresponding call;
+ * calling a function whose slot is empty throws MockGlUnexpectedCall.
+ *
+ * Use TestGl instead if the code under test does not need this level of control.
+ */
+class MockGl : public Gl
+{
+public:
+  std::function<void(GLbitfield)> onClear;
+  std::function<void(GLfloat, GLfloat, GLfloat, GLfloat)> onClearColor;
+
+  std::function<void(GLint, GLint, GLsizei, GLsizei)> onViewport;
+
+  std::function<void(GLenum)> onMatrixMode;
+  std::function<void(const GLdouble*)> onLoadMatrixd;
+  std::function<void(const GLfloat*)> onLoadMatrixf;
+
+  std::function<void(GLenum, GLboolean*)> onGetBooleanv;
+  std::function<void(GLenum, GLdouble*)> onGetDoublev;
+  std::function<void(GLenum, GLfloat*)> onGetFloatv;
+  std::function<void(GLenum, GLint*)> onGetIntegerv;
+
+  std::function<void(GLenum)> onEnableClientState;
+  std::function<void(GLenum)> onDisableClientState;
+
+  std::function<void(GLbitfield)> onPushAttrib;
+  std::function<void()> onPopAttrib;
+
+  std::function<void(GLenum)> onEnable;
+  std::function<void(GLenum)> onDisable;
+
+  std::function<void(GLfloat)> onLineWidth;
+
+  std::function<void(GLenum, GLenum)> onPolygonMode;
+
+  std::function<void(GLenum)> onFrontFace;
+  std::function<void(GLenum)> onCullFace;
+
+  std::function<void(GLenum, GLenum)> onBlendFunc;
+
+  std::function<void(GLenum)> onShadeModel;
+
+  std::function<void(GLboolean)> onDepthMask;
+  std::function<void(GLclampd, GLclampd)> onDepthRange;
+  std::function<void(GLenum)> onDepthFunc;
+
+  std::function<GLuint()> onCreateProgram;
+  std::function<void(GLuint)> onDeleteProgram;
+
+  std::function<void(GLuint)> onLinkProgram;
+
+  std::function<void(GLuint, GLsizei, GLsizei*, GLchar*)> onGetProgramInfoLog;
+
+  std::function<void(GLuint, GLenum, GLint*)> onGetProgramiv;
+
+  std::function<void(GLuint)> onUseProgram;
+
+  std::function<GLuint(GLenum)> onCreateShader;
+  std::function<void(GLuint)> onDeleteShader;
+
+  std::function<void(GLuint, GLuint)> onAttachShader;
+
+  std::function<void(GLuint, GLsizei, const GLchar* const*, const GLint*)> onShaderSource;
+  std::function<void(GLuint)> onCompileShader;
+
+  std::function<void(GLuint, GLsizei, GLsizei*, GLchar*)> onGetShaderInfoLog;
+
+  std::function<void(GLuint, GLenum, GLint*)> onGetShaderiv;
+
+  std::function<void(GLint, GLfloat)> onUniform1f;
+  std::function<void(GLint, GLfloat, GLfloat)> onUniform2f;
+  std::function<void(GLint, GLfloat, GLfloat, GLfloat)> onUniform3f;
+  std::function<void(GLint, GLfloat, GLfloat, GLfloat, GLfloat)> onUniform4f;
+
+  std::function<void(GLint, GLint)> onUniform1i;
+  std::function<void(GLint, GLint, GLint)> onUniform2i;
+  std::function<void(GLint, GLint, GLint, GLint)> onUniform3i;
+  std::function<void(GLint, GLint, GLint, GLint, GLint)> onUniform4i;
+
+  std::function<void(GLint, GLsizei, const GLfloat*)> onUniform1fv;
+  std::function<void(GLint, GLsizei, const GLfloat*)> onUniform2fv;
+  std::function<void(GLint, GLsizei, const GLfloat*)> onUniform3fv;
+  std::function<void(GLint, GLsizei, const GLfloat*)> onUniform4fv;
+  std::function<void(GLint, GLsizei, const GLint*)> onUniform1iv;
+  std::function<void(GLint, GLsizei, const GLint*)> onUniform2iv;
+  std::function<void(GLint, GLsizei, const GLint*)> onUniform3iv;
+  std::function<void(GLint, GLsizei, const GLint*)> onUniform4iv;
+
+  std::function<void(GLint, GLsizei, GLboolean, const GLfloat*)> onUniformMatrix2fv;
+  std::function<void(GLint, GLsizei, GLboolean, const GLfloat*)> onUniformMatrix3fv;
+  std::function<void(GLint, GLsizei, GLboolean, const GLfloat*)> onUniformMatrix4fv;
+  std::function<void(GLint, GLsizei, GLboolean, const GLfloat*)> onUniformMatrix2x3fv;
+  std::function<void(GLint, GLsizei, GLboolean, const GLfloat*)> onUniformMatrix3x2fv;
+  std::function<void(GLint, GLsizei, GLboolean, const GLfloat*)> onUniformMatrix2x4fv;
+  std::function<void(GLint, GLsizei, GLboolean, const GLfloat*)> onUniformMatrix4x2fv;
+  std::function<void(GLint, GLsizei, GLboolean, const GLfloat*)> onUniformMatrix3x4fv;
+  std::function<void(GLint, GLsizei, GLboolean, const GLfloat*)> onUniformMatrix4x3fv;
+
+  std::function<GLint(GLuint, const GLchar*)> onGetAttribLocation;
+  std::function<GLint(GLuint, const GLchar*)> onGetUniformLocation;
+
+  std::function<void(GLsizei, GLuint*)> onGenBuffers;
+  std::function<void(GLsizei, const GLuint*)> onDeleteBuffers;
+
+  std::function<void(GLenum, GLuint)> onBindBuffer;
+  std::function<void(GLenum, GLsizeiptr, const GLvoid*, GLenum)> onBufferData;
+  std::function<void(GLenum, GLintptr, GLsizeiptr, const void*)> onBufferSubData;
+
+  std::function<void(GLint, GLenum, GLsizei, const GLvoid*)> onVertexPointer;
+  std::function<void(GLint, GLenum, GLsizei, const GLvoid*)> onColorPointer;
+  std::function<void(GLenum, GLsizei, const GLvoid*)> onNormalPointer;
+  std::function<void(GLint, GLenum, GLsizei, const GLvoid*)> onTexCoordPointer;
+
+  std::function<void(GLuint)> onEnableVertexAttribArray;
+  std::function<void(GLuint)> onDisableVertexAttribArray;
+
+  std::function<void(GLuint, GLint, GLenum, GLboolean, GLsizei, const void*)>
+    onVertexAttribPointer;
+
+  std::function<void(GLsizei, GLuint*)> onGenTextures;
+  std::function<void(GLsizei, const GLuint*)> onDeleteTextures;
+
+  std::function<void(GLenum, GLuint)> onBindTexture;
+  std::function<void(GLenum)> onActiveTexture;
+
+  std::function<void(
+    GLenum, GLint, GLint, GLsizei, GLsizei, GLint, GLenum, GLenum, const GLvoid*)>
+    onTexImage2D;
+
+  std::function<void(
+    GLenum, GLint, GLenum, GLsizei, GLsizei, GLint, GLsizei, const GLvoid*)>
+    onCompressedTexImage2D;
+
+  std::function<void(GLenum, GLenum, GLfloat)> onTexParameterf;
+  std::function<void(GLenum, GLenum, GLint)> onTexParameteri;
+
+  std::function<void(GLenum, GLfloat)> onPixelStoref;
+  std::function<void(GLenum, GLint)> onPixelStorei;
+
+  std::function<void(GLenum)> onClientActiveTexture;
+
+  std::function<void(GLenum, GLint, GLsizei)> onDrawArrays;
+  std::function<void(GLenum, GLsizei, GLenum, const void*)> onDrawElements;
+  std::function<void(GLenum, const GLint*, const GLsizei*, GLsizei)> onMultiDrawArrays;
+
+  std::function<const GLubyte*(GLenum)> onGetString;
+  std::function<GLenum()> onGetError;
+
+  void clear(GLbitfield mask) override;
+  void clearColor(GLfloat red, GLfloat green, GLfloat blue, GLfloat alpha) override;
+
+  void viewport(GLint x, GLint y, GLsizei width, GLsizei height) override;
+
+  void matrixMode(GLenum mode) override;
+  void loadMatrixd(const GLdouble* matrix) override;
+  void loadMatrixf(const GLfloat* matrix) override;
+
+  void getBooleanv(GLenum pname, GLboolean* params) override;
+  void getDoublev(GLenum pname, GLdouble* params) override;
+  void getFloatv(GLenum pname, GLfloat* params) override;
+  void getIntegerv(GLenum pname, GLint* params) override;
+
+  void enableClientState(GLenum cap) override;
+  void disableClientState(GLenum cap) override;
+
+  void pushAttrib(GLbitfield mask) override;
+  void popAttrib() override;
+
+  void enable(GLenum cap) override;
+  void disable(GLenum cap) override;
+
+  void lineWidth(GLfloat width) override;
+
+  void polygonMode(GLenum face, GLenum mode) override;
+
+  void frontFace(GLenum mode) override;
+  void cullFace(GLenum mode) override;
+
+  void blendFunc(GLenum sfactor, GLenum dfactor) override;
+
+  void shadeModel(GLenum mode) override;
+
+  void depthMask(GLboolean flag) override;
+  void depthRange(GLclampd nearVal, GLclampd farVal) override;
+  void depthFunc(GLenum func) override;
+
+  GLuint createProgram() override;
+  void deleteProgram(GLuint program) override;
+
+  void linkProgram(GLuint program) override;
+
+  void getProgramInfoLog(
+    GLuint program, GLsizei maxLength, GLsizei* length, GLchar* infoLog) override;
+
+  void getProgramiv(GLuint program, GLenum pname, GLint* params) override;
+
+  void useProgram(GLuint program) override;
+
+  GLuint createShader(GLenum shaderType) override;
+  void deleteShader(GLuint shader) override;
+
+  void attachShader(GLuint program, GLuint shader) override;
+
+  void shaderSource(
+    GLuint shader,
+    GLsizei count,
+    const GLchar* const* string,
+    const GLint* length) override;
+  void compileShader(GLuint shader) override;
+
+  void getShaderInfoLog(
+    GLuint shader, GLsizei maxLength, GLsizei* length, GLchar* infoLog) override;
+
+  void getShaderiv(GLuint shader, GLenum pname, GLint* params) override;
+
+  void uniform1f(GLint location, GLfloat v0) override;
+  void uniform2f(GLint location, GLfloat v0, GLfloat v1) override;
+  void uniform3f(GLint location, GLfloat v0, GLfloat v1, GLfloat v2) override;
+  void uniform4f(GLint location, GLfloat v0, GLfloat v1, GLfloat v2, GLfloat v3) override;
+
+  void uniform1i(GLint location, GLint v0) override;
+  void uniform2i(GLint location, GLint v0, GLint v1) override;
+  void uniform3i(GLint location, GLint v0, GLint v1, GLint v2) override;
+  void uniform4i(GLint location, GLint v0, GLint v1, GLint v2, GLint v3) override;
+
+  void uniform1fv(GLint location, GLsizei count, const GLfloat* value) override;
+  void uniform2fv(GLint location, GLsizei count, const GLfloat* value) override;
+  void uniform3fv(GLint location, GLsizei count, const GLfloat* value) override;
+  void uniform4fv(GLint location, GLsizei count, const GLfloat* value) override;
+  void uniform1iv(GLint location, GLsizei count, const GLint* value) override;
+  void uniform2iv(GLint location, GLsizei count, const GLint* value) override;
+  void uniform3iv(GLint location, GLsizei count, const GLint* value) override;
+  void uniform4iv(GLint location, GLsizei count, const GLint* value) override;
+
+  void uniformMatrix2fv(
+    GLint location, GLsizei count, GLboolean transpose, const GLfloat* value) override;
+  void uniformMatrix3fv(
+    GLint location, GLsizei count, GLboolean transpose, const GLfloat* value) override;
+  void uniformMatrix4fv(
+    GLint location, GLsizei count, GLboolean transpose, const GLfloat* value) override;
+  void uniformMatrix2x3fv(
+    GLint location, GLsizei count, GLboolean transpose, const GLfloat* value) override;
+  void uniformMatrix3x2fv(
+    GLint location, GLsizei count, GLboolean transpose, const GLfloat* value) override;
+  void uniformMatrix2x4fv(
+    GLint location, GLsizei count, GLboolean transpose, const GLfloat* value) override;
+  void uniformMatrix4x2fv(
+    GLint location, GLsizei count, GLboolean transpose, const GLfloat* value) override;
+  void uniformMatrix3x4fv(
+    GLint location, GLsizei count, GLboolean transpose, const GLfloat* value) override;
+  void uniformMatrix4x3fv(
+    GLint location, GLsizei count, GLboolean transpose, const GLfloat* value) override;
+
+  GLint getAttribLocation(GLuint program, const GLchar* name) override;
+  GLint getUniformLocation(GLuint program, const GLchar* name) override;
+
+  void genBuffers(GLsizei n, GLuint* buffers) override;
+  void deleteBuffers(GLsizei n, const GLuint* buffers) override;
+
+  void bindBuffer(GLenum target, GLuint buffer) override;
+  void bufferData(
+    GLenum target, GLsizeiptr size, const GLvoid* data, GLenum usage) override;
+  void bufferSubData(
+    GLenum target, GLintptr offset, GLsizeiptr size, const void* data) override;
+
+  void vertexPointer(GLint size, GLenum type, GLsizei stride, const GLvoid* ptr) override;
+  void colorPointer(
+    GLint size, GLenum type, GLsizei stride, const GLvoid* pointer) override;
+  void normalPointer(GLenum type, GLsizei stride, const GLvoid* ptr) override;
+  void texCoordPointer(
+    GLint size, GLenum type, GLsizei stride, const GLvoid* pointer) override;
+
+  void enableVertexAttribArray(GLuint index) override;
+  void disableVertexAttribArray(GLuint index) override;
+
+  void vertexAttribPointer(
+    GLuint index,
+    GLint size,
+    GLenum type,
+    GLboolean normalized,
+    GLsizei stride,
+    const void* pointer) override;
+
+  void genTextures(GLsizei n, GLuint* textures) override;
+  void deleteTextures(GLsizei n, const GLuint* textures) override;
+
+  void bindTexture(GLenum target, GLuint texture) override;
+  void activeTexture(GLenum texture) override;
+
+  void texImage2D(
+    GLenum target,
+    GLint level,
+    GLint internalFormat,
+    GLsizei width,
+    GLsizei height,
+    GLint border,
+    GLenum format,
+    GLenum type,
+    const GLvoid* data) override;
+
+  void compressedTexImage2D(
+    GLenum target,
+    GLint level,
+    GLenum internalformat,
+    GLsizei width,
+    GLsizei height,
+    GLint border,
+    GLsizei imageSize,
+    const GLvoid* data) override;
+
+  void texParameterf(GLenum target, GLenum pname, GLfloat param) override;
+  void texParameteri(GLenum target, GLenum pname, GLint param) override;
+
+  void pixelStoref(GLenum pname, GLfloat param) override;
+  void pixelStorei(GLenum pname, GLint param) override;
+
+  void clientActiveTexture(GLenum texture) override;
+
+  void drawArrays(GLenum mode, GLint first, GLsizei count) override;
+  void drawElements(
+    GLenum mode, GLsizei count, GLenum type, const void* indices) override;
+  void multiDrawArrays(
+    GLenum mode, const GLint* first, const GLsizei* count, GLsizei primcount) override;
+
+  const GLubyte* getString(GLenum name) override;
+  GLenum getError() override;
+};
+
+} // namespace tb::gl
