d910318e6023d07b2eb11ad1a5b657c191e13016
Kristian Duske <kristian.duske@gmail.com>
2026-08-07 19:14:03 +0200

    Add tests for LoadDkmModel

---
+++ lib/TbMdlLib/test/src/tst_LoadDkmModel.cpp
@@ -0,0 +1,70 @@
+/*
+ Copyright (C) 2026 Kristian Duske
+
+ This file is part of TrenchBroom.
+
+ TrenchBroom is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ TrenchBroom is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with TrenchBroom. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "TestEnvironment.h"
+#include "base/Logger.h"
+#include "fs/DiskIO.h"
+#include "fs/VirtualFileSystem.h"
+#include "mdl/CatchConfig.h"
+#include "mdl/LoadDkmModel.h"
+
+#include <catch2/catch_test_macros.hpp>
+
+namespace tb::mdl
+{
+
+TEST_CASE("loadDkmModel")
+{
+  auto logger = NullLogger{};
+  auto fs = fs::VirtualFileSystem{};
+
+  SECTION("valid DKM model")
+  {
+    const auto path = getFixtureRoot() / "test/mdl/LoadDkmModel/cube.dkm";
+    const auto file = fs::Disk::openFile(path) | kdl::value();
+
+    auto reader = file->reader().buffer();
+    loadDkmModel("cube", reader, fs, logger) | kdl::transform([](const auto& modelData) {
+      CHECK(modelData.surfaceCount() == 1u);
+      CHECK(modelData.frameCount() == 1u);
+
+      const auto* frame = modelData.frame(0u);
+      REQUIRE(frame != nullptr);
+      CHECK(frame->name() == "frame1");
+
+      const auto& surfaces = modelData.surfaces();
+      const auto& surface = surfaces.front();
+      CHECK(surface.skinCount() == 0u);
+      CHECK(surface.frameCount() == 1u);
+    }) | kdl::transform_error([](const auto& e) { FAIL(e.msg); });
+  }
+
+  SECTION("invalid DKM file")
+  {
+    const auto path = getFixtureRoot() / "test/mdl/LoadDkmModel/invalid.dkm";
+    const auto file = fs::Disk::openFile(path) | kdl::value();
+
+    auto reader = file->reader().buffer();
+    CHECK(
+      loadDkmModel("cube", reader, fs, logger)
+      == Result<mdl::EntityModelData>{Error{"Unknown DKM model ident: 305419896"}});
+  }
+}
+
+} // namespace tb::mdl
