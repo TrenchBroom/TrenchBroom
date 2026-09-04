4769d964769665eee339103c8f23b6843a970355
Kristian Duske <kristian.duske@gmail.com>
2026-09-04 22:08:15 +0200

    WIP Introduce BoundValueStore
    todo:
    - verify we need this

--- lib/TbElLib/test/src/tst_VariableStore.cpp
+++ lib/TbElLib/test/src/tst_VariableStore.cpp
@@ -17,6 +17,8 @@
  along with TrenchBroom. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "el/BoundValue.h"
+#include "el/BoundValueStore.h"
 #include "el/Value.h"
 #include "el/VariableStore.h"
 
@@ -171,6 +173,16 @@ TEST_CASE("VariableStore")
     // stores are compared by their contents, not by their type
     CHECK(VariableTable{} == NullVariableStore{});
     CHECK_FALSE(VariableTable{{{"a", Value{1.0}}}} == NullVariableStore{});
+
+    const auto fields = BoundValueFields<std::string>{
+      {"a", [](const std::string& s) { return Value{s}; }},
+    };
+    CHECK(
+      VariableTable{{{"a", Value{"1"}}}}
+      == BoundValueStore{makeBoundValueOf(std::string{"1"}, fields)});
+    CHECK_FALSE(
+      VariableTable{{{"a", Value{"2"}}}}
+      == BoundValueStore{makeBoundValueOf(std::string{"1"}, fields)});
   }
 
   SECTION("operator!=")
