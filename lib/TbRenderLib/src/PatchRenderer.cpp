950ad11f070e1c32abcb880f10abbbd3be2be9bb
Kristian Duske <kristian.duske@gmail.com>
2026-09-10 22:36:10 +0200

    Cleanup: move helper functions to anonymous namespace

--- lib/TbRenderLib/src/PatchRenderer.cpp
+++ lib/TbRenderLib/src/PatchRenderer.cpp
@@ -47,6 +47,168 @@
 
 namespace tb::render
 {
+namespace
+{
+
+gl::MaterialIndexArrayRenderer buildMeshRenderer(
+  const std::vector<const mdl::PatchNode*>& patchNodes,
+  const mdl::EditorContext& editorContext)
+{
+  size_t vertexCount = 0u;
+  auto indexArrayMapSize = gl::MaterialIndexArrayMap::Size{};
+
+  for (const auto* patchNode : patchNodes)
+  {
+    if (editorContext.visible(*patchNode))
+    {
+      vertexCount += patchNode->grid().pointRowCount * patchNode->grid().pointColumnCount;
+
+      const auto* material = patchNode->patch().material();
+      const auto quadCount =
+        patchNode->grid().quadRowCount() * patchNode->grid().quadColumnCount();
+      indexArrayMapSize.inc(material, gl::PrimType::Triangles, 6u * quadCount);
+    }
+  }
+
+  using Vertex = gl::VertexTypes::P3NT2::Vertex;
+  auto vertices = std::vector<Vertex>{};
+  vertices.reserve(vertexCount);
+
+  auto indexArrayMapBuilder = gl::MaterialIndexArrayMapBuilder{indexArrayMapSize};
+  using Index = gl::MaterialIndexArrayMapBuilder::Index;
+
+  for (const auto* patchNode : patchNodes)
+  {
+    if (editorContext.visible(*patchNode))
+    {
+      const auto vertexOffset = vertices.size();
+
+      const auto& grid = patchNode->grid();
+      auto gridVertices =
+        grid.points | std::views::transform([](const auto& p) {
+          return Vertex{
+            vm::vec3f{p.position}, vm::vec3f{p.normal}, vm::vec2f{p.uvCoords}};
+        })
+        | kdl::ranges::to<std::vector>();
+      kdl::vec_append(vertices, std::move(gridVertices));
+
+      const auto* material = patchNode->patch().material();
+
+      const auto pointsPerRow = grid.pointColumnCount;
+      for (size_t row = 0u; row < grid.quadRowCount(); ++row)
+      {
+        for (size_t col = 0u; col < grid.quadColumnCount(); ++col)
+        {
+          const auto i0 = vertexOffset + row * pointsPerRow + col;
+          const auto i1 = vertexOffset + row * pointsPerRow + col + 1u;
+          const auto i2 = vertexOffset + (row + 1u) * pointsPerRow + col + 1u;
+          const auto i3 = vertexOffset + (row + 1u) * pointsPerRow + col;
+
+          indexArrayMapBuilder.addTriangle(
+            material,
+            static_cast<Index>(i0),
+            static_cast<Index>(i1),
+            static_cast<Index>(i2));
+          indexArrayMapBuilder.addTriangle(
+            material,
+            static_cast<Index>(i2),
+            static_cast<Index>(i3),
+            static_cast<Index>(i0));
+        }
+      }
+    }
+  }
+
+  auto vertexArray = gl::VertexArray::move(std::move(vertices));
+  auto indexArray = gl::IndexArray::move(std::move(indexArrayMapBuilder.indices()));
+  return gl::MaterialIndexArrayRenderer{
+    std::move(vertexArray),
+    std::move(indexArray),
+    std::move(indexArrayMapBuilder.ranges())};
+}
+
+DirectEdgeRenderer buildEdgeRenderer(
+  const std::vector<const mdl::PatchNode*>& patchNodes,
+  const mdl::EditorContext& editorContext)
+{
+  size_t vertexCount = 0u;
+  auto indexRangeMapSize = gl::IndexRangeMap::Size{};
+
+  for (const auto* patchNode : patchNodes)
+  {
+    if (editorContext.visible(*patchNode))
+    {
+      vertexCount +=
+        (patchNode->grid().pointRowCount + patchNode->grid().pointColumnCount - 2u) * 2u;
+      indexRangeMapSize.inc(gl::PrimType::LineLoop, vertexCount);
+    }
+  }
+
+  auto indexRangeMapBuilder =
+    gl::IndexRangeMapBuilder<gl::VertexTypes::P3>{vertexCount, indexRangeMapSize};
+
+  for (const auto* patchNode : patchNodes)
+  {
+    if (editorContext.visible(*patchNode))
+    {
+      const auto& grid = patchNode->grid();
+
+      auto edgeLoopVertices = std::vector<gl::VertexTypes::P3::Vertex>{};
+      edgeLoopVertices.reserve((grid.pointRowCount + grid.pointColumnCount - 2u) * 2u);
+
+      // walk around the patch to collect the edge vertices
+      // for each side, collect the first vertex up to but not including the last vertex
+
+      const auto t = 0u;
+      const auto b = grid.pointRowCount - 1u;
+      const auto l = 0u;
+      const auto r = grid.pointColumnCount - 1u;
+
+      auto row = t;
+      auto col = l;
+
+      while (col < r)
+      {
+        edgeLoopVertices.emplace_back(vm::vec3f{grid.point(row, col++).position});
+      }
+      contract_assert(row == t && col == r);
+
+      while (row < b)
+      {
+        edgeLoopVertices.emplace_back(vm::vec3f{grid.point(row++, col).position});
+      }
+      contract_assert(row == b && col == r);
+
+      while (col > l)
+      {
+        edgeLoopVertices.emplace_back(vm::vec3f{grid.point(row, col--).position});
+      }
+      contract_assert(row == b && col == l);
+
+      while (row > t)
+      {
+        edgeLoopVertices.emplace_back(vm::vec3f{grid.point(row--, col).position});
+      }
+      contract_assert(row == t && col == l);
+
+      indexRangeMapBuilder.addLineLoop(edgeLoopVertices);
+    }
+  }
+
+  auto vertexArray = gl::VertexArray::move(std::move(indexRangeMapBuilder.vertices()));
+  auto indexRangeMap = std::move(indexRangeMapBuilder.indices());
+  return DirectEdgeRenderer{std::move(vertexArray), std::move(indexRangeMap)};
+}
+
+bool isRealBlend(const mdl::PatchNode* patchNode)
+{
+  const auto* material = patchNode->patch().material();
+  return material
+         && material->effectiveBlendFunc().enable
+              == gl::MaterialBlendFunc::Enable::UseFactors;
+}
+
+} // namespace
 
 PatchRenderer::MeshPass::MeshPass(
   PatchRenderer& owner, gl::MaterialIndexArrayRenderer& meshRenderer)
@@ -185,167 +347,6 @@ void PatchRenderer::renderTransparent(
     renderBatch.add(&m_transparentPass);
   }
 }
-
-static gl::MaterialIndexArrayRenderer buildMeshRenderer(
-  const std::vector<const mdl::PatchNode*>& patchNodes,
-  const mdl::EditorContext& editorContext)
-{
-  size_t vertexCount = 0u;
-  auto indexArrayMapSize = gl::MaterialIndexArrayMap::Size{};
-
-  for (const auto* patchNode : patchNodes)
-  {
-    if (editorContext.visible(*patchNode))
-    {
-      vertexCount += patchNode->grid().pointRowCount * patchNode->grid().pointColumnCount;
-
-      const auto* material = patchNode->patch().material();
-      const auto quadCount =
-        patchNode->grid().quadRowCount() * patchNode->grid().quadColumnCount();
-      indexArrayMapSize.inc(material, gl::PrimType::Triangles, 6u * quadCount);
-    }
-  }
-
-  using Vertex = gl::VertexTypes::P3NT2::Vertex;
-  auto vertices = std::vector<Vertex>{};
-  vertices.reserve(vertexCount);
-
-  auto indexArrayMapBuilder = gl::MaterialIndexArrayMapBuilder{indexArrayMapSize};
-  using Index = gl::MaterialIndexArrayMapBuilder::Index;
-
-  for (const auto* patchNode : patchNodes)
-  {
-    if (editorContext.visible(*patchNode))
-    {
-      const auto vertexOffset = vertices.size();
-
-      const auto& grid = patchNode->grid();
-      auto gridVertices =
-        grid.points | std::views::transform([](const auto& p) {
-          return Vertex{
-            vm::vec3f{p.position}, vm::vec3f{p.normal}, vm::vec2f{p.uvCoords}};
-        })
-        | kdl::ranges::to<std::vector>();
-      kdl::vec_append(vertices, std::move(gridVertices));
-
-      const auto* material = patchNode->patch().material();
-
-      const auto pointsPerRow = grid.pointColumnCount;
-      for (size_t row = 0u; row < grid.quadRowCount(); ++row)
-      {
-        for (size_t col = 0u; col < grid.quadColumnCount(); ++col)
-        {
-          const auto i0 = vertexOffset + row * pointsPerRow + col;
-          const auto i1 = vertexOffset + row * pointsPerRow + col + 1u;
-          const auto i2 = vertexOffset + (row + 1u) * pointsPerRow + col + 1u;
-          const auto i3 = vertexOffset + (row + 1u) * pointsPerRow + col;
-
-          indexArrayMapBuilder.addTriangle(
-            material,
-            static_cast<Index>(i0),
-            static_cast<Index>(i1),
-            static_cast<Index>(i2));
-          indexArrayMapBuilder.addTriangle(
-            material,
-            static_cast<Index>(i2),
-            static_cast<Index>(i3),
-            static_cast<Index>(i0));
-        }
-      }
-    }
-  }
-
-  auto vertexArray = gl::VertexArray::move(std::move(vertices));
-  auto indexArray = gl::IndexArray::move(std::move(indexArrayMapBuilder.indices()));
-  return gl::MaterialIndexArrayRenderer{
-    std::move(vertexArray),
-    std::move(indexArray),
-    std::move(indexArrayMapBuilder.ranges())};
-}
-
-static DirectEdgeRenderer buildEdgeRenderer(
-  const std::vector<const mdl::PatchNode*>& patchNodes,
-  const mdl::EditorContext& editorContext)
-{
-  size_t vertexCount = 0u;
-  auto indexRangeMapSize = gl::IndexRangeMap::Size{};
-
-  for (const auto* patchNode : patchNodes)
-  {
-    if (editorContext.visible(*patchNode))
-    {
-      vertexCount +=
-        (patchNode->grid().pointRowCount + patchNode->grid().pointColumnCount - 2u) * 2u;
-      indexRangeMapSize.inc(gl::PrimType::LineLoop, vertexCount);
-    }
-  }
-
-  auto indexRangeMapBuilder =
-    gl::IndexRangeMapBuilder<gl::VertexTypes::P3>{vertexCount, indexRangeMapSize};
-
-  for (const auto* patchNode : patchNodes)
-  {
-    if (editorContext.visible(*patchNode))
-    {
-      const auto& grid = patchNode->grid();
-
-      auto edgeLoopVertices = std::vector<gl::VertexTypes::P3::Vertex>{};
-      edgeLoopVertices.reserve((grid.pointRowCount + grid.pointColumnCount - 2u) * 2u);
-
-      // walk around the patch to collect the edge vertices
-      // for each side, collect the first vertex up to but not including the last vertex
-
-      const auto t = 0u;
-      const auto b = grid.pointRowCount - 1u;
-      const auto l = 0u;
-      const auto r = grid.pointColumnCount - 1u;
-
-      auto row = t;
-      auto col = l;
-
-      while (col < r)
-      {
-        edgeLoopVertices.emplace_back(vm::vec3f{grid.point(row, col++).position});
-      }
-      contract_assert(row == t && col == r);
-
-      while (row < b)
-      {
-        edgeLoopVertices.emplace_back(vm::vec3f{grid.point(row++, col).position});
-      }
-      contract_assert(row == b && col == r);
-
-      while (col > l)
-      {
-        edgeLoopVertices.emplace_back(vm::vec3f{grid.point(row, col--).position});
-      }
-      contract_assert(row == b && col == l);
-
-      while (row > t)
-      {
-        edgeLoopVertices.emplace_back(vm::vec3f{grid.point(row--, col).position});
-      }
-      contract_assert(row == t && col == l);
-
-      indexRangeMapBuilder.addLineLoop(edgeLoopVertices);
-    }
-  }
-
-  auto vertexArray = gl::VertexArray::move(std::move(indexRangeMapBuilder.vertices()));
-  auto indexRangeMap = std::move(indexRangeMapBuilder.indices());
-  return DirectEdgeRenderer{std::move(vertexArray), std::move(indexRangeMap)};
-}
-
-namespace
-{
-bool isRealBlend(const mdl::PatchNode* patchNode)
-{
-  const auto* material = patchNode->patch().material();
-  return material
-         && material->effectiveBlendFunc().enable
-              == gl::MaterialBlendFunc::Enable::UseFactors;
-}
-} // namespace
 
 void PatchRenderer::validate()
 {
